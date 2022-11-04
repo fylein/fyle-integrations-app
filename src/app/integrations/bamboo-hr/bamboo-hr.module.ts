59025e78dc1c6e5d584170b47019aa6997c99d20
ashwin1111 <winash1998@gmail.com>
2022-11-04 12:25:25 +0530

    Adding modules

---
+++ src/app/integrations/bamboo-hr/bamboo-hr.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BambooHrRoutingModule } from './bamboo-hr-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BambooHrRoutingModule
+  ]
+})
+export class BambooHrModule { }
---
+++ src/app/integrations/integrations.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IntegrationsRoutingModule } from './integrations-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IntegrationsRoutingModule
+  ]
+})
+export class IntegrationsModule { }
