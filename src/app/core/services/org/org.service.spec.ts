1ea3440426302b868b10c69639ad8b33fe8de11e
ashwin1111 <winash1998@gmail.com>
2022-12-13 13:43:45 +0530

    Fix broken unit tests

--- src/app/core/services/org/org.service.spec.ts
+++ src/app/core/services/org/org.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { OrgService } from './org.service';
@@ -6,7 +7,11 @@ describe('OrgService', () => {
   let service: OrgService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(OrgService);
   });
 
