cfd8656d27a4041bf1f5e15d80729c02773a90e3
Fyle <fyle@Fyles-MacBook-Air.local>
2023-01-12 15:03:42 +0530

    Routers for the app

--- src/app/integrations/qbd/main/main-routing.module.ts
+++ src/app/integrations/qbd/main/main-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'configuration',
+        loadChildren: () => import('./configuration/configuration.module').then(m => m.ConfigurationModule)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/integrations/qbd/onboarding/onboarding-routing.module.ts
+++ src/app/integrations/qbd/onboarding/onboarding-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OnboardingAdvancedSettingComponent } from './onboarding-advanced-setting/onboarding-advanced-setting.component';
+import { OnboardingDoneComponent } from './onboarding-done/onboarding-done.component';
+import { OnboardingExportSettingComponent } from './onboarding-export-setting/onboarding-export-setting.component';
+import { OnboardingFieldSettingComponent } from './onboarding-field-setting/onboarding-field-setting.component';
+import { LandingComponent } from './onboarding-landing/landing.component';
+import { OnboardingComponent } from './onboarding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OnboardingComponent,
+    children: [
+      {
+        path: 'landing',
+        component: LandingComponent,
+        // canActivate: [WorkspacesGuard]
+      },
+      {
+        path: 'export_settings',
+        component: OnboardingExportSettingComponent,
+        // canActivate: [WorkspacesGuard]
+      },
+      {
+        path: 'field_mapping',
+        component: OnboardingFieldSettingComponent,
+        // canActivate: [WorkspacesGuard]
+      },
+      {
+        path: 'advanced_settings',
+        component: OnboardingAdvancedSettingComponent,
+        // canActivate: [WorkspacesGuard]
+      },
+      {
+        path: 'done',
+        component: OnboardingDoneComponent,
+        // canActivate: [WorkspacesGuard]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
