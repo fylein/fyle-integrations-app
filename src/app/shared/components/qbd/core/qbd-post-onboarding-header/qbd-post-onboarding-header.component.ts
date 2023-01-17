1e82471c12e673b05c15bcd28db8b450c4de465d
Fyle <fyle@Fyles-MacBook-Air.local>
2023-01-17 14:01:16 +0530

    folder changes commit

---
+++ src/app/shared/components/qbd/core/qbd-post-onboarding-header/qbd-post-onboarding-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qbd-post-onboarding-header',
+  templateUrl: './qbd-post-onboarding-header.component.html',
+  styleUrls: ['./qbd-post-onboarding-header.component.scss']
+})
+export class QbdPostOnboardingHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
