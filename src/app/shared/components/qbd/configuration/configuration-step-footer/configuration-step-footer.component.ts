90e7f800e94180f8b8a59fbc11a9d32cada0c2cc
Fyle <fyle@Fyles-MacBook-Air.local>
2023-01-27 17:39:54 +0530

    lint fix commit

--- src/app/shared/components/qbd/configuration/configuration-step-footer/configuration-step-footer.component.ts
+++ src/app/shared/components/qbd/configuration/configuration-step-footer/configuration-step-footer.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class ConfigurationStepFooterComponent implements OnInit {
 
   @Input() ctaText: string;
-  
+
   @Input() isButtonDisabled: boolean;
 
   @Input() showBackButton: boolean;
