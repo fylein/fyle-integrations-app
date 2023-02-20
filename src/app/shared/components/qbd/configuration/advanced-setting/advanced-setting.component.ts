33c056a9a2b753c737a1ab3aca28d8e52d4beb97
Fyle <fyle@Fyles-MacBook-Air.local>
2023-02-20 17:49:05 +0530

    bug fixing in onboarding

--- src/app/shared/components/qbd/configuration/advanced-setting/advanced-setting.component.ts
+++ src/app/shared/components/qbd/configuration/advanced-setting/advanced-setting.component.ts
@@ -162,7 +162,8 @@ export class AdvancedSettingComponent implements OnInit {
           dayOfMonth: [this.advancedSettings?.day_of_month ? this.advancedSettings?.day_of_month : null],
           dayOfWeek: [this.advancedSettings?.day_of_week ? this.advancedSettings?.day_of_week : null],
           timeOfDay: [this.initialTime()],
-          meridiem: [this.merdiemType()]
+          meridiem: [this.merdiemType()],
+          search: []
       });
       this.isLoading = false;
       this.createMemoStructureWatcher();
@@ -176,7 +177,8 @@ export class AdvancedSettingComponent implements OnInit {
           dayOfMonth: [null],
           dayOfWeek: [null],
           timeOfDay: [this.initialTime()],
-          meridiem: [this.merdiemType()]
+          meridiem: [this.merdiemType()],
+          search: []
         });
         this.isLoading = false;
         this.createMemoStructureWatcher();
