295efcb0151769b28da325eef9ea4f9dd95cad60
Fyle <fyle@Fyles-MacBook-Air.local>
2023-03-13 13:13:53 +0530

    configutation module error

--- src/app/integrations/gusto/gusto.module.ts
+++ src/app/integrations/gusto/gusto.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GustoRoutingModule } from './gusto-routing.module';
-
 import { GustoComponent } from './gusto.component';
-import { ConfigurationComponent } from './configuration/configuration.component';
+import { GustoConfigurationComponent } from './gusto-configuration/gusto-configuration.component';
 
 
 @NgModule({
   declarations: [
     GustoComponent,
-    ConfigurationComponent
+    GustoConfigurationComponent
   ],
   imports: [
     CommonModule,
