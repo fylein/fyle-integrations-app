f278fea574eaf9f3e2f539434988ad34e40cffe9
labhvam5 <labhvam.s@fyle.in>
2023-03-13 17:57:30 +0530

    fixing test

---
+++ src/app/core/services/travelperk/travelperk.fixture.ts
@@ -0,0 +1,13 @@
+import { Travelperk } from "../../models/travelperk/travelperk.model";
+
+export const travelperkMockData: Travelperk = {
+    id: 1,
+    folder_id: '1234',
+    package_id: '467',
+    is_fyle_connected: true,
+    is_s3_connected: true,
+    org: 1,
+    travelperk_connection_id: 123,
+    created_at: new Date(),
+    updated_at: new Date()
+};
--- src/app/core/services/travelperk/travelperk.service.spec.ts
+++ src/app/core/services/travelperk/travelperk.service.spec.ts
@@ -1,9 +1,14 @@
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
+import { travelperkMockData } from './travelperk.fixture';
 
 import { TravelperkService } from './travelperk.service';
 
 describe('TravelperkService', () => {
   let service: TravelperkService;
+  let httpMock: HttpTestingController;
+  const API_BASE_URL = environment.api_url;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +17,45 @@ describe('TravelperkService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get travelperk data', () => {
+    service.getTravelperkData().subscribe((res) => {
+      expect(res).toEqual(travelperkMockData);
+    });
+
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/orgs/1/travelperk/`
+    });
+
+    req.flush(travelperkMockData);
+  });
+
+  it('should create folder', () => {
+    service.createFolder().subscribe((res) => {
+      console.log(res)
+      expect(res).toEqual({});
+    });
+
+    const req = httpMock.expectOne({
+      method: 'POST',
+      url: `${API_BASE_URL}/orgs/1/travelperk/folder/`
+    });
+
+    req.flush({});
+  });
+
+  it('should upload package', () => {
+    service.uploadPackage().subscribe((res) => {
+      expect(res).toEqual({});
+    });
+
+    const req = httpMock.expectOne({
+      method: 'POST',
+      url: `${API_BASE_URL}/orgs/1/travelperk/packages/`
+    });
+
+    req.flush({});
   });
 });
