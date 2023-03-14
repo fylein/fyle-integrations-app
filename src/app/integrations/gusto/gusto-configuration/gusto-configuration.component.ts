f5363e67fbcedce60f955a27659b47fba9098d0c
Fyle <fyle@Fyles-MacBook-Air.local>
2023-03-14 21:52:47 +0530

    gusto configuration page

--- src/app/integrations/gusto/gusto-configuration/gusto-configuration.component.ts
+++ src/app/integrations/gusto/gusto-configuration/gusto-configuration.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Page, QBDConfigurationCtaText, QBDOnboardingState, ToastSeverity } from 'src/app/core/models/enum/enum.model';
+import { EmailOption, GustoConfiguration, GustoConfigurationPost, GustoModel } from 'src/app/core/models/gusto/gusto.model';
+import { Org } from 'src/app/core/models/org/org.model';
+import { GustoService } from 'src/app/core/services/gusto/gusto.service';
+import { TrackingService } from 'src/app/core/services/integration/tracking.service';
+import { OrgService } from 'src/app/core/services/org/org.service';
+import { QbdToastService } from 'src/app/core/services/qbd/qbd-core/qbd-toast.service';
 
 @Component({
   selector: 'app-gusto-configuration',
@@ -7,9 +15,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GustoConfigurationComponent implements OnInit {
 
-  constructor() { }
+  @Input() gustoConfiguration: GustoConfiguration;
+
+  @Input() additionalEmails: EmailOption[];
+
+  @Output() updateConfiguration = new EventEmitter<GustoConfigurationPost>();
+
+  configurationForm: FormGroup;
+
+  saveInProgress: boolean;
+
+  ConfigurationCtaText = QBDConfigurationCtaText;
+
+  org: Org = this.orgService.getCachedOrg();
+
+  configurationFields: string[] = ['Employee Name', 'Employee ID', 'Email ID', 'Department', 'Manager'];
+
+  private sessionStartTime = new Date();
+
+  constructor(
+    private gustoService: GustoService,
+    private formBuilder: FormBuilder,
+    private orgService: OrgService,
+    private trackingService: TrackingService,
+    private toastService: QbdToastService
+  ) { }
+
+  private constructPayloadAndSave(): void {
+    this.saveInProgress = true;
+    const configurationPayload = GustoModel.constructGustoConfigurationPayload(this.configurationForm, this.org.id);
+    this.gustoService.postConfigurations(configurationPayload).subscribe((response: GustoConfiguration) => {
+      this.saveInProgress = false;
+      this.toastService.displayToastMessage(ToastSeverity.SUCCESS, 'Email address saved successfully');
+      this.trackingService.trackTimeSpent(Page.ADVANCED_SETTINGS_QBD, this.sessionStartTime);
+    }, () => {
+      this.saveInProgress = false;
+      this.toastService.displayToastMessage(ToastSeverity.ERROR, 'Error saving Email address, please try again later');
+      });
+  }
+
+  save(): void {
+    if (this.configurationForm.valid) {
+      this.constructPayloadAndSave();
+    }
+  }
+
+  setupGustoConfiguration() {
+    this.configurationForm = this.formBuilder.group({
+      email: [this.gustoConfiguration?.emails_selected.length > 0 ? this.gustoConfiguration?.emails_selected : []],
+      additionalEmails: [this.gustoConfiguration?.additional_email_options.length > 0 ? this.gustoConfiguration?.additional_email_options : []]
+    });
+  }
 
   ngOnInit(): void {
+    this.setupGustoConfiguration();
   }
 
 }
