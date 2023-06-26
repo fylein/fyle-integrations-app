1d89f28e8ca476e0ac3dab1bc90c6c210d6d8295
anishfyle <anishkumar.s@fyle.in>
2023-06-26 15:31:37 +0530

    onboarding export setting

--- src/app/integrations/si/onboarding/onboarding.module.ts
+++ src/app/integrations/si/onboarding/onboarding.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnboardingComponent } from './onboarding.component';
 import { OnboardingAdvancedSettingComponent } from '../onboarding-advanced-setting/onboarding-advanced-setting.component';
+import { OnboardingExportSettingComponent } from '../onboarding-export-setting/onboarding-export-setting.component';
 
 
 
 @NgModule({
   declarations: [
     OnboardingComponent,
-    OnboardingAdvancedSettingComponent
+    OnboardingAdvancedSettingComponent,
+    OnboardingExportSettingComponent
   ],
   imports: [
     CommonModule
