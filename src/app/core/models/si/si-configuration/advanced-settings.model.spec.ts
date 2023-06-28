003db88a587b16fe9d4e1f3bccf8d2f998bc789a
anishfyle <anishkumar.s@fyle.in>
2023-06-28 15:58:23 +0530

    models

---
+++ src/app/core/models/si/si-configuration/advanced-settings.model.spec.ts
@@ -0,0 +1,7 @@
+import { AdvancedSettings } from './advanced-settings.model';
+
+describe('AdvancedSettings', () => {
+  it('should create an instance', () => {
+    expect(new AdvancedSettings()).toBeTruthy();
+  });
+});
