75785097fbd174f601349ba6815a83579890dc8a
anishfyle <anishkumar.s@fyle.in>
2023-07-10 21:58:00 +0530

    Sage Intacct Auth

--- src/app/core/services/si/si-core/si-api.service.ts
+++ src/app/core/services/si/si-core/si-api.service.ts
@@ -1,9 +1,80 @@
+import {
+  HttpClient,
+  HttpErrorResponse,  HttpHeaders,
+  HttpParams
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+const API_BASE_URL = environment.api_url;
 
 @Injectable({
   providedIn: 'root'
 })
-export class SiApiService {
+export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse, httpMethod: string) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      if (error.status >= 500 && httpMethod === 'GET') {
+        console.error(
+          `Backend returned code ${error.status}, ` + `body was: ${JSON.stringify(error.error)}`
+        );
+      } else if (error.status >= 400 && httpMethod === 'POST') {
+        console.error(
+          `Backend returned code ${error.status}, ` + `body was: ${JSON.stringify(error.error)}`
+        );
+      }
+    }
+    return throwError(error);
+  }
+  // Having any here is ok
+  post(endpoint: string, body: {}): Observable<any> {
+    return this.http
+      .post(
+        API_BASE_URL + endpoint,
+        body,
+        httpOptions
+      )
+      .pipe(catchError(error => {
+        return this.handleError(error, 'POST');
+      }));
+  }
+  patch(endpoint: string, body: {}): Observable<any> {
+    return this.http
+      .patch(
+        API_BASE_URL + endpoint,
+        body,
+        httpOptions
+      )
+      .pipe(catchError(error => {
+        return this.handleError(error, 'PATCH');
+      }));
+  }
+  // Having any here is ok
+  get(endpoint: string, apiParams: {}): Observable<any> {
+    let params = new HttpParams();
+    Object.keys(apiParams).forEach(key => {
+      params = params.set(key, apiParams[key]);
+    });
+
+    return this.http.get(
+        API_BASE_URL + endpoint,
+        {params}
+      )
+      .pipe(catchError(error => {
+        return this.handleError(error, 'GET');
+      }));
+  }
 }
--- src/app/core/services/si/si-core/si-auth.service.ts
+++ src/app/core/services/si/si-core/si-auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Token } from 'src/app/core/models/misc/token.model';
+import { SiApiService } from './si-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SiAuthService {
 
-  constructor() { }
+  constructor(
+    private apiService: SiApiService
+  ) { }
+
+  siLogin(refresh_token: string): Observable<Token> {
+    return this.apiService.post('/auth/login_with_refresh_token/', { refresh_token: refresh_token });
+  }
 }
