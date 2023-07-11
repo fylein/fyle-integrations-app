5321f199f071db57abf3af96dc03af77c241fb1d
anishfyle <anishkumar.s@fyle.in>
2023-07-11 15:45:53 +0530

    rouring

--- src/app/integrations/si/onboarding/onboarding-routing.module.ts
+++ src/app/integrations/si/onboarding/onboarding-routing.module.ts
@@ -1,12 +1,46 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OnboardingLandingComponent } from './onboarding-landing/onboarding-landing.component';
+import { OnboardingExportSettingComponent } from './onboarding-export-setting/onboarding-export-setting.component';
+import { OnboardingImportSettingComponent } from './onboarding-import-setting/onboarding-import-setting.component';
+import { OnboardingAdvancedSettingComponent } from './onboarding-advanced-setting/onboarding-advanced-setting.component';
+import { OnboardingDoneComponent } from './onboarding-done/onboarding-done.component';
+import { OnboardingComponent } from './onboarding.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OnboardingComponent,
+    children: [
+      {
+        path: 'landing',
+        component: OnboardingLandingComponent
+      },
+      {
+        path: 'export_settings',
+        component: OnboardingExportSettingComponent
+      },
+      {
+        path: 'import_settings',
+        component: OnboardingImportSettingComponent
+      },
+      {
+        path: 'advanced_settings',
+        component: OnboardingAdvancedSettingComponent
+      },
+      {
+        path: 'done',
+        component: OnboardingDoneComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class OnboardingRoutingModule { }
