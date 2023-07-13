b967ce1595ff369ad64c14a711275ed88d7184fb
anishfyle <anishkumar.s@fyle.in>
2023-07-13 15:24:00 +0530

    generic extendable workspace model

--- src/app/core/models/db/workspaces.model.ts
+++ src/app/core/models/db/workspaces.model.ts
@@ -1,15 +1,9 @@
-import { IntacctOnboardingState } from "../enum/enum.model";
-
-
 export type Workspace = {
   id: number;
   name: string;
   user: number[];
   org_id: string;
   currency: string;
-  reimbursable_last_synced_at: Date | null;
-  ccc_last_synced_at: Date | null;
-  onboarding_state: IntacctOnboardingState;
   created_at: Date;
   updated_at: Date;
 }
