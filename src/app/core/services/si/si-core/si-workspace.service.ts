ee6f04da5114eab8f185861533db1582a1e1691b
anishfyle <anishkumar.s@fyle.in>
2023-07-13 12:20:14 +0530

    workspace service

--- src/app/core/services/si/si-core/si-workspace.service.ts
+++ src/app/core/services/si/si-core/si-workspace.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OnboardingState } from 'src/app/core/models/enum/enum.model';
+import { Workspace } from 'src/app/core/models/qbd/db/workspaces.model';
+import { Cacheable } from 'ts-cacheable';
+import { StorageService } from '../../core/storage.service';
+import { SiApiService } from './si-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SiWorkspaceService {
 
-  constructor() { }
+  constructor(
+    private storageService: StorageService,
+    private apiService: SiApiService
+  ) { }
+
+  @Cacheable()
+  getQBDWorkspace(orgId: string): Observable<Workspace> {
+    return this.apiService.get('/workspaces/', {org_id: orgId});
+  }
+
+  postQBDWorkspace(): Observable<Workspace> {
+    return this.apiService.post('/workspaces/', {});
+  }
+
+  getWorkspaceId(): string {
+    return this.storageService.get('workspaceId');
+  }
+
+  setOnboardingState(onboardingState: OnboardingState): void {
+    return this.storageService.set('QBDOnboardingState', onboardingState);
+  }
+
+  getOnboardingState(): OnboardingState {
+    const onboardingState = this.storageService.get('OnboardingState');
+    return onboardingState ? onboardingState : OnboardingState.EXPORT_SETTINGS;
+  }
+
 }
