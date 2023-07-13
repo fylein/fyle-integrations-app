eb3ed60a0dff1c313a56eba6743564b1814835df
anishfyle <anishkumar.s@fyle.in>
2023-07-13 12:28:49 +0530

    workspace models

--- src/app/core/models/si/db/workspaces.model.ts
+++ src/app/core/models/si/db/workspaces.model.ts
@@ -1,2 +1,14 @@
-export class Workspaces {
+import { OnboardingState } from "../../enum/enum.model";
+
+export type Workspace = {
+  id: number;
+  name: string;
+  user: number[];
+  org_id: string;
+  currency: string;
+  reimbursable_last_synced_at: Date | null;
+  ccc_last_synced_at: Date | null;
+  onboarding_state: OnboardingState;
+  created_at: Date;
+  updated_at: Date;
 }
