5491ac2c92c3de06e73f41fd4150bf5b2cd10767
anishfyle <anishkumar.s@fyle.in>
2023-07-17 08:51:55 +0530

    sage stepper

--- src/app/integrations/si/onboarding/onboarding-sage-stepper/onboarding-sage-stepper.component.ts
+++ src/app/integrations/si/onboarding/onboarding-sage-stepper/onboarding-sage-stepper.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IntacctOnboardingState } from 'src/app/core/models/enum/enum.model';
+import { OnboardingStepper } from 'src/app/core/models/misc/onboarding-stepper.model';
+import { SiWorkspaceService } from 'src/app/core/services/si/si-core/si-workspace.service'; 
 
 @Component({
   selector: 'app-onboarding-sage-stepper',
@@ -7,9 +11,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingSageStepperComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private workspaceService: SiWorkspaceService
+  ) { }
+
+  @Input() currentStep: string;
+
+  onboardingSteps: OnboardingStepper[] = [
+    {
+      active: false,
+      completed: false,
+      number: 1,
+      step: 'Connect to Sage Intacct',
+      icon: 'connector',
+      route: 'connector',
+      size: {
+        height: '18px',
+        width: '15px'
+      }
+    },
+    {
+      active: false,
+      completed: false,
+      number: 2,
+      step: 'Export Settings',
+      icon: 'export-setting',
+      route: 'export_settings',
+      size: {
+        height: '18px',
+        width: '15px'
+      }
+    },
+    {
+      active: false,
+      completed: false,
+      number: 3,
+      step: 'Import Settings',
+      icon: 'import-settings',
+      route: 'import_settings',
+      size: {
+        height: '18px',
+        width: '15px'
+      }
+    },
+    {
+      active: false,
+      completed: false,
+      number: 4,
+      step: 'Advanced',
+      icon: 'advanced-setting',
+      route: 'advanced_settings',
+      size: {
+        height: '20px',
+        width: '20px'
+      }
+    }
+  ];
+
+  private updateActiveAndCompletedSteps(): void {
+    this.onboardingSteps.forEach(step => {
+      if (step.step === this.currentStep) {
+        step.active = true;
+      }
+    });
+    const onboardingState: IntacctOnboardingState = this.workspaceService.getIntacctOnboardingState();
+    const onboardingStateStepMap = {
+      [IntacctOnboardingState.CONNECTION]: 1,
+      [IntacctOnboardingState.LOCATION_ENTITY]: 2,
+      [IntacctOnboardingState.EXPORT_SETTINGS]: 3,
+      [IntacctOnboardingState.IMPORT_SETTINGS]: 4,
+      [IntacctOnboardingState.ADVANCED_SETTINGS]: 5,
+      [IntacctOnboardingState.COMPLETE]: 6
+    };
+
+    for (let index = onboardingStateStepMap[onboardingState] - 2; index > 0; index--) {
+      this.onboardingSteps[index - 1].completed = true;
+    }
+  }
+
+  navigate(canNavigate: boolean, route: string): void {
+    if (canNavigate) {
+      this.router.navigate([`/integrations/intacct/onboarding/${route}`]);
+    }
+  }
 
   ngOnInit(): void {
+    this.updateActiveAndCompletedSteps();
   }
 
 }
