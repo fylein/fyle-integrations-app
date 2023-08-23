a5ab305e710b16a8de03b5108a3a8026c01d8d24
anishfyle <anishkumar.s@fyle.in>
2023-08-23 08:49:25 +0530

    service files

--- src/app/core/services/si/si-configuration/si-import-setting.service.ts
+++ src/app/core/services/si/si-configuration/si-import-setting.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { SiApiService } from '../si-core/si-api.service';
+import { SiWorkspaceService } from '../si-core/si-workspace.service';
+import { Observable } from 'rxjs';
+import { ImportSettingGet, ImportSettingPost } from 'src/app/core/models/si/si-configuration/import-settings.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SiImportSettingService {
 
-  constructor() { }
+  constructor(
+    private apiService: SiApiService,
+    private workspaceService: SiWorkspaceService
+  ) { }
+
+  getImportSettings(): Observable<ImportSettingGet>{
+    return this.apiService.get(`/v2/workspaces/${this.workspaceService.getWorkspaceId()}/import_settings/`, {});
+  }
+
+  postImportSettings(importSettingsPayload: ImportSettingPost): Observable<ImportSettingGet> {
+    return this.apiService.put(`/v2/workspaces/${this.workspaceService.getWorkspaceId()}/import_settings/`, importSettingsPayload);
+  }
 }
