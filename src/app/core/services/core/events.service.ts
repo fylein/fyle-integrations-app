bdf504490fd11a4262536caf9ddc31d9e152b2ae
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-08-31 15:05:06 +0530

    Auth changes for redesigned intacct app (#115)

--- src/app/core/services/core/events.service.ts
+++ src/app/core/services/core/events.service.ts
@@ -3,12 +3,16 @@ import { environment } from 'src/environments/environment';
 import { WorkatoConnectionStatus } from '../../models/travelperk/travelperk.model';
 import { WindowService } from './window.service';
 
+export const EXPOSE_INTACCT_NEW_APP = false;
+
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
   @Output() getWorkatoConnectionStatus: EventEmitter<WorkatoConnectionStatus> = new EventEmitter();
+
+  @Output() sageIntacctLogin: EventEmitter<string> = new EventEmitter();
 
   constructor(
     private windowService: WindowService
@@ -17,7 +21,11 @@ export class EventsService {
   receiveEvent(): void {
     this.windowService.nativeWindow.addEventListener('message', (message) => {
       if (message.data && message.data.redirectUri && message.origin === environment.fyle_app_url) {
-        this.windowService.openInNewTab(message.data.redirectUri);
+        if (EXPOSE_INTACCT_NEW_APP && message.data.redirectUri.includes('sage-intacct')) {
+          this.sageIntacctLogin.emit(message.data.redirectUri);
+        } else {
+          this.windowService.openInNewTab(message.data.redirectUri);
+        }
       } else if (message.data && JSON.parse(message.data).type === 'connectionStatusChange' && message.origin.includes('workato')) {
         this.getWorkatoConnectionStatus.emit(JSON.parse(message.data));
       }
