2e3d164eec0aa05a4578da467396ee3f5673df8e
anishfyle <anishkumar.s@fyle.in>
2023-09-13 14:30:53 +0530

    Lint Fix

--- src/app/shared/components/apps/app-integration-landing/app-integration-landing.component.ts
+++ src/app/shared/components/apps/app-integration-landing/app-integration-landing.component.ts
@@ -11,7 +11,7 @@ import { WindowService } from 'src/app/core/services/core/window.service';
 export class AppIntegrationLandingComponent implements OnInit {
 
   @Input() headlineText: string;
-  
+
   @Input() headerText: string;
 
   @Input() svgPath: string;
