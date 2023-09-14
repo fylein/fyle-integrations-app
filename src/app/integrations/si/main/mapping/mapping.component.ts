10dd8680afba44db9d242ee1412493e855aba48a
anishfyle <anishkumar.s@fyle.in>
2023-09-14 07:18:01 +0530

    Initial Commit

--- src/app/integrations/si/main/mapping/mapping.component.ts
+++ src/app/integrations/si/main/mapping/mapping.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-mapping',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MappingComponent implements OnInit {
 
-  constructor() { }
+  mappingPages: MenuItem[] = [
+    {label: 'Employee', routerLink: '/integrations/intacct/main/mapping/employee_mapping'},
+    {label: 'Category', routerLink: '/integrations/intacct/main/mapping/category_mapping'},
+    {label: 'Generic', routerLink: '/integrations/intacct/main/mapping/generic_mapping'},
+  ];
+
+  activeModule: MenuItem;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.activeModule = this.mappingPages[0];
+    this.router.navigateByUrl(this.mappingPages[0].routerLink);
   }
 
 }
