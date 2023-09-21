600665743b24e9359eb4bd1c638ae9ac91313a60
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-09-21 12:41:43 +0530

    Dashboard Mapping Resolve : Employee/Category (#133)
    * P-Dialog for Dashboard Mapping Resolve
    
    * Employee Mapping Dashboard Resolve
    
    * Lint Fixes

--- src/app/core/models/si/db/category-mapping.model.ts
+++ src/app/core/models/si/db/category-mapping.model.ts
@@ -2,15 +2,19 @@ import { ExpenseAttributeDetail } from "./expense-attribute-detail.model";
 import { MappingDestination } from "./mapping-destination.model";
 import { MappingSource } from "./mapping-source.model";
 
-export type CategoryMapping = {
-  id?: number;
+
+export type CategoryMappingPost = {
   source_category: MappingSource;
   destination_account?: MappingDestination;
   destination_expense_head?: MappingDestination;
   created_at?: Date;
   updated_at?: Date;
   workspace: number;
-};
+}
+
+export interface CategoryMapping extends CategoryMappingPost {
+  id: number;
+}
 
 export type CategoryMappingResult = {
   id: number;
