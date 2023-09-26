726f27b6f11a5facc743e36b4c6f5cd64e1c1a6a
ashwin1111 <winash1998@gmail.com>
2023-09-26 19:46:37 +0530

    connector and few more fixes

--- src/app/shared/helper/mandatory-field/mandatory-field.component.ts
+++ src/app/shared/helper/mandatory-field/mandatory-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mandatory-field',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mandatory-field.component.scss']
 })
 export class MandatoryFieldComponent implements OnInit {
+
+  @Input() noSpacing: boolean;
 
   constructor() { }
 
