b6a8464d344e5037e7770336f0040bc984c43b3e
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-10-09 12:54:37 +0530

    Fix for Destination ID: Export and Import Settings [General Mappings] (#183)
    * Fix Export Setting Payload: Destination ID
    
    * Import Setting Fix
    
    * default_reimbursable_expense_payment_type Fix ID

--- src/app/shared/components/si/configuration/configuration-export-settings/configuration-export-settings.component.ts
+++ src/app/shared/components/si/configuration/configuration-export-settings/configuration-export-settings.component.ts
@@ -403,7 +403,7 @@ export class ConfigurationExportSettingsComponent implements OnInit {
     private initializeExportSettingsFormWithData(): void {
       const configurations = this.exportSettings?.configurations;
       const generalMappings = this.exportSettings?.general_mappings;
-      const findObjectById = (array: DestinationAttribute[], id: string) => array?.find(item => item.id.toString() === id) || null;
+      const findObjectById = (array: DestinationAttribute[], id: string) => array?.find(item => item.destination_id === id) || null;
 
       this.exportSettingsForm = this.formBuilder.group({
         reimbursableExpense: [Boolean(configurations?.reimbursable_expenses_object) || null, this.exportSelectionValidator()],
