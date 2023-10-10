7e6635a41d66ec8e83004774ad7082cdf0b1bb11
Fyle <fyle@Fyles-MacBook-Air.local>
2023-10-10 18:52:26 +0530

    helper services coding

--- src/app/core/services/core/helper.service.spec.ts
+++ src/app/core/services/core/helper.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HelperService } from './helper.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
 
 describe('HelperService', () => {
   let service: HelperService;
+  const routerSpy = { navigate: jasmine.createSpy('navigate'), url: 'localhost/path/intacct' };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
     service = TestBed.inject(HelperService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('callSetBaseApiURL function check', () => {
+    expect(service.callSetBaseApiURL()).toBeUndefined()
   });
 });
--- src/app/core/services/core/helper.service.ts
+++ src/app/core/services/core/helper.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from './api.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private apiService: ApiService
+  ) {}
+
+  callSetBaseApiURL() {
+    const urlarray = this.router.url.split('/')
+    const api_url = urlarray.length > 2 ? urlarray[2] : urlarray[1];
+    const api_urls: { [key: string]: string } = {
+      'intacct': environment.si_api_url,
+      'qbd': environment.qbd_api_url,
+      'travelperk': environment.api_url,
+      'bamboohr': environment.api_url,
+      'integration': environment.api_url
+    };
+
+    this.apiService.setBaseApiURL(api_urls[api_url])
+  }
 }
