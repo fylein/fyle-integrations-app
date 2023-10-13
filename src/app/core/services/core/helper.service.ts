e98204daf58589c2c717836811b23b7b7a85b7cb
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2023-10-13 15:20:23 +0530

    workspace apis construction (#192)
    workspace apis construction

--- src/app/core/services/core/helper.service.ts
+++ src/app/core/services/core/helper.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from './api.service';
 import { environment } from 'src/environments/environment';
+import { AppUrlMap } from '../../models/integrations/integrations.model';
+import { AppUrl } from '../../models/enum/enum.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +17,22 @@ export class HelperService {
 
   setBaseApiURL(): void {
     const urlSplit = this.router.url.split('/');
-    const module = urlSplit.length > 2 ? urlSplit[2] : urlSplit[1];
-    const apiUrlMap: { [key: string]: string } = {
-      'intacct': environment.si_api_url,
-      'qbd': environment.qbd_api_url,
-      'travelperk': environment.api_url,
-      'bamboohr': environment.api_url,
-      'integration': environment.api_url
+    const module:AppUrl = (urlSplit.length > 2 ? urlSplit[2] : urlSplit[1]) as AppUrl;
+    const apiUrlMap: AppUrlMap = {
+      [AppUrl.INTACCT]: environment.si_api_url,
+      [AppUrl.QBD]: environment.qbd_api_url,
+      [AppUrl.TRAVELPERK]: environment.api_url,
+      [AppUrl.BAMBOO_HR]: environment.api_url,
+      [AppUrl.GUSTO]: environment.api_url,
+      [AppUrl.SAGE300]: environment.api_url,
+      [AppUrl.INTEGRATION]: environment.api_url
     };
 
     this.apiService.setBaseApiURL(apiUrlMap[module]);
+  }
+
+  getAppName(): string {
+    const appName = this.router.url.split('/')[2];
+    return appName;
   }
 }
--- src/app/core/services/core/workspace.service.ts
+++ src/app/core/services/core/workspace.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import { StorageService } from './storage.service';
+import { Observable } from 'rxjs';
+import { Cacheable } from 'ts-cacheable';
+import { Configuration } from '../../models/db/configuration.model';
+import { AppUrl, IntacctOnboardingState, QBDOnboardingState } from '../../models/enum/enum.model';
+import { IntacctWorkspace } from '../../models/si/db/workspaces.model';
+import { ApiService } from './api.service';
+import { QBDWorkspace } from '../../models/qbd/db/workspaces.model';
+import { HelperService } from './helper.service';
+import { AppUrlMap } from '../../models/integrations/integrations.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkspaceService {
 
-  constructor() { }
+  constructor(
+    private storageService: StorageService,
+    private apiService: ApiService,
+    private helper: HelperService
+  ) {
+    helper.setBaseApiURL();
+   }
+
+  importFyleAttributes(refresh: boolean): Observable<{}> {
+    return this.apiService.post(`/workspaces/${this.getWorkspaceId()}/fyle/import_attributes/`, {refresh});
+  }
+
+  // The return type is made any intentionally, the caller can specify the return type to be aligned with the app
+  getWorkspace(orgId: string): any {
+    return this.apiService.get('/workspaces/', {org_id: orgId});
+  }
+
+  // The return type is made any intentionally, the caller can specify the return type to be aligned with the app
+  postWorkspace(): any {
+    return this.apiService.post('/workspaces/', {});
+  }
+
+  getWorkspaceId(): string {
+    return this.storageService.get('workspaceId');
+  }
+
+  setOnboardingState(onboardingState: IntacctOnboardingState | QBDOnboardingState): void {
+    return this.storageService.set('onboarding-state', onboardingState);
+  }
+
+  getOnboardingState(): QBDOnboardingState | IntacctOnboardingState {
+    const appInitialOnboardingState: AppUrlMap = {
+      [AppUrl.INTACCT]: IntacctOnboardingState.CONNECTION,
+      [AppUrl.GUSTO]: QBDOnboardingState.EXPORT_SETTINGS,
+      [AppUrl.SAGE300]: IntacctOnboardingState.CONNECTION,
+      [AppUrl.BAMBOO_HR]: IntacctOnboardingState.CONNECTION,
+      [AppUrl.QBD]: IntacctOnboardingState.CONNECTION,
+      [AppUrl.TRAVELPERK]: IntacctOnboardingState.CONNECTION,
+      [AppUrl.INTEGRATION]: IntacctOnboardingState.CONNECTION
+    };
+    const onboardingState = this.storageService.get('onboarding-state');
+    return onboardingState ? onboardingState : appInitialOnboardingState[(this.helper.getAppName()) as AppUrl];
+  }
+
+  // The return type is made any intentionally, the caller can specify the return type to be aligned with the app
+  getConfiguration(): any {
+    return this.apiService.get(`/workspaces/${this.getWorkspaceId()}/configuration/`, {});
+  }
 }
