c9149151c48fc65de2ef4efad317ccdeee1af265
anishfyle <anishkumar.s@fyle.in>
2023-10-16 18:04:21 +0530

    Main Module

---
+++ src/app/integrations/sage300/main/main.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MainRoutingModule
+  ]
+})
+export class MainModule { }
