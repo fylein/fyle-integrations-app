9e9aed3096870210d113bf9c8e9a43ccde439dc0
anishfyle <anishkumar.s@fyle.in>
2023-10-17 12:12:02 +0530

    Shared Components for Dashboard

---
+++ src/app/shared/components/dashboard/dashboard-export-section/dashboard-export-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-export-section',
+  templateUrl: './dashboard-export-section.component.html',
+  styleUrls: ['./dashboard-export-section.component.scss']
+})
+export class DashboardExportSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/components/dashboard/dashboard-last-export-section/dashboard-last-export-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-last-export-section',
+  templateUrl: './dashboard-last-export-section.component.html',
+  styleUrls: ['./dashboard-last-export-section.component.scss']
+})
+export class DashboardLastExportSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
