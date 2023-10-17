b0492dd94506a8b2afb3d2db07bd3bcb409f1b7a
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-10-17 14:31:22 +0530

    Sage 300: Dashboard Base Branch (#210)
    * Main Module
    
    * Lint Fixes
    
    * Dashboard: Base Commit
    
    * Sage 300 Dashboard: Renamed

---
+++ src/app/integrations/sage300/sage300-main/sage300-dashboard/sage300-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sage300-dashboard',
+  templateUrl: './sage300-dashboard.component.html',
+  styleUrls: ['./sage300-dashboard.component.scss']
+})
+export class Sage300DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/integrations/sage300/sage300-main/sage300-dashboard/sage300-dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Sage300DashboardRoutingModule } from './sage300-dashboard-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Sage300DashboardRoutingModule
+  ]
+})
+export class Sage300DashboardModule { }
