43f85d9d6046682f8b1491e59c7611c5749740ea
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-10-17 14:12:29 +0530

    Sage 300: Main Module (#209)
    * Main Module
    
    * Lint Fixes
    
    * PR Comments: Module Rename
    
    * Omboarding Rename: Sage300

---
+++ src/app/integrations/sage300/sage300-main/sage300-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sage300-main',
+  templateUrl: './sage300-main.component.html',
+  styleUrls: ['./sage300-main.component.scss']
+})
+export class Sage300MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/integrations/sage300/sage300-onboarding/sage300-onboarding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sage300-onboarding',
+  templateUrl: './sage300-onboarding.component.html',
+  styleUrls: ['./sage300-onboarding.component.scss']
+})
+export class Sage300OnboardingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
