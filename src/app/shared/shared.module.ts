4166f8dab8b28506b9c4d49f6a89b7a0ea85cd47
Fyle <fyle@Fyles-MacBook-Air.local>
2023-10-18 15:08:53 +0530

    onboarding export setting relater sharded folders folder changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -26,20 +26,19 @@ import { PaginatorComponent } from './components/helper/paginator/paginator.comp
 import { MandatoryFieldComponent } from './components/helper/mandatory-field/mandatory-field.component';
 import { MandatoryErrorMessageComponent } from './components/helper/mandatory-error-message/mandatory-error-message.component';
 import { ZeroStateWithIllustrationComponent } from './components/qbd/core/zero-state-with-illustration/zero-state-with-illustration.component';
-import { DateFilterComponent } from './components/qbd/helper/date-filter/date-filter.component';
 import { OnboardingStepperComponent } from './components/qbd/helper/onboarding-stepper/onboarding-stepper.component';
 import { OnboardingSteppersComponent } from './components/helper/onboarding-stepper/onboarding-stepper.component';
 import { ExportSettingComponent } from './components/qbd/configuration/export-setting/export-setting.component';
 import { AdvancedSettingComponent } from './components/qbd/configuration/advanced-setting/advanced-setting.component';
 import { FieldMappingComponent } from './components/qbd/configuration/field-mapping/field-mapping.component';
-import { ConfigurationToggleFieldComponent } from './components/qbd/configuration/configuration-toggle-field/configuration-toggle-field.component';
+import { ConfigurationToggleFieldComponent } from './components/configuration/configuration-toggle-field/configuration-toggle-field.component';
 import { ConfigurationSelectFieldComponent } from './components/configuration/configuration-select-field/configuration-select-field.component';
 import { ConfigurationRadioFieldComponent } from './components/qbd/configuration/configuration-radio-field/configuration-radio-field.component';
 import { ConfigurationStepFooterComponent } from './components/configuration/configuration-step-footer/configuration-step-footer.component';
 import { ConfigurationStepHeaderComponent } from './components/configuration/configuration-step-header/configuration-step-header.component';
 import { EmailMultiSelectFieldComponent } from './components/configuration/email-multi-select-field/email-multi-select-field.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ConfigurationMultiSelectComponent } from './components/qbd/configuration/configuration-multi-select/configuration-multi-select.component';
+import { ConfigurationMultiSelectComponent } from './components/configuration/configuration-multi-select/configuration-multi-select.component';
 import { ConfigurationLabelComponent } from './components/qbd/configuration/configuration-label/configuration-label.component';
 import { CalendarModule } from 'primeng/calendar';
 import { AppLandingPageBodyComponent } from './components/helper/app-landing-page-body/app-landing-page-body.component';
@@ -77,7 +76,6 @@ import { DashboardExportSummarySectionComponent } from './components/dashboard/d
     MandatoryFieldComponent,
     MandatoryErrorMessageComponent,
     ZeroStateWithIllustrationComponent,
-    DateFilterComponent,
     OnboardingStepperComponent,
     OnboardingSteppersComponent,
     ExportSettingComponent,
@@ -146,7 +144,6 @@ import { DashboardExportSummarySectionComponent } from './components/dashboard/d
     MandatoryFieldComponent,
     MandatoryErrorMessageComponent,
     ZeroStateWithIllustrationComponent,
-    DateFilterComponent,
     OnboardingStepperComponent,
     OnboardingSteppersComponent,
     ExportSettingComponent,
