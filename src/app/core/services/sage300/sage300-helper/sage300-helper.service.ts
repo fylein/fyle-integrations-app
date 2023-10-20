1fb5f8faf0ff985128143846570a6be581aeab31
Fyle <fyle@Fyles-MacBook-Air.local>
2023-10-20 15:34:23 +0530

    export setting CCC UI coding

---
+++ src/app/core/services/sage300/sage300-helper/sage300-helper.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { WorkspaceService } from '../../common/workspace.service';
+import { Sage300MappingService } from '../sage300-mapping/sage300-mapping.service';
+import { IntegrationsToastService } from '../../common/integrations-toast.service';
+import { ToastSeverity } from 'src/app/core/models/enum/enum.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Sage300HelperService {
+
+  constructor(
+    private workspaceService: WorkspaceService,
+    private mappingService: Sage300MappingService,
+    private toastService: IntegrationsToastService
+  ) { }
+
+  importAttributes(isRefresh: boolean) {
+    this.workspaceService.importFyleAttributes(isRefresh).subscribe();
+    this.mappingService.importSage300Attributes(isRefresh).subscribe();
+    this.toastService.displayToastMessage(ToastSeverity.SUCCESS, 'Refreshing data dimensions from Sage 300 CRE');
+  }
+}
--- src/app/integrations/sage300/sage300-shared/sage300-export-settings/sage300-export-settings.component.ts
+++ src/app/integrations/sage300/sage300-shared/sage300-export-settings/sage300-export-settings.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { AppName, AutoMapEmployeeOptions, CCCExpenseState, ConfigurationCta, ExpenseGroupedBy, ExpenseState, ExportDateType, FyleField, Sage300ExpenseDate, Sage300ExportTypes } from 'src/app/core/models/enum/enum.model';
 import { Sage300ExportSettingFormOption, sage300ExportSettingGet } from 'src/app/core/models/sage300/sage300-configuration/sage300-export-setting.model';
 import { Sage300ExportSettingService } from 'src/app/core/services/sage300/sage300-configuration/sage300-export-setting.service';
+import { Sage300HelperService } from 'src/app/core/services/sage300/sage300-helper/sage300-helper.service';
 
 @Component({
   selector: 'app-sage300-export-settings',
@@ -120,11 +121,12 @@ export class Sage300ExportSettingsComponent implements OnInit {
   constructor(
     private exportSettingService: Sage300ExportSettingService,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private helperService: Sage300HelperService
   ) { }
 
-  getEmployeeFieldMapping(exportType: ExpenseGroupedBy): string {
-    return exportType[0].toUpperCase() + exportType.substr(1);
+  getFieldMapping(exportType: ExpenseGroupedBy): string {
+    return exportType[0].toUpperCase() + exportType.substr(1).toLowerCase();
   }
 
   private exportSelectionValidator(): ValidatorFn {
@@ -209,20 +211,34 @@ export class Sage300ExportSettingsComponent implements OnInit {
   private setupForm(): void {
     this.isLoading = false;
     this.isOnboarding = this.router.url.includes('onboarding');
-    this.exportSettingService.getSage300ExportSettings().subscribe((exportSettingsResponse: sage300ExportSettingGet) => {
-      this.exportSettings = exportSettingsResponse;
+    // This.exportSettingService.getSage300ExportSettings().subscribe((exportSettingsResponse: sage300ExportSettingGet) => {
+      // This.exportSettings = exportSettingsResponse;
+      this.exportSettings = {
+        "reimbursable_expenses_export_type": "PURCHASE_INVOICE",
+        "reimbursable_expense_state": "PAYMENT_PROCESSING",
+        "reimbursable_expense_date": "LAST_SPENT_AT",
+        "reimbursable_expense_grouped_by": "EXPENSE",
+        "credit_card_expense_export_type": "PURCHASE_INVOICE",
+        "credit_card_expense_state": "CLOSED",
+        "credit_card_expense_grouped_by": "EXPENSE",
+        "credit_card_expense_date": "APPROVED_AT",
+        "default_ccc_account_name": "credit card account",
+        "default_ccc_account_id": "12312",
+        "default_vendor_name": "Nilesh",
+        "default_vendor_id": "123"
+      };
       this.exportSettingForm = this.formBuilder.group({
-        reimbursableExpenses: [this.exportSettings?.reimbursable_expenses_export_type ? true : false, this.exportSelectionValidator()],
+        reimbursableExpense: [this.exportSettings?.reimbursable_expenses_export_type ? true : false, this.exportSelectionValidator()],
         reimbursableExportType: [this.exportSettings?.reimbursable_expenses_export_type ? this.exportSettings.reimbursable_expenses_export_type : null],
         reimbursableExpenseState: [this.exportSettings?.reimbursable_expense_state ? this.exportSettings?.reimbursable_expense_state : null],
         reimbursableExportDate: [this.exportSettings?.reimbursable_expense_date ? this.exportSettings?.reimbursable_expense_date : null],
         reimbursableExportGroup: [this.exportSettings?.reimbursable_expense_grouped_by ? this.exportSettings?.reimbursable_expense_grouped_by: null],
         reimbursableEmployeeType: [null],
         reimbursableEmployeeMappingType: [null],
-        cccExpenses: [this.exportSettings?.credit_card_expense_export_type ? true : false, this.exportSelectionValidator()],
+        creditCardExpense: [this.exportSettings?.credit_card_expense_export_type ? true : false, this.exportSelectionValidator()],
         cccExportType: [this.exportSettings?.credit_card_expense_export_type ? this.exportSettings.credit_card_expense_export_type : null],
         cccExpenseState: [this.exportSettings?.credit_card_expense_state ? this.exportSettings?.credit_card_expense_state : null],
-        cccExpenseDate: [this.exportSettings?.credit_card_expense_date ? this.exportSettings?.credit_card_expense_date : null],
+        cccExportDate: [this.exportSettings?.credit_card_expense_date ? this.exportSettings?.credit_card_expense_date : null],
         cccExportGroup: [this.exportSettings?.credit_card_expense_grouped_by ? this.exportSettings?.credit_card_expense_grouped_by: null],
         defaultCCCAccountName: [this.exportSettings?.default_ccc_account_name ? this.exportSettings?.default_ccc_account_name : null],
         defaultCCCAccountId: [this.exportSettings?.default_ccc_account_id ? this.exportSettings?.default_ccc_account_id : null],
@@ -231,11 +247,15 @@ export class Sage300ExportSettingsComponent implements OnInit {
       });
       this.setCustomValidatorsAndWatchers();
       this.isLoading = false;
-    });
+    // });
   }
 
   save() {
     // Will be added here soon
+  }
+
+  refreshDimensions(isRefresh: boolean) {
+    this.helperService.importAttributes(isRefresh);
   }
 
   ngOnInit(): void {
