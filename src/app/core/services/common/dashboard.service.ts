050751d7b894b5337b32f54b84b490b0464a873f
anishfyle <anishkumar.s@fyle.in>
2023-10-25 12:08:10 +0530

    Account Export Summary: Common Dashboard Service

--- src/app/core/services/common/dashboard.service.ts
+++ src/app/core/services/common/dashboard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { WorkspaceService } from './workspace.service';
+import { AccountingExportSummary } from '../../models/db/accounting-export-summary.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
+  workspaceId: string = this.workspaceService.getWorkspaceId();
+
+  constructor(
+    private apiService: ApiService,
+    private workspaceService: WorkspaceService
+  ) { }
+
+  getAccountingExportSummary(): Observable<AccountingExportSummary> {
+    return this.apiService.get(`/workspaces/${this.workspaceId}/accounting_exports/summary`, {});
+  }
 }
