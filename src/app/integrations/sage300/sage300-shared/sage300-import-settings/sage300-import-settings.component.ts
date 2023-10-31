0f694cdedf50f6d7a168a302e80ffd28917d3eea
Fyle <fyle@Fyles-MacBook-Air.local>
2023-10-31 16:04:40 +0530

    Import setting base component creations

---
+++ src/app/integrations/sage300/sage300-shared/sage300-import-settings/sage300-import-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sage300-import-settings',
+  templateUrl: './sage300-import-settings.component.html',
+  styleUrls: ['./sage300-import-settings.component.scss']
+})
+export class Sage300ImportSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
