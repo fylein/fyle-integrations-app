9f7e7f8bbb36212d1313fdd8b1058a0de91f2646
anishfyle <anishkumar.s@fyle.in>
2023-10-31 11:44:52 +0530

    Sage 300: Error Section - Shared

--- src/app/shared/components/dashboard/dashboard-error-section/dashboard-error-section.component.ts
+++ src/app/shared/components/dashboard/dashboard-error-section/dashboard-error-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-error-section',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-error-section.component.scss']
 })
 export class DashboardErrorSectionComponent implements OnInit {
+
+  @Input() isLoading: boolean;
 
   constructor() { }
 
