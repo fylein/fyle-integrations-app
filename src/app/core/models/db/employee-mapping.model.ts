b9210534fc4450c4f5707fd15ecdf342a70d1c82
anishfyle <anishkumar.s@fyle.in>
2023-11-07 17:15:10 +0530

    Sage 300: Mapping Models

---
+++ src/app/core/models/db/employee-mapping.model.ts
@@ -0,0 +1,16 @@
+import { MappingDestination } from "./mapping-destination.model";
+import { MappingSource } from "./mapping-source.model";
+import { MappingResult } from "./mapping.model";
+
+export type EmployeeMapping = {
+    id: number;
+    source_employee: MappingSource;
+    destination_employee?: MappingDestination;
+    destination_vendor?: MappingDestination;
+    destination_card_account?: MappingDestination;
+    workspace: number;
+  };
+
+  export interface MappingIntacct extends MappingResult {
+    employeemapping: EmployeeMapping;
+  }
--- src/app/core/models/db/mapping-source.model.ts
+++ src/app/core/models/db/mapping-source.model.ts
@@ -1,17 +1,6 @@
 import { ExpenseAttributeDetail } from "./expense-attribute.model";
 
-export type IntegrationField = {
-    attribute_type: string,
-    display_name: string
-}
-
-export type FyleField = {
-    attribute_type: string,
-    display_name: string,
-    is_dependent: boolean
-}
-
-export type MappingResult = {
+export type MappingSource = {
     id: number;
     attribute_type?: string;
     display_name?: string;
