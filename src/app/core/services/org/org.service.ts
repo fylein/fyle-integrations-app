1bb1aa7b2b52289d6aa5eaa258afa7edfaa1a4e2
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-11-22 12:12:54 +0530

    Revert API url path changes commit (#278)

--- src/app/core/services/org/org.service.ts
+++ src/app/core/services/org/org.service.ts
@@ -3,11 +3,10 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { Cacheable } from 'ts-cacheable';
 import { EmailOption } from '../../models/bamboo-hr/bamboo-hr.model';
-import { AppName, AppUrl } from '../../models/enum/enum.model';
+import { AppName } from '../../models/enum/enum.model';
 import { GeneratedToken, Org } from '../../models/org/org.model';
 import { ApiService } from '../common/api.service';
 import { StorageService } from '../common/storage.service';
-import { HelperService } from '../common/helper.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +16,8 @@ export class OrgService {
   constructor(
     private sanitizer: DomSanitizer,
     private apiService: ApiService,
-    private storageService: StorageService,
-    private helper : HelperService
-  ) {
-    helper.setBaseApiURL(AppUrl.INTEGRATION);
-   }
+    private storageService: StorageService
+  ) { }
 
   getOrgId(): string {
     return this.storageService.get('orgId');
