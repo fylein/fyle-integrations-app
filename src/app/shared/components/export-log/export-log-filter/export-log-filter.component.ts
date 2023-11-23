66e28d201d0f26315d76d6ce54a1d9b19c924d0b
anishfyle <anishkumar.s@fyle.in>
2023-11-23 11:41:37 +0530

    Export Log Filter

--- src/app/shared/components/export-log/export-log-filter/export-log-filter.component.ts
+++ src/app/shared/components/export-log/export-log-filter/export-log-filter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { DateFilter } from 'src/app/core/models/qbd/misc/date-filter.model';
 
 @Component({
   selector: 'app-export-log-filter',
@@ -7,7 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExportLogFilterComponent implements OnInit {
 
+  @Input() exportLogForm: FormGroup;
+
+  @Input() isSearchFocused: boolean
+
+  @Input() isDateFieldFocused: boolean;
+
+  @Output() filterTableChange = new EventEmitter<any>();
+
+  isCalendarVisible: boolean;
+
+  presentDate = new Date().toLocaleDateString();
+
+  dateOptions: DateFilter[] = [
+    {
+      dateRange: 'This Month',
+      startDate: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+      endDate: new Date()
+    },
+    {
+      dateRange: 'This Week',
+      startDate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - new Date().getDay()),
+      endDate: new Date()
+    },
+    {
+      dateRange: 'Today',
+      startDate: new Date(),
+      endDate: new Date()
+    },
+    {
+      dateRange: new Date().toLocaleDateString(),
+      startDate: new Date(),
+      endDate: new Date()
+    }
+  ];
+
   constructor() { }
+
+  filterTable(event: any) {
+    this.filterTableChange.emit(event);
+  }
+
+  dropDownWatcher() {
+    if (this.exportLogForm.controls.dateRange.value !== this.dateOptions[3].dateRange) {
+      this.isCalendarVisible = false;
+    }
+    this.isCalendarVisible = true;
+  }
+
+  removeFilter(formField: AbstractControl) {
+    (formField as FormGroup).reset();
+    event?.stopPropagation();
+  }
+
+  showCalendar(event: Event) {
+    event.stopPropagation();
+    this.isCalendarVisible = true;
+  }
+
+  getDates() {
+    this.dateOptions[3].dateRange = this.exportLogForm.value.start[0].toLocaleDateString() + '-' + this.exportLogForm.value.start[1].toLocaleDateString();
+    this.dateOptions[3].startDate = this.exportLogForm.value.start[0];
+    this.dateOptions[3].endDate = this.exportLogForm.value.start[1];
+    this.presentDate = this.dateOptions[3].dateRange;
+    this.exportLogForm.controls.dateRange.patchValue(this.dateOptions[3]);
+  }
 
   ngOnInit(): void {
   }
