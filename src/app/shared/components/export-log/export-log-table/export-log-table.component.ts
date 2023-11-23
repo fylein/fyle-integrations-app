5c40c8d2f561ff68bf8519a1eb87e9330e44bf2f
anishfyle <anishkumar.s@fyle.in>
2023-11-23 11:47:00 +0530

    Export Log Table

--- src/app/shared/components/export-log/export-log-table/export-log-table.component.ts
+++ src/app/shared/components/export-log/export-log-table/export-log-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ExpenseGroupList } from 'src/app/core/models/si/db/expense-group.model';
+import { Expense } from 'src/app/core/models/si/db/expense.model';
 
 @Component({
   selector: 'app-export-log-table',
@@ -6,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./export-log-table.component.scss']
 })
 export class ExportLogTableComponent implements OnInit {
+  
+  @Input() filteredExpenseGroups: ExpenseGroupList [];
+
+  clickedExportLogIndex: number = 0;
+
+  expenses: Expense [] = [];
+
+  visible: boolean = false;
 
   constructor() { }
+
+  displayChildTable(index: number) {
+    this.clickedExportLogIndex = index;
+    this.expenses = this.filteredExpenseGroups[this.clickedExportLogIndex].expenses;
+    this.visible = true;
+  }
+
+  openUrl(event: Event, url: string) {
+    window.open(url, '_blank');
+    event.stopPropagation();
+  }
 
   ngOnInit(): void {
   }
