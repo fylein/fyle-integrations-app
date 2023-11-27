37fb4251dbf239e6be38f17090b3c3f3fd437208
anishfyle <anishkumar.s@fyle.in>
2023-11-27 14:05:56 +0530

    Skipped Export Log Table

---
+++ src/app/shared/components/export-log/skipped-export-log-table/skipped-export-log-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skipped-export-log-table',
+  templateUrl: './skipped-export-log-table.component.html',
+  styleUrls: ['./skipped-export-log-table.component.scss']
+})
+export class SkippedExportLogTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -81,6 +81,7 @@ import { MappingCardHeaderComponent } from './components/helper/mapping/mapping-
 import { ExportLogTableComponent } from './components/export-log/export-log-table/export-log-table.component';
 import { ExportLogFilterComponent } from './components/export-log/export-log-filter/export-log-filter.component';
 import { ExportLogChildTableDialogComponent } from './components/export-log/export-log-dialog/export-log-child-table-dialog.component';
+import { SkippedExportLogTableComponent } from './components/export-log/skipped-export-log-table/skipped-export-log-table.component';
 
 
 @NgModule({
@@ -144,7 +145,8 @@ import { ExportLogChildTableDialogComponent } from './components/export-log/expo
     MappingCardHeaderComponent,
     ExportLogTableComponent,
     ExportLogFilterComponent,
-    ExportLogChildTableDialogComponent
+    ExportLogChildTableDialogComponent,
+    SkippedExportLogTableComponent
   ],
   imports: [
     CommonModule,
