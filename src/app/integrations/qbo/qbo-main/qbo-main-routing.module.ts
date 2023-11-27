1611c82d020f6837ad6c7615c8da94c91bfb5d72
ashwin1111 <winash1998@gmail.com>
2023-11-27 19:30:16 +0530

    QBO Routes

--- src/app/integrations/qbo/qbo-main/qbo-main-routing.module.ts
+++ src/app/integrations/qbo/qbo-main/qbo-main-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QboMainComponent } from './qbo-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: QboMainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./qbo-dashboard/qbo-dashboard.module').then(m => m.QboDashboardModule)
+      },
+      {
+        path: 'export_log',
+        loadChildren: () => import('./qbo-export-log/qbo-export-log.module').then(m => m.QboExportLogModule)
+      },
+      {
+        path: 'mapping',
+        loadChildren: () => import('./qbo-mapping/qbo-mapping.module').then(m => m.QboMappingModule)
+      },
+      {
+        path: 'configuration',
+        loadChildren: () => import('./qbo-configuration/qbo-configuration.module').then(m => m.QboConfigurationModule)
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/integrations/qbo/qbo-onboarding/qbo-onboarding-routing.module.ts
+++ src/app/integrations/qbo/qbo-onboarding/qbo-onboarding-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QboOnboardingComponent } from './qbo-onboarding.component';
+import { QboOnboardingLandingComponent } from './qbo-onboarding-landing/qbo-onboarding-landing.component';
+import { QboOnboardingConnectorComponent } from './qbo-onboarding-connector/qbo-onboarding-connector.component';
+import { QboOnboardingEmployeeSettingsComponent } from './qbo-onboarding-employee-settings/qbo-onboarding-employee-settings.component';
+import { QboOnboardingExportSettingsComponent } from './qbo-onboarding-export-settings/qbo-onboarding-export-settings.component';
+import { QboOnboardingImportSettingsComponent } from './qbo-onboarding-import-settings/qbo-onboarding-import-settings.component';
+import { QboOnboardingAdvancedSettingsComponent } from './qbo-onboarding-advanced-settings/qbo-onboarding-advanced-settings.component';
+import { QboOnboardingDoneComponent } from './qbo-onboarding-done/qbo-onboarding-done.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: QboOnboardingComponent,
+    children: [
+      {
+        path: 'landing',
+        component: QboOnboardingLandingComponent
+      },
+      {
+        path: 'connector',
+        component: QboOnboardingConnectorComponent
+      },
+      {
+        path: 'employee_settings',
+        component: QboOnboardingEmployeeSettingsComponent
+      },
+      {
+        path: 'export_settings',
+        component: QboOnboardingExportSettingsComponent
+      },
+      {
+        path: 'import_settings',
+        component: QboOnboardingImportSettingsComponent
+      },
+      {
+        path: 'advanced_settings',
+        component: QboOnboardingAdvancedSettingsComponent
+      },
+      {
+        path: 'done',
+        component: QboOnboardingDoneComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
