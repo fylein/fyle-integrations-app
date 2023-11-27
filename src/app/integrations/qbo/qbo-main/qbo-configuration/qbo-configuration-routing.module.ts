b9d6e31053130bffee6a6119b12cb60f8b593643
ashwin1111 <winash1998@gmail.com>
2023-11-27 19:33:30 +0530

    config module routes

--- src/app/integrations/qbo/qbo-main/qbo-configuration/qbo-configuration-routing.module.ts
+++ src/app/integrations/qbo/qbo-main/qbo-configuration/qbo-configuration-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QboConfigurationComponent } from './qbo-configuration.component';
+import { QboEmployeeSettingsComponent } from '../../qbo-shared/qbo-employee-settings/qbo-employee-settings.component';
+import { QboExportSettingsComponent } from '../../qbo-shared/qbo-export-settings/qbo-export-settings.component';
+import { QboImportSettingsComponent } from '../../qbo-shared/qbo-import-settings/qbo-import-settings.component';
+import { QboAdvancedSettingsComponent } from '../../qbo-shared/qbo-advanced-settings/qbo-advanced-settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: QboConfigurationComponent,
+    children: [
+      {
+        path: 'employee_settings',
+        component: QboEmployeeSettingsComponent
+      },
+      {
+        path: 'export_settings',
+        component: QboExportSettingsComponent
+      },
+      {
+        path: 'import_settings',
+        component: QboImportSettingsComponent
+      },
+      {
+        path: 'advanced_settings',
+        component: QboAdvancedSettingsComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
