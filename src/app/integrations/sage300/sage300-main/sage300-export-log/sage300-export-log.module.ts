358892aafc4721c6d711e35353f570903aa459d9
anishfyle <anishkumar.s@fyle.in>
2023-11-28 15:48:01 +0530

    Minor Fixes: Added Export Log in Main Routing

--- src/app/integrations/sage300/sage300-main/sage300-export-log/sage300-export-log.module.ts
+++ src/app/integrations/sage300/sage300-main/sage300-export-log/sage300-export-log.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Sage300ExportLogRoutingModule } from './sage300-export-log-routing.module';
-
-
 import { Sage300ExportLogComponent } from './sage300-export-log.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { Sage300CompleteExportLogComponent } from './sage300-complete-export-log/sage300-complete-export-log.component';
