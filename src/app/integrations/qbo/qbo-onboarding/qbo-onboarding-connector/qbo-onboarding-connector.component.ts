a4fb0ac36eb61c443da5b92669efeef04ec3cb7c
ashwin1111 <winash1998@gmail.com>
2023-11-30 18:52:26 +0530

    QBO Connector - Base

--- src/app/integrations/qbo/qbo-onboarding/qbo-onboarding-connector/qbo-onboarding-connector.component.ts
+++ src/app/integrations/qbo/qbo-onboarding/qbo-onboarding-connector/qbo-onboarding-connector.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { brandingConfig, brandingKbArticles } from 'src/app/branding/branding-config';
+import { BrandingConfiguration } from 'src/app/core/models/branding/branding-configuration.model';
+import { ConfigurationCta } from 'src/app/core/models/enum/enum.model';
+import { OnboardingStepper } from 'src/app/core/models/misc/onboarding-stepper.model';
+import { QBOOnboaringClass } from 'src/app/core/models/qbo/qbo-configuration/qbo-onboarding.model';
+import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
 
 @Component({
   selector: 'app-qbo-onboarding-connector',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QboOnboardingConnectorComponent implements OnInit {
 
-  constructor() { }
+  onboardingSteps: OnboardingStepper[] = new QBOOnboaringClass().getOnboardingSteps('Connect to QuickBooks Online', this.workspaceService.getOnboardingState());
+
+  isLoading: boolean = true;
+
+  redirectLink: string = brandingKbArticles.onboardingArticles.QBO.CONNECTOR;
+
+  brandingConfig: BrandingConfiguration = brandingConfig;
+
+  ConfigurationCtaText = ConfigurationCta;
+
+  saveInProgress: boolean = false;
+
+  constructor(
+    private workspaceService: WorkspaceService
+  ) { }
+
+  save(): void {
+    // TODO
+  }
+
+  private setupPage(): void {
+    this.isLoading = false;
+  }
 
   ngOnInit(): void {
+    this.setupPage();
   }
 
 }
