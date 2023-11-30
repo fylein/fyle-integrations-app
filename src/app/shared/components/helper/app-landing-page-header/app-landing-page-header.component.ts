ecb6212ba7af0f3b3dfce12fc49a0dbe8eb00f56
Fyle <fyle@Fyles-MacBook-Air.local>
2023-11-30 11:54:50 +0530

    PR commit fix

--- src/app/shared/components/helper/app-landing-page-header/app-landing-page-header.component.ts
+++ src/app/shared/components/helper/app-landing-page-header/app-landing-page-header.component.ts
@@ -51,9 +51,9 @@ export class AppLandingPageHeaderComponent implements OnInit {
 
   @Input() logoWidth: string = '140px';
 
-  @Input() logoStlyeClasses: string = 'tw-py-10-px tw-px-20-px';
+  @Input() logoStyleClasses: string = 'tw-py-10-px tw-px-20-px';
 
-  @Input() logoSectionStlyeClasses: string = 'tw-rounded-4-px tw-border-1-px tw-border-box-color tw-bg-white tw-w-176-px';
+  @Input() logoSectionStyleClasses: string = 'tw-rounded-4-px tw-border-1-px tw-border-box-color tw-bg-white tw-w-176-px';
 
   readonly brandingConfig = brandingConfig;
 
