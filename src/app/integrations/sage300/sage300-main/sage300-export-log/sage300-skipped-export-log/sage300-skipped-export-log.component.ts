fed464c3e73e911902e8f156e3b7192fdcfa7938
anishfyle <anishkumar.s@fyle.in>
2023-12-01 12:09:41 +0530

    Skip Export GET and Model Changes

--- src/app/integrations/sage300/sage300-main/sage300-export-log/sage300-skipped-export-log/sage300-skipped-export-log.component.ts
+++ src/app/integrations/sage300/sage300-main/sage300-export-log/sage300-skipped-export-log/sage300-skipped-export-log.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { AccountingExportClass, AccountingExportList, SkippedAccountingExportClass } from 'src/app/core/models/db/accounting-export.model';
+import { SkippedAccountingExportClass } from 'src/app/core/models/db/accounting-export.model';
 import { SkipExportList } from 'src/app/core/models/si/db/expense-group.model';
 
 @Component({
@@ -34,6 +34,47 @@ export class Sage300SkippedExportLogComponent implements OnInit {
     this.filteredAccountingExports = this.accountingExports.filter((group: SkipExportList) => {
       return SkippedAccountingExportClass.getfilteredSkippedAccountingExports(query, group);
     });
+  }
+
+  getSkippedAccountingExports(limit: number, offset:number) {
+    this.isLoading = true;
+    const expenseGroups: SkipExportList[] = [];
+
+    if (this.limit !== limit) {
+      this.paginatorService.storePageSize(PaginatorPage.EXPORT_LOG, limit);
+    }
+
+    return this.exportLogService.getSkipExportLogs(limit, offset).subscribe((skippedExpenses: SkipExportLogResponse) => {
+      if (!this.isDateSelected) {
+        this.totalCount = skippedExpenses.count;
+      }
+      skippedExpenses.results.forEach((skippedExpenses: SkipExportLog) => {
+        expenseGroups.push({
+          updated_at: skippedExpenses.updated_at,
+          claim_number: skippedExpenses.claim_number,
+          employee: [skippedExpenses.employee_name, skippedExpenses.employee_email],
+          expenseType: skippedExpenses.fund_source === 'PERSONAL' ? 'Reimbursable' : 'Corporate Card',
+          fyleUrl: `${environment.fyle_app_url}/app/main/#/view_expense/${skippedExpenses.expense_id}`
+        });
+      });
+      this.filteredExpenseGroups = expenseGroups;
+      this.expenseGroups = [...this.filteredExpenseGroups];
+      this.isLoading = false;
+    });
+  }
+
+  pageSizeChanges(limit: number): void {
+    this.isLoading = true;
+    this.limit = limit;
+    this.currentPage = 1;
+    this.getSkippedAccountingExports(limit, this.offset);
+  }
+
+  pageChanges(offset: number): void {
+    this.isLoading = true;
+    this.offset = offset;
+    this.currentPage = Math.ceil(offset / this.limit) + 1;
+    this.getSkippedAccountingExports(this.limit, offset);
   }
 
   ngOnInit(): void {
