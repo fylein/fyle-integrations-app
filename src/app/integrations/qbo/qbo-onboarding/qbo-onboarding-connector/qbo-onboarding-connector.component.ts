99da274e9d16e8151eb88e6d1d8bbc3daa75f59d
ashwin1111 <winash1998@gmail.com>
2023-12-01 11:49:33 +0530

    Connect QBO

--- src/app/integrations/qbo/qbo-onboarding/qbo-onboarding-connector/qbo-onboarding-connector.component.ts
+++ src/app/integrations/qbo/qbo-onboarding/qbo-onboarding-connector/qbo-onboarding-connector.component.ts
@@ -56,6 +56,10 @@ export class QboOnboardingConnectorComponent implements OnInit {
     private workspaceService: WorkspaceService
   ) { }
 
+  connectQbo(): void {
+    // TODO
+  }
+
   save(): void {
     // TODO
   }
