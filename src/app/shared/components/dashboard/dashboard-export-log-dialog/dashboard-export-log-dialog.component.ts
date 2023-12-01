e0cc9b1d324fdd6cc54aa4e8d12a58dffe514dac
anishfyle <anishkumar.s@fyle.in>
2023-12-01 20:55:27 +0530

    Correct Shared Component for Export Log Dialog

--- src/app/shared/components/dashboard/dashboard-export-log-dialog/dashboard-export-log-dialog.component.ts
+++ src/app/shared/components/dashboard/dashboard-export-log-dialog/dashboard-export-log-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AccountingExportList } from 'src/app/core/models/db/accounting-export.model';
+import { WindowService } from 'src/app/core/services/common/window.service';
 
 @Component({
   selector: 'app-dashboard-export-log-dialog',
@@ -7,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardExportLogDialogComponent implements OnInit {
 
-  constructor() { }
+  @Input() exportLogHeader: string;
+
+  @Input() isExportLogVisible: boolean;
+
+  @Input() accountingExports: AccountingExportList[];
+
+  @Input() isExportLogFetchInProgress: boolean;
+
+  readonly dummyExpenseGroupList: AccountingExportList[] = [{
+    exportedAt: new Date(),
+    employee: ['a', 'b'],
+    expenseType: 'Corporate Card',
+    referenceNumber: '123',
+    exportedAs: 'a',
+    integrationUrl: 'a',
+    fyleUrl: 'a',
+    expenses: []
+  }];
+
+  constructor(
+    private windowService: WindowService
+  ) { }
+
+  openUrl(event: any, url: string) {
+    this.windowService.openInNewTab(url);
+    event.stopPropagation();
+  }
 
   ngOnInit(): void {
   }
