e2826af47a7e70d7cae0b4636f90d8f332b7a725
anishfyle <anishkumar.s@fyle.in>
2023-12-04 12:49:15 +0530

    Appname as input

--- src/app/shared/components/dashboard/dashboard-export-log-dialog/dashboard-export-log-dialog.component.ts
+++ src/app/shared/components/dashboard/dashboard-export-log-dialog/dashboard-export-log-dialog.component.ts
@@ -18,7 +18,7 @@ export class DashboardExportLogDialogComponent implements OnInit {
 
   @Input() isExportLogFetchInProgress: boolean;
 
-  appName: AppName = AppName.SAGE300;
+  @Input() appName: AppName;
 
   readonly dummyExpenseGroupList: AccountingExportList[] = [{
     exportedAt: new Date(),
