9a9a96bd22f31e713ae47c72c07b1d2c782b573e
anishfyle <anishkumar.s@fyle.in>
2023-12-06 02:54:08 +0530

    Lint Fix

--- src/app/shared/components/helper/shimmers/shimmers.component.ts
+++ src/app/shared/components/helper/shimmers/shimmers.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ShimmersComponent implements OnInit {
 
   @Input() exportLogHeader: string;
-  
+
   @Input() isExportLogFetchInProgress: boolean;
 
   constructor() { }
