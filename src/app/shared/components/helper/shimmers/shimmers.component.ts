110ef51fc792c64f28ca14e7529b6fa7c20b61e0
anishfyle <anishkumar.s@fyle.in>
2023-12-06 02:48:42 +0530

    Shimmers Component

---
+++ src/app/shared/components/helper/shimmers/shimmers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shimmers',
+  templateUrl: './shimmers.component.html',
+  styleUrls: ['./shimmers.component.scss']
+})
+export class ShimmersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
