1786cf9f9da71c1a0637296201ab857516c64a34
anishfyle <anishkumar.s@fyle.in>
2023-12-06 17:23:49 +0530

    Dashboard Business Logic

--- src/app/integrations/business-central/business-central-main/business-central-dashboard/business-central-dashboard.component.ts
+++ src/app/integrations/business-central/business-central-main/business-central-dashboard/business-central-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountingExportSummary } from 'src/app/core/models/db/accounting-export-summary.model';
+import { DestinationFieldMap } from 'src/app/core/models/db/dashboard.model';
+import { AccountingGroupedErrorStat, AccountingGroupedErrors } from 'src/app/core/models/db/error.model';
+import { AccountingErrorType, AppName } from 'src/app/core/models/enum/enum.model';
 
 @Component({
   selector: 'app-business-central-dashboard',
@@ -6,6 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-central-dashboard.component.scss']
 })
 export class BusinessCentralDashboardComponent implements OnInit {
+
+  isLoading: boolean = true;
+
+  appName: AppName = AppName.BUSINESS_CENTRAL;
+
+  isImportInProgress: boolean = true;
+
+  isExportInProgress: boolean = false;
+
+  exportableAccountingExportIds: number[] = [];
+
+  failedExpenseGroupCount: number = 0;
+
+  exportProgressPercentage: number = 0;
+
+  accountingExportSummary: AccountingExportSummary;
+
+  processedCount: number = 0;
+
+  errors: AccountingGroupedErrors;
+
+  groupedErrorStat: AccountingGroupedErrorStat = {
+    [AccountingErrorType.EMPLOYEE_MAPPING]: null,
+    [AccountingErrorType.CATEGORY_MAPPING]: null
+  };
+
+  readonly destinationFieldMap : DestinationFieldMap = {
+    'EMPLOYEE': 'VENDOR',
+    'CATEGORY': 'ACCOUNT'
+  };
 
   constructor() { }
 
