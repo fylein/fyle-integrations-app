c6ee3d35cdc9dee9dc106cd75c235a238d6b39b3
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-12-14 11:28:05 +0530

    Sage300: Dashboard Base Component Model Fixes (#367)
    * Sage300: Dashboard Types
    
    * removed unused import

--- src/app/core/models/sage300/db/sage300-accounting-export.model.ts
+++ src/app/core/models/sage300/db/sage300-accounting-export.model.ts
@@ -1,4 +1,3 @@
-import { BusinessCentralAccountingExport } from "../../business-central/db/business-central-accounting-export.model";
 import { AccountingExport } from "../../db/accounting-export.model";
 import { PaginatedResponse } from "../../db/paginated-response.model";
 
@@ -10,5 +9,5 @@ export interface Sage300AccountingExport extends AccountingExport {
 }
 
 export interface Sage300AccountingExportResponse extends PaginatedResponse {
-    results: Sage300AccountingExport[] | BusinessCentralAccountingExport[];
+    results: Sage300AccountingExport[];
 }
