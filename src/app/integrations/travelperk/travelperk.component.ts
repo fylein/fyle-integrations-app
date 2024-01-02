5663c3223ad7a4b1e1910b9f7e31b55f906659bd
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2024-01-02 17:23:51 +0530

    replace workato oauth connection with inhouse connection (#422)

--- src/app/integrations/travelperk/travelperk.component.ts
+++ src/app/integrations/travelperk/travelperk.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { concat, toArray } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ToastSeverity } from 'src/app/core/models/enum/enum.model';
 import { AppName } from 'src/app/core/models/enum/enum.model';
 import { Org } from 'src/app/core/models/org/org.model';
@@ -9,13 +9,15 @@ import { IntegrationsToastService } from 'src/app/core/services/common/integrati
 import { TravelperkService } from 'src/app/core/services/travelperk/travelperk.service';
 import { environment } from 'src/environments/environment';
 import { brandingConfig, brandingKbArticles } from 'src/app/branding/branding-config';
+import { WindowService } from 'src/app/core/services/common/window.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-travelperk',
   templateUrl: './travelperk.component.html',
   styleUrls: ['./travelperk.component.scss']
 })
-export class TravelperkComponent implements OnInit {
+export class TravelperkComponent implements OnInit, OnDestroy {
   brandingKbArticles = brandingKbArticles;
 
   AppName = AppName;
@@ -34,79 +36,23 @@ export class TravelperkComponent implements OnInit {
 
   org: Org = this.orgService.getCachedOrg();
 
+  private routeWatcher$: Subscription;
+
   readonly brandingConfig = brandingConfig;
 
   constructor(
+    private route: ActivatedRoute,
     private travelperkService: TravelperkService,
     private orgService: OrgService,
-    private toastService: IntegrationsToastService
+    private toastService: IntegrationsToastService,
+    private windowService: WindowService
   ) { }
-
-  private syncData() {
-    const syncData = [];
-
-    if (!this.org?.managed_user_id) {
-      syncData.push(this.orgService.createWorkatoWorkspace());
-    }
-
-    if (!this.travelperkData || !this.travelperkData?.folder_id) {
-      syncData.push(this.travelperkService.createFolder());
-    }
-
-    if (!this.travelperkData || !this.travelperkData?.package_id) {
-      syncData.push(this.travelperkService.uploadPackage());
-    }
-
-    if (!this.org?.is_fyle_connected) {
-      syncData.push(this.orgService.connectFyle(AppName.TRAVELPERK));
-    }
-
-    if (!this.travelperkData?.travelperk_connection_id) {
-      syncData.push(this.travelperkService.connectTravelperk());
-    }
-
-    if (!this.travelperkData?.is_s3_connected) {
-      syncData.push(this.travelperkService.connectAwsS3());
-    }
-
-    syncData.push(this.orgService.getOrgs(this.org.fyle_org_id));
-    return syncData;
-  }
-
-  private setConnectionStatus(): void {
-    this.travelperkService.getConfigurations().subscribe((configuration) => {
-      this.isIntegrationConnected = configuration.is_recipe_enabled;
-    });
-  }
-
-  private setupTravelperk() {
-    const syncData = this.syncData();
-
-    this.isTravelperkSetupInProgress = true;
-    concat(...syncData).pipe(
-      toArray()
-    ).subscribe((responses) => {
-      responses.forEach((response: any) => {
-        if (response?.hasOwnProperty('managed_user_id') ) {
-          this.org.managed_user_id = response.managed_user_id;
-        }
-      });
-      this.isLoading = false;
-      this.isTravelperkSetupInProgress = false;
-      this.setConnectionStatus();
-    }, () => {
-      this.isLoading = false;
-      this.isTravelperkSetupInProgress = false;
-      this.showErrorScreen = true;
-    });
-  }
 
   private setupPage(): void {
     this.travelperkService.getTravelperkData().subscribe((travelperkData : Travelperk) => {
       this.travelperkData = travelperkData;
-      this.setupTravelperk();
-    }, () => {
-      this.setupTravelperk();
+      this.isIntegrationConnected = travelperkData.is_travelperk_connected;
+      this.isLoading = false;
     });
   }
 
@@ -120,30 +66,28 @@ export class TravelperkComponent implements OnInit {
   connectTravelperk(): void {
     this.isConnectionInProgress = true;
     const url = `${environment.travelperk_base_url}/oauth2/authorize?client_id=${environment.travelperk_client_id}&redirect_uri=${environment.travelperk_redirect_uri}&scope=expenses:read&response_type=code&state=${environment.production ? 'none' : 'travelperk_local_redirect'}`;
-    const popup = window.open(url, 'popup', 'popup=true, width=500, height=800, left=500');
 
-    const activePopup = setInterval(() => {
-      if (popup?.location?.href?.includes('code')) {
-        const callbackURL = popup?.location.href;
-        const code = callbackURL.split('code=')[1].split('&')[0];
-
-        this.travelperkService.connect(code).subscribe(() => {
-          this.toastService.displayToastMessage(ToastSeverity.SUCCESS, 'Connected Travelperk successfully');
-          this.isIntegrationConnected = true;
-          this.isConnectionInProgress = false;
-        });
-
-        popup.close();
-      } else if (!popup || !popup.closed) {
-        return;
-      }
-
-      clearInterval(activePopup);
-    }, 500);
+    this.windowService.redirect(url);
   }
 
   ngOnInit(): void {
-    this.setupPage();
+    this.routeWatcher$ = this.route.queryParams.subscribe(params => {
+      if (params.code) {
+        this.travelperkService.connect(params.code).subscribe(() => {
+            this.toastService.displayToastMessage(ToastSeverity.SUCCESS, 'Connected Travelperk successfully');
+            this.isIntegrationConnected = true;
+            this.isConnectionInProgress = false;
+            this.isLoading = false;
+          });
+        } else {
+        this.setupPage();
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.routeWatcher$) {
+      this.routeWatcher$.unsubscribe();
+    }
+  }
 }
