9ac90a651a2d92a01466ae098551d2a331313444
ashwin1111 <winash1998@gmail.com>
2024-01-04 19:00:46 +0530

    temp url changes

--- src/app/integrations/travelperk/travelperk.component.ts
+++ src/app/integrations/travelperk/travelperk.component.ts
@@ -71,7 +71,7 @@ export class TravelperkComponent implements OnInit {
 
   connectTravelperk(): void {
     this.isConnectionInProgress = true;
-    const url = `${environment.travelperk_base_url}/oauth2/authorize?client_id=${environment.travelperk_client_id}&redirect_uri=${environment.travelperk_redirect_uri}&scope=expenses:read&response_type=code&state=${environment.production ? 'none' : 'travelperk_local_redirect'}`;
+    const url = `${environment.travelperk_base_url}/oauth2/authorize?client_id=${environment.travelperk_client_id}&redirect_uri=https://staging1.fyle.tech/app/settings/#/integrations/native_apps?integrationIframeTarget=integrations/travelperk&scope=expenses:read&response_type=code&state=${environment.production ? 'none' : 'travelperk_local_redirect'}`;
 
     const popup = window.open(url, 'popup', 'popup=true, width=500, height=800, left=500');
 
