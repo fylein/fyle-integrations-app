8ebe1aec76c935a32f4a48e1cda83ecd102bee99
Fyle <fyle@Fyles-MacBook-Air.local>
2024-01-08 18:49:30 +0530

    PR comments fix

--- src/app/auth/redirect/redirect.component.ts
+++ src/app/auth/redirect/redirect.component.ts
@@ -21,13 +21,15 @@ export class RedirectComponent implements OnInit {
   }
 
   private setupNavigation(): void {
-    if (this.route.snapshot.queryParams?.state.includes('travelperk_local_redirect')) {
-      const orgId = this.route.snapshot.queryParams.state.split('_')[0];
-      const url = `http://localhost:4200/oauth/travelperk?code=${this.route.snapshot.queryParams.code}&state=${orgId}`;
-      this.windowService.redirect(url);
-    } else if (this.route.snapshot.queryParams?.state === 'qbo_local_redirect' || this.route.snapshot.queryParams?.state === 'business_central_local_redirect') {
-      const url = `http://localhost:4200/integrations/oauth_callback?code=${this.route.snapshot.queryParams.code}&realmId=${this.route.snapshot.queryParams.realmId}`;
-      this.windowService.redirect(url);
+    if (this.route.snapshot.queryParams?.state) {
+      if (this.route.snapshot.queryParams?.state.includes('travelperk_local_redirect')) {
+        const orgId = this.route.snapshot.queryParams.state.split('_')[0];
+        const url = `http://localhost:4200/oauth/travelperk?code=${this.route.snapshot.queryParams.code}&state=${orgId}`;
+        this.windowService.redirect(url);
+      } else if (this.route.snapshot.queryParams?.state === 'qbo_local_redirect' || this.route.snapshot.queryParams?.state === 'business_central_local_redirect') {
+        const url = `http://localhost:4200/integrations/oauth_callback?code=${this.route.snapshot.queryParams.code}&realmId=${this.route.snapshot.queryParams.realmId}`;
+        this.windowService.redirect(url);
+      } 
     } else {
       this.redirectToFyleOAuth();
     }
