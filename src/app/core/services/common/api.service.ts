8cfe1974f8a45f879a89816cc0384e07e17daf92
ashwin1111 <winash1998@gmail.com>
2024-01-24 17:35:24 +0530

    Support for different clusters

--- src/app/core/services/common/api.service.ts
+++ src/app/core/services/common/api.service.ts
@@ -2,9 +2,8 @@ import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular
 import { Injectable } from '@angular/core';
 import { catchError, Observable } from 'rxjs';
 import { throwError } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
-let API_BASE_URL = environment.api_url;
+let API_BASE_URL: string;
 
 const httpOptions = {
   headers: new HttpHeaders({
--- src/app/core/services/common/workspace.service.ts
+++ src/app/core/services/common/workspace.service.ts
@@ -5,6 +5,7 @@ import { AppUrl, BusinessCentralOnboardingState, IntacctOnboardingState, QBDOnbo
 import { ApiService } from './api.service';
 import { HelperService } from './helper.service';
 import { AppUrlMap } from '../../models/integrations/integrations.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
