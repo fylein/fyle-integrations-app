ee2820b09ea559c000417754a5046d982ae2a0ed
Fyle <fyle@Fyles-MacBook-Air.local>
2024-01-31 15:46:53 +0530

    travelperk folder creation

---
+++ src/app/integrations/travelperk/travelperk-shared/travelperk-shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TravelperkSharedRoutingModule } from './travelperk-shared-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TravelperkSharedRoutingModule
+  ]
+})
+export class TravelperkSharedModule { }
