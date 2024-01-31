37ecbb05cb12f47e04a9dc1df3f2b02f9caea4f0
Fyle <fyle@Fyles-MacBook-Air.local>
2024-01-31 15:53:27 +0530

    travelperk router creation

--- src/app/integrations/travelperk/travelperk-onboarding/travelperk-onboarding-routing.module.ts
+++ src/app/integrations/travelperk/travelperk-onboarding/travelperk-onboarding-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TravelperkOnboardingPaymentProfileSettingsComponent } from './travelperk-onboarding-payment-profile-settings/travelperk-onboarding-payment-profile-settings.component';
+import { TravelperkOnboardingAdvancedSettingsComponent } from './travelperk-onboarding-advanced-settings/travelperk-onboarding-advanced-settings.component';
+import { TravelperkOnboardingComponent } from './travelperk-onboarding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TravelperkOnboardingComponent,
+    children: [
+      // {
+      //   path: 'landing',
+      //   component: 
+      // },
+      {
+        path: 'payment_profile_settings',
+        component: TravelperkOnboardingPaymentProfileSettingsComponent
+      },
+      {
+        path: 'advanced_settings',
+        component: TravelperkOnboardingAdvancedSettingsComponent
+      },
+      // {
+      //   path: 'done',
+      //   component: TravelperkOnboardingDoneComponent
+      // }
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
