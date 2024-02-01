387e28bf7f674da9cf87a7712f7a9039fc9616b4
ashwin1111 <winash1998@gmail.com>
2024-02-01 15:29:16 +0530

    Cluster domain changes

--- src/app/integrations/landing/landing.component.ts
+++ src/app/integrations/landing/landing.component.ts
@@ -68,6 +68,8 @@ export class LandingComponent implements OnInit {
 
   readonly brandingConfig = brandingConfig;
 
+  readonly isINCluster = this.storageService.get('cluster-domain').includes('in1');
+
   constructor(
     private eventsService: EventsService,
     private qboAuthService: QboAuthService,
