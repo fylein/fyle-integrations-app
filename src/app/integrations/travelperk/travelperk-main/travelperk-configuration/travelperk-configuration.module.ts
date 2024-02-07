be35e4fb4305d334ef41d3092862fa0623d1f607
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2024-02-07 15:45:36 +0530

    travel onboarding done (#509)
    travel onboarding done

--- src/app/integrations/travelperk/travelperk-main/travelperk-configuration/travelperk-configuration.module.ts
+++ src/app/integrations/travelperk/travelperk-main/travelperk-configuration/travelperk-configuration.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TravelperkConfigurationRoutingModule } from './travelperk-configuration-routing.module';
-
 
 @NgModule({
   declarations: [],
