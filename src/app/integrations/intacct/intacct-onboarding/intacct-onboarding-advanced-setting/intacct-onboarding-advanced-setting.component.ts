4ea9719d6b819e513e9d9772e3440d95de2721d9
anishfyle <anishkumar.s@fyle.in>
2024-02-09 16:00:44 +0530

    Advanced Settings

--- src/app/integrations/intacct/intacct-onboarding/intacct-onboarding-advanced-setting/intacct-onboarding-advanced-setting.component.ts
+++ src/app/integrations/intacct/intacct-onboarding/intacct-onboarding-advanced-setting/intacct-onboarding-advanced-setting.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IntacctOnboardingModel } from 'src/app/core/models/intacct/intacct-configuration/intacct-onboarding.model';
+import { OnboardingStepper } from 'src/app/core/models/misc/onboarding-stepper.model';
+import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
 
 @Component({
   selector: 'app-intacct-onboarding-advanced-setting',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntacctOnboardingAdvancedSettingComponent implements OnInit {
 
-  constructor() { }
+  onboardingSteps: OnboardingStepper[] = new IntacctOnboardingModel().getOnboardingSteps('Advanced Settings', this.workspaceService.getOnboardingState());
+
+  constructor(
+    private workspaceService: WorkspaceService
+  ) { }
 
   ngOnInit(): void {
   }
