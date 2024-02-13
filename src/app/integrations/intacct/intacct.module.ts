c94037ac8b806e34914fd6e4a3e850b70ddee09f
anishfyle <anishkumar.s@fyle.in>
2024-02-14 02:21:23 +0530

    module changes wl intacct

--- src/app/integrations/intacct/intacct.module.ts
+++ src/app/integrations/intacct/intacct.module.ts
@@ -7,11 +7,11 @@ import { TabMenuModule } from 'primeng/tabmenu';
 import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 import { SiRoutingModule } from './intacct-routing.module';
-import { MainComponent } from './main/main.component';
-import { ConfigurationComponent } from './main/configuration/configuration.component';
-import { ConfigurationExportSettingComponent } from './main/configuration/configuration-export-setting/configuration-export-setting.component';
-import { ConfigurationAdvancedSettingComponent } from './main/configuration/configuration-advanced-setting/configuration-advanced-setting.component';
-import { ConfigurationImportSettingComponent } from './main/configuration/configuration-import-setting/configuration-import-setting.component';
+import { MainComponent } from './intacct-main/main.component';
+import { ConfigurationComponent } from './intacct-main/configuration/configuration.component';
+import { ConfigurationExportSettingComponent } from './intacct-main/configuration/configuration-export-setting/configuration-export-setting.component';
+import { ConfigurationAdvancedSettingComponent } from './intacct-main/configuration/configuration-advanced-setting/configuration-advanced-setting.component';
+import { ConfigurationImportSettingComponent } from './intacct-main/configuration/configuration-import-setting/configuration-import-setting.component';
 import { DialogModule } from 'primeng/dialog';
 import { IntacctSharedModule } from './intacct-shared/intacct-shared.module';
 
