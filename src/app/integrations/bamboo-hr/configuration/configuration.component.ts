2d69f3b7c7dfec4288b547832ef64823d869c0e4
Fyle <fyle@Fyles-MacBook-Air.local>
2024-02-15 17:08:20 +0530

    arrow and dropdown fix

--- src/app/integrations/bamboo-hr/configuration/configuration.component.ts
+++ src/app/integrations/bamboo-hr/configuration/configuration.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DropdownFilterOptions } from 'primeng/dropdown';
 import { brandingConfig } from 'src/app/branding/branding-config';
 import { BambooHRConfiguration, BambooHRConfigurationPost, BambooHrModel, EmailOption } from 'src/app/core/models/bamboo-hr/bamboo-hr.model';
-import { ClickEvent, TrackingApp } from 'src/app/core/models/enum/enum.model';
+import { AppName, ClickEvent, TrackingApp } from 'src/app/core/models/enum/enum.model';
 import { Org } from 'src/app/core/models/org/org.model';
 import { TrackingService } from 'src/app/core/services/integration/tracking.service';
 import { OrgService } from 'src/app/core/services/org/org.service';
@@ -35,6 +35,8 @@ export class ConfigurationComponent implements OnInit {
   });
 
   showDialog: boolean;
+
+  appName = AppName.BAMBOO_HR
 
   readonly brandingConfig = brandingConfig;
 
