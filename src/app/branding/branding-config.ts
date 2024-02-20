c1e933a90a8fae2d6587abfb024edee9610d5c73
anishfyle <anishkumar.s@fyle.in>
2024-02-20 19:18:30 +0530

    brandingConfig for Export Settings Done

--- src/app/branding/branding-config.ts
+++ src/app/branding/branding-config.ts
@@ -214,7 +214,8 @@ const content: ContentConfiguration = {
                     defaultCCCVendorLabel: 'Set default corporate Card Vendor as',
                     accountsPayableLabel: 'To which Accounts Payable account should the ',
                     accountsPayableSubLabel: ' to the selected Accounts Payable Account.',
-                    creditCardExportTypeSubLabel: 'You could choose to export CCC expenses when they have been approved and are awaiting payment clearance, OR simply when they have been paid out',
+                    creditCardExpenseSubLabel: 'You could choose to export CCC expenses when they have been approved and are awaiting payment clearance, OR simply when they have been paid out.',
+                    creditCardExportTypeSubLabel: 'Expense can either be exported as single line items (Expense) or as a grouped report with multiple line items (Expense Report)',
                     journalOptionLabel: 'Name in Journal Entry (CCC)',
                     journalOptionSubLabel: 'You can select either the \'Merchant Name\' or the \'Employee Name\' to appear in the \'Name\' field of your Journal Entries'
                 }
@@ -237,7 +238,8 @@ const content: ContentConfiguration = {
                     defaultCCCVendorLabel: 'Set default corporate card vendor as',
                     accountsPayableLabel: 'To which accounts payable account should the ',
                     accountsPayableSubLabel: ' to the selected accounts payable Account.',
-                    creditCardExportTypeSubLabel: 'You could choose to export ccc expenses when they have been approved and are awaiting payment clearance, or simply when they have been paid out',
+                    creditCardExpenseSubLabel: 'You could choose to export ccc expenses when they have been approved and are awaiting payment clearance, or simply when they have been paid out.',
+                    creditCardExportTypeSubLabel: 'Expense can either be exported as single line items (expense) or as a grouped report with multiple line items (expense report)',
                     journalOptionLabel: 'Name in journal entry (ccc)',
                     journalOptionSubLabel: 'You can select either the \'merchant name\' or the \'employee name\' to appear in the \'name\' field of your journal entries'
                 }
