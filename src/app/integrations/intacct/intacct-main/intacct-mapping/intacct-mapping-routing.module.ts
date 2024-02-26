39ebdf37f404e5dbedef3b0f3f6ed752337f54dd
anishfyle <anishkumar.s@fyle.in>
2024-02-26 12:15:08 +0530

    IntacctBaseMappingComponent

--- src/app/integrations/intacct/intacct-main/intacct-mapping/intacct-mapping-routing.module.ts
+++ src/app/integrations/intacct/intacct-main/intacct-mapping/intacct-mapping-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IntacctMappingComponent } from './intacct-mapping.component';
+import { IntacctBaseMappingComponent } from './intacct-base-mapping/intacct-base-mapping.component';
 
 
 const routes: Routes = [
   {
-    path: '',
-    component: IntacctMappingComponent,
+    path: ':source_field',
+    component: IntacctBaseMappingComponent,
     children: [
     ]
   }
