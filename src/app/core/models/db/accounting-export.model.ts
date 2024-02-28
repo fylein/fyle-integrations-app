6b44dfd8db69264825482e33cba4d961a4cc176f
anishfyle <anishkumar.s@fyle.in>
2024-02-28 15:07:26 +0530

    response_logs?.url_id

--- src/app/core/models/db/accounting-export.model.ts
+++ src/app/core/models/db/accounting-export.model.ts
@@ -218,7 +218,7 @@ export class AccountingExportModel {
     if(appName===AppName.QBO) {
       return this.constructExportUrlQBO(expenseGroup);
     } else if (appName===AppName.INTACCT) {
-      return this.constructExportUrlIntacct(expenseGroup.response_logs.urlId)
+      return this.constructExportUrlIntacct(expenseGroup.response_logs?.url_id)
     }
 
     return '';
