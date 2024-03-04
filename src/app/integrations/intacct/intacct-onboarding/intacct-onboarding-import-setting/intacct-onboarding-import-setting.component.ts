3cec0de1c02adf2825ef56ab8fbd822ec6c99781
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2024-03-04 15:52:45 +0530

    Intacct c1 import settings (#622)

--- src/app/integrations/intacct/intacct-onboarding/intacct-onboarding-import-setting/intacct-onboarding-import-setting.component.ts
+++ src/app/integrations/intacct/intacct-onboarding/intacct-onboarding-import-setting/intacct-onboarding-import-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { brandingConfig } from 'src/app/branding/branding-config';
 import { IntacctOnboardingModel } from 'src/app/core/models/intacct/intacct-configuration/intacct-onboarding.model';
 import { OnboardingStepper } from 'src/app/core/models/misc/onboarding-stepper.model';
 import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
@@ -12,6 +13,7 @@ export class IntacctOnboardingImportSettingComponent implements OnInit {
 
   onboardingSteps: OnboardingStepper[] = new IntacctOnboardingModel().getOnboardingSteps('Import Settings', this.workspaceService.getOnboardingState());
 
+  readonly brandingConfig = brandingConfig;
 
   constructor(
     private workspaceService: WorkspaceService
