00a783fd3003f8ab1598fe3fba6455838a47d4b5
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2024-03-04 20:09:06 +0530

    Export Log Advance Search (#600)
    * Export Log Advance Search
    
    * linting resolved
    
    * bug fix for wrong number of pages when filter is used
    
    * bug fix for skipped export
    
    * bug found and fixed
    
    * Mappings page advance search (#609)
    
    * Advance Search for mappings Page
    
    * minor UI change
    
    * loading problem solved
    
    * isLoading issue resolved
    
    * mapping page seraching bug fox
    
    * resolving issue
    
    * loading screen for export logs and mapping page

--- src/app/core/models/db/expense-group.model.ts
+++ src/app/core/models/db/expense-group.model.ts
@@ -33,6 +33,10 @@ export type ExpenseGroupParam = {
   tasklog__status: string;
   exported_at__gte?: string;
   exported_at__lte?: string;
+  expenses__expense_number?: string;
+  expenses__employee_name?: string;
+  expenses__employee_email?: string;
+  expenses__claim_number?: string;
 }
 
 export type SkipExportParam = {
@@ -42,4 +46,8 @@ export type SkipExportParam = {
   is_skipped: boolean;
   updated_at__gte?: string;
   updated_at__lte?: string;
+  expense_number?: string;
+  employee_name?: string;
+  employee_email?: string;
+  claim_number?: string;
 }
