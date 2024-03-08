1fe3d19d5ef0bac0eed51cc63142903d84bd3f52
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-08 13:50:17 +0530

    travelperk toast fix

--- src/app/integrations/travelperk/travelperk-main/travelperk-main.component.ts
+++ src/app/integrations/travelperk/travelperk-main/travelperk-main.component.ts
@@ -40,7 +40,7 @@ export class TravelperkMainComponent implements OnInit {
   refreshDimentions():void {
     this.travelperkService.syncCategories().subscribe();
     this.travelperkService.syncPaymentProfile().subscribe;
-    this.toastService.displayToastMessage(ToastSeverity.SUCCESS, 'Refreshing data dimensions from TravelPerk CRE');
+    this.toastService.displayToastMessage(ToastSeverity.SUCCESS, 'Refreshing data dimensions from TravelPerk');
   }
 
   private setupPage() {
