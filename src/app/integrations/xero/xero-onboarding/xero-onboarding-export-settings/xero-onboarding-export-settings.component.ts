79fa7838c37ecf9c8f2bf2ec3a0b0e5db43f628b
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-11 19:22:59 +0530

    xero onboarding export settings page construction

--- src/app/integrations/xero/xero-onboarding/xero-onboarding-export-settings/xero-onboarding-export-settings.component.ts
+++ src/app/integrations/xero/xero-onboarding/xero-onboarding-export-settings/xero-onboarding-export-settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { brandingContent } from 'src/app/branding/branding-config';
+import { OnboardingStepper } from 'src/app/core/models/misc/onboarding-stepper.model';
+import { XeroOnboardingModel } from 'src/app/core/models/xero/xero-configuration/xero-onboarding.model';
+import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
 
 @Component({
   selector: 'app-xero-onboarding-export-settings',
@@ -7,7 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XeroOnboardingExportSettingsComponent implements OnInit {
 
-  constructor() { }
+  brandingContent = brandingContent.configuration.exportSetting;
+
+  onboardingSteps: OnboardingStepper[] = new XeroOnboardingModel().getOnboardingSteps(this.brandingContent.stepName, this.workspaceService.getOnboardingState());
+
+  constructor(
+    private workspaceService: WorkspaceService
+  ) { }
 
   ngOnInit(): void {
   }
