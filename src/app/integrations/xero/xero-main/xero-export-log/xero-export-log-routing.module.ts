8e225a3421af60c915a6adbd9f6e79ef41f5f255
anishfyle <anishkumar.s@fyle.in>
2024-03-13 13:01:45 +0530

    export log/skipped - 3

--- src/app/integrations/xero/xero-main/xero-export-log/xero-export-log-routing.module.ts
+++ src/app/integrations/xero/xero-main/xero-export-log/xero-export-log-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { XeroSkippedExportLogComponent } from './xero-skipped-export-log/xero-skipped-export-log.component';
+import { XeroCompleteExportLogComponent } from './xero-complete-export-log/xero-complete-export-log.component';
+import { XeroExportLogComponent } from './xero-export-log.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: XeroExportLogComponent,
+    children: [
+      {
+        path: 'complete',
+        component: XeroCompleteExportLogComponent
+      },
+      {
+        path: 'skipped',
+        component: XeroSkippedExportLogComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class XeroExportLogRoutingModule { }
