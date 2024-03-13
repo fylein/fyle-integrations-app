91a412ff1f78038b989e686f4adef72e5c3d4c26
anishfyle <anishkumar.s@fyle.in>
2024-03-13 14:02:55 +0530

    xero mapping module

---
+++ src/app/integrations/xero/xero-main/xero-mapping/xero-mapping.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { XeroMappingRoutingModule } from './xero-mapping-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    XeroMappingRoutingModule
+  ]
+})
+export class XeroMappingModule { }
