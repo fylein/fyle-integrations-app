1bdde8d96e3a0258d67293c29f7481017380535d
anishfyle <anishkumar.s@fyle.in>
2024-03-13 13:31:39 +0530

    TrackingApp

--- src/app/integrations/xero/xero-main/xero-export-log/xero-export-log.module.ts
+++ src/app/integrations/xero/xero-main/xero-export-log/xero-export-log.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { XeroExportLogRoutingModule } from '../xero-export-log-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    XeroExportLogRoutingModule
+    CommonModule
   ]
 })
 export class XeroExportLogModule { }
