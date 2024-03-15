05ca761cda8a8142b0f448e692cdbadbf5a466fb
anishfyle <anishkumar.s@fyle.in>
2024-03-15 11:07:50 +0530

    xero base mapping

---
+++ src/app/integrations/xero/xero-main/xero-mapping/xero-base-mapping/xero-base-mapping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xero-base-mapping',
+  templateUrl: './xero-base-mapping.component.html',
+  styleUrls: ['./xero-base-mapping.component.scss']
+})
+export class XeroBaseMappingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
