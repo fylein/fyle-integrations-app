d75dd6270b71b60aeae1c0d7de112e6e6407ab54
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-15 13:43:01 +0530

    xero configuration setting

--- src/app/integrations/xero/xero-main/xero-configuration/xero-configuration.component.ts
+++ src/app/integrations/xero/xero-main/xero-configuration/xero-configuration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { brandingConfig, brandingContent, brandingFeatureConfig } from 'src/app/branding/branding-config';
 
 @Component({
   selector: 'app-xero-configuration',
@@ -6,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./xero-configuration.component.scss']
 })
 export class XeroConfigurationComponent implements OnInit {
+
+  readonly brandingContent = brandingContent.xero.configuration;
+
+  modules: MenuItem[] = [
+    {label: this.brandingContent.exportSetting.stepName, routerLink: '/integrations/xero/main/configuration/export_settings'},
+    {label: this.brandingContent.importSetting.stepName, routerLink: '/integrations/xero/main/configuration/import_settings'},
+    {label: this.brandingContent.advancedSettings.stepName, routerLink: '/integrations/xero/main/configuration/advanced_settings'}
+  ];
+
+  activeModule: MenuItem = this.modules[0];
+
+  readonly isGradientAllowed: boolean = brandingFeatureConfig.isGradientAllowed;
+
+  readonly brandingConfig = brandingConfig;
 
   constructor() { }
 
