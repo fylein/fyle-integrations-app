3a69aef24e9848b9e1fbd2fbe30bfa8f2da0e8be
anishfyle <anishkumar.s@fyle.in>
2024-03-15 13:25:29 +0530

    export log

--- src/app/core/models/db/accounting-export.model.ts
+++ src/app/core/models/db/accounting-export.model.ts
@@ -214,11 +214,42 @@ export class AccountingExportModel {
     return [`https://www-p02.intacct.com/ia/acct/ur.phtml?.r=${expenseGroup.response_logs?.url_id}`, expenseGroup.export_type];
   }
 
+  static constructXeroExportUrlAndType(expenseGroup: ExpenseGroup): [string, string] {
+    let exportRedirection = '';
+    let exportType = '';
+    let exportId = null;
+    let accountId = null;
+    let xeroShortCode = '';
+    const xeroUrl = 'https://go.xero.com';
+    if ('Invoices' in expenseGroup.response_logs && expenseGroup.response_logs.Invoices) {
+      exportType = 'Bill';
+      exportId = expenseGroup.response_logs.Invoices[0].InvoiceID;
+      if (xeroShortCode) {
+        exportRedirection = `${xeroUrl}/organisationlogin/default.aspx?shortcode=${xeroShortCode}&redirecturl=/AccountsPayable/Edit.aspx?InvoiceID=${exportId}`;
+      } else {
+        exportRedirection = `${xeroUrl}/AccountsPayable/View.aspx?invoiceID=${exportId}`;
+      }
+    } else if ('BankTransactions' in expenseGroup.response_logs && expenseGroup.response_logs.BankTransactions) {
+      exportType = 'Bank Transaction';
+      exportId = expenseGroup.response_logs.BankTransactions[0].BankTransactionID;
+      accountId = expenseGroup.response_logs.BankTransactions[0].BankAccount.AccountID;
+      if (xeroShortCode) {
+        exportRedirection = `${xeroUrl}/organisationlogin/default.aspx?shortcode=${xeroShortCode}&redirecturl=/Bank/ViewTransaction.aspx?bankTransactionID=${exportId}`;
+      } else {
+        exportRedirection = `${xeroUrl}/Bank/ViewTransaction.aspx?bankTransactionID=${exportId}&accountID=${accountId}`;
+      }
+    }
+
+    return [exportRedirection, exportType];
+  }
+
   static constructExportUrlAndType(appName: AppName, expenseGroup: ExpenseGroup): [string, string] {
     if (appName === AppName.QBO) {
       return this.constructQBOExportUrlAndType(expenseGroup);
     } else if (appName === AppName.INTACCT) {
       return this.constructIntacctExportUrlAndType(expenseGroup);
+    } else if (appName == AppName.XERO) {
+      return this.constructXeroExportUrlAndType(expenseGroup);
     }
 
     return ['', ''];
@@ -229,7 +260,7 @@ export class AccountingExportModel {
       const referenceNumber = this.getFyleReferenceNumber(referenceType, expenseGroup.expenses[0]);
 
       const [url, exportType] = this.constructExportUrlAndType(appName, expenseGroup);
-
+       console.log(appName, expenseGroup);
       return {
         exportedAt: expenseGroup.exported_at,
         employee: [expenseGroup.expenses[0].employee_name, expenseGroup.description.employee_email],
