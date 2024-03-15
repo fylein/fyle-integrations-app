1d8bf8699b57c0aead1c962a4e9b4092c501c22e
anishfyle <anishkumar.s@fyle.in>
2024-03-15 11:04:13 +0530

    export log routing

--- src/app/integrations/xero/xero-main/xero-export-log/xero-export-log.module.ts
+++ src/app/integrations/xero/xero-main/xero-export-log/xero-export-log.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { XeroExportLogRoutingModule } from './xero-export-log-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    XeroExportLogRoutingModule
   ]
 })
 export class XeroExportLogModule { }
