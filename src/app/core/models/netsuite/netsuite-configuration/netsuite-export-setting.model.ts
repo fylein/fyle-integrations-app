7fc5afa67b34d4b991dcef42033dbe91702175a7
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2024-03-16 11:49:06 +0530

    Netsuite export settings 2 (#675)

--- src/app/core/models/netsuite/netsuite-configuration/netsuite-export-setting.model.ts
+++ src/app/core/models/netsuite/netsuite-configuration/netsuite-export-setting.model.ts
@@ -5,11 +5,14 @@ import { DefaultDestinationAttribute } from "../../db/destination-attribute.mode
 import { ExpenseGroupSettingGet, ExpenseGroupSettingPost } from "../../db/expense-group-setting.model";
 import { CCCExpenseState, EmployeeFieldMapping, ExpenseGroupingFieldOption, ExpenseState, ExportDateType, NameInJournalEntry, NetSuiteCorporateCreditCardExpensesObject, NetsuiteReimbursableExpensesObject } from "../../enum/enum.model";
 import { brandingFeatureConfig } from "src/app/branding/branding-config";
+import { ExportSettingFormOption } from "../../intacct/intacct-configuration/export-settings.model";
+import { disable } from "@rxweb/reactive-form-validators";
 
 
 export type NetsuiteExportSettingWorkspaceGeneralSettingPost = {
     employee_field_mapping: EmployeeFieldMapping,
     reimbursable_expenses_object: NetsuiteReimbursableExpensesObject | null,
+    auto_map_employees: ExportSettingFormOption | null,
     corporate_credit_card_expenses_object: NetSuiteCorporateCreditCardExpensesObject | null
     name_in_journal_entry: NameInJournalEntry;
 }
@@ -25,43 +28,35 @@ export type NetsuiteExportSettingGeneralMapping = {
 
 export type NetSuiteExportSettingPost = {
   expense_group_settings: ExpenseGroupSettingPost,
-    configurations: NetsuiteExportSettingWorkspaceGeneralSettingPost,
+    configuration: NetsuiteExportSettingWorkspaceGeneralSettingPost,
     general_mappings: NetsuiteExportSettingGeneralMapping
   }
 
 
 export type NetSuiteExportSettingGet = {
     expense_group_settings: ExpenseGroupSettingGet,
-    configurations: NetsuiteExportSettingWorkspaceGeneralSettingPost,
+    configuration: NetsuiteExportSettingWorkspaceGeneralSettingPost,
     general_mappings: NetsuiteExportSettingGeneralMapping,
     workspace_id: number
 }
 
 
 export class NetSuiteExportSettingModel extends ExportSettingModel {
-    static getReimbursableExportTypeOptions(employeeFieldMapping: EmployeeFieldMapping): SelectFormOption[] {
-      return {
-        EMPLOYEE: [
-          {
+    static getReimbursableExportTypeOptions(): SelectFormOption[] {
+      return [
+        {
+          label: 'Bill',
+          value: NetSuiteCorporateCreditCardExpensesObject.BILL
+        },
+        {
+          label: 'Journal Entry',
+          value: NetSuiteCorporateCreditCardExpensesObject.JOURNAL_ENTRY
+        },
+        {
             label: 'Expense Report',
-            value: NetsuiteReimbursableExpensesObject.EXPENSE_REPORT
-          },
-          {
-            label: 'Journal Entry',
-            value: NetsuiteReimbursableExpensesObject.JOURNAL_ENTRY
-          }
-        ],
-        VENDOR: [
-          {
-            label: 'Bill',
-            value: NetsuiteReimbursableExpensesObject.BILL
-          },
-          {
-            label: 'Journal Entry',
-            value: NetsuiteReimbursableExpensesObject.JOURNAL_ENTRY
-          }
-        ]
-      }[employeeFieldMapping];
+            value: NetSuiteCorporateCreditCardExpensesObject.EXPENSE_REPORT
+        }
+      ];
     }
 
     static getCreditCardExportTypes(): SelectFormOption[] {
@@ -81,7 +76,7 @@ export class NetSuiteExportSettingModel extends ExportSettingModel {
         {
             label: 'Expense Report',
             value: NetSuiteCorporateCreditCardExpensesObject.EXPENSE_REPORT
-          }
+        }
       ];
     }
 
@@ -218,24 +213,23 @@ export class NetSuiteExportSettingModel extends ExportSettingModel {
         return [exportSettingValidatorRule, exportModuleRule];
       }
 
-      static mapAPIResponseToFormGroup(exportSettings: NetSuiteExportSettingGet | null, employeeFieldMapping: EmployeeFieldMapping): FormGroup {
+      static mapAPIResponseToFormGroup(exportSettings: NetSuiteExportSettingGet | null): FormGroup {
         return new FormGroup({
-          employeeMapping: new FormControl(employeeFieldMapping),
           expenseState: new FormControl(exportSettings?.expense_group_settings?.expense_state),
-          employeeFieldMapping: new FormControl(exportSettings?.configurations?.employee_field_mapping),
-          reimbursableExpense: new FormControl(exportSettings?.configurations?.reimbursable_expenses_object ? true : false),
-          reimbursableExportType: new FormControl(exportSettings?.configurations?.reimbursable_expenses_object),
+          employeeFieldMapping: new FormControl(exportSettings?.configuration?.employee_field_mapping),
+          autoMapEmployees: new FormControl(exportSettings?.configuration?.auto_map_employees),
+          reimbursableExpense: new FormControl(exportSettings?.configuration?.reimbursable_expenses_object ? true : false),
+          reimbursableExportType: new FormControl(exportSettings?.configuration?.reimbursable_expenses_object),
           reimbursableExportGroup: new FormControl(this.getExportGroup(exportSettings?.expense_group_settings?.reimbursable_expense_group_fields)),
           reimbursableExportDate: new FormControl(exportSettings?.expense_group_settings?.reimbursable_export_date_type),
           cccExpenseState: new FormControl(exportSettings?.expense_group_settings?.ccc_expense_state),
-          creditCardExpense: new FormControl(exportSettings?.configurations?.corporate_credit_card_expenses_object ? true : false),
-          creditCardExportType: new FormControl(exportSettings?.configurations?.corporate_credit_card_expenses_object),
+          creditCardExpense: new FormControl(exportSettings?.configuration?.corporate_credit_card_expenses_object ? true : false),
+          creditCardExportType: new FormControl(exportSettings?.configuration?.corporate_credit_card_expenses_object),
           creditCardExportGroup: new FormControl(this.getExportGroup(exportSettings?.expense_group_settings?.corporate_credit_card_expense_group_fields)),
           creditCardExportDate: new FormControl(exportSettings?.expense_group_settings?.ccc_export_date_type),
           defaultCCCAccount: new FormControl(exportSettings?.general_mappings?.default_ccc_account?.id ? exportSettings.general_mappings.default_ccc_account : null),
           accountsPayable: new FormControl(exportSettings?.general_mappings?.accounts_payable?.id ? exportSettings.general_mappings.accounts_payable : null),
           defaultCreditCardVendor: new FormControl(exportSettings?.general_mappings?.default_ccc_vendor?.id ? exportSettings.general_mappings.default_ccc_vendor : null)
-          // NameInJournalEntry: new FormControl(exportSettings?.configurations.name_in_journal_entry ? exportSettings.configurations.name_in_journal_entry : NameInJournalEntry.EMPLOYEE )
         });
       }
 
@@ -258,10 +252,11 @@ export class NetSuiteExportSettingModel extends ExportSettingModel {
             corporate_credit_card_expense_group_fields: exportSettingsForm.get('creditCardExportGroup')?.value ? [exportSettingsForm.get('creditCardExportGroup')?.value] : null,
             ccc_export_date_type: exportSettingsForm.get('creditCardExportDate')?.value
           },
-          configurations: {
+          configuration: {
             reimbursable_expenses_object: exportSettingsForm.get('reimbursableExportType')?.value,
             corporate_credit_card_expenses_object: exportSettingsForm.get('creditCardExportType')?.value,
             employee_field_mapping: exportSettingsForm.get('employeeFieldMapping')?.value,
+            auto_map_employees: exportSettingsForm.get('autoMapEmployees')?.value,
             name_in_journal_entry: nameInJournalEntry
           },
           general_mappings: {
