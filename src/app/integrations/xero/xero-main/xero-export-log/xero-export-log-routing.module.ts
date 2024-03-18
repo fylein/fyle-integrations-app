4c50115ce589279d43b782fd805ece7247bba7ff
anishfyle <anishkumar.s@fyle.in>
2024-03-18 13:57:02 +0530

    Complete Export Log

--- src/app/integrations/xero/xero-main/xero-export-log/xero-export-log-routing.module.ts
+++ src/app/integrations/xero/xero-main/xero-export-log/xero-export-log-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: XeroExportLogComponent,
     children: [
       {
-        path: 'complete',
+        path: '',
         component: XeroCompleteExportLogComponent
       }
     ]
