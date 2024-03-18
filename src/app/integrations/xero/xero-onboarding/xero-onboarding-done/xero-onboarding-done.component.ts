5af01dce3920ae4f55cd5a5ebb2b78b2132cac54
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2024-03-18 11:23:22 +0530

    advanced settings ts coding (#670)
    advanced settings ts coding

--- src/app/integrations/xero/xero-onboarding/xero-onboarding-done/xero-onboarding-done.component.ts
+++ src/app/integrations/xero/xero-onboarding/xero-onboarding-done/xero-onboarding-done.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-xero-onboarding-done',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XeroOnboardingDoneComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  navigateToDashboard(): void {
+    this.router.navigate([`/integrations/xero/main/dashboard`]);
+  }
 
   ngOnInit(): void {
   }
