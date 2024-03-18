d4f24f50f9e1b57bd857169a4f79722c1d5462f1
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-18 13:26:44 +0530

    clone settings construction

---
+++ src/app/integrations/xero/xero-onboarding/xero-clone-settings/xero-clone-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xero-clone-settings',
+  templateUrl: './xero-clone-settings.component.html',
+  styleUrls: ['./xero-clone-settings.component.scss']
+})
+export class XeroCloneSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
