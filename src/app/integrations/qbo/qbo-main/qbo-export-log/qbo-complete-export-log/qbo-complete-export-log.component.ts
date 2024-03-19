d465211ac0eb12f902e5f9eceda1bb920fc0d835
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2024-03-19 12:35:56 +0530

    Fix calendar close issue on date selection (#684)

--- src/app/integrations/qbo/qbo-main/qbo-export-log/qbo-complete-export-log/qbo-complete-export-log.component.ts
+++ src/app/integrations/qbo/qbo-main/qbo-export-log/qbo-complete-export-log/qbo-complete-export-log.component.ts
@@ -40,8 +40,6 @@ export class QboCompleteExportLogComponent implements OnInit {
 
   exportLogForm: FormGroup;
 
-  isCalendarVisible: boolean;
-
   accountingExports: AccountingExportList [];
 
   filteredAccountingExports: AccountingExportList [];
@@ -98,10 +96,7 @@ export class QboCompleteExportLogComponent implements OnInit {
   }
 
   private getAccountingExports(limit: number, offset:number) {
-
-      this.isLoading = true;
-
-
+    this.isLoading = true;
     if (this.limit !== limit) {
       this.paginatorService.storePageSize(PaginatorPage.EXPORT_LOG, limit);
     }
