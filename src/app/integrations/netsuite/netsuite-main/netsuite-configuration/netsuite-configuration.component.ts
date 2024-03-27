d6515a46e5481e300547207f849f33bf4e183a2f
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-27 18:02:13 +0530

    netsuite configuration page set up

--- src/app/integrations/netsuite/netsuite-main/netsuite-configuration/netsuite-configuration.component.ts
+++ src/app/integrations/netsuite/netsuite-main/netsuite-configuration/netsuite-configuration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { brandingContent, brandingFeatureConfig, brandingConfig } from 'src/app/branding/branding-config';
 
 @Component({
   selector: 'app-netsuite-configuration',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetsuiteConfigurationComponent implements OnInit {
 
+  readonly brandingContent = brandingContent.configuration;
+
+  modules: MenuItem[] = [
+    {label: this.brandingContent.exportSetting.stepName, routerLink: '/integrations/netsuite/main/configuration/export_settings'},
+    {label: this.brandingContent.importSetting.stepName, routerLink: '/integrations/netsuite/main/configuration/import_settings'},
+    {label: this.brandingContent.advancedSettings.stepName, routerLink: '/integrations/netsuite/main/configuration/advanced_settings'}
+  ];
+
+  activeModule: MenuItem = this.modules[0];
+
+  readonly isGradientAllowed: boolean = brandingFeatureConfig.isGradientAllowed;
+
+  readonly brandingConfig = brandingConfig;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (!brandingFeatureConfig.featureFlags.mapEmployees) {
+      this.modules.splice(0, 1);
+    }
   }
 
 }
