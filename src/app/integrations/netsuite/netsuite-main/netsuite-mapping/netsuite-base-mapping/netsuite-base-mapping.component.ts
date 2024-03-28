33bbf4f02e2abe98665938c296961a1fc67436f1
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-28 14:54:15 +0530

    generic  mapping constrction

--- src/app/integrations/netsuite/netsuite-main/netsuite-mapping/netsuite-base-mapping/netsuite-base-mapping.component.ts
+++ src/app/integrations/netsuite/netsuite-main/netsuite-mapping/netsuite-base-mapping/netsuite-base-mapping.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { DestinationAttribute } from 'src/app/core/models/db/destination-attribute.model';
+import { MappingSetting } from 'src/app/core/models/db/mapping-setting.model';
+import { AccountingDisplayName, AccountingField, AppName, FyleField, NetSuiteCorporateCreditCardExpensesObject, NetsuiteReimbursableExpensesObject, ToastSeverity } from 'src/app/core/models/enum/enum.model';
+import { NetsuiteExportSettingWorkspaceGeneralSettingPost } from 'src/app/core/models/netsuite/netsuite-configuration/netsuite-export-setting.model';
+import { IntegrationsToastService } from 'src/app/core/services/common/integrations-toast.service';
+import { MappingService } from 'src/app/core/services/common/mapping.service';
+import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
 
 @Component({
   selector: 'app-netsuite-base-mapping',
@@ -7,9 +16,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetsuiteBaseMappingComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean = true;
+
+  destinationOptions: DestinationAttribute[];
+
+  employeeFieldMapping: FyleField;
+
+  sourceField: string;
+
+  destinationField: string;
+
+  showAutoMapEmployee: boolean;
+
+  reimbursableExpenseObject: NetsuiteReimbursableExpensesObject | null;
+
+  cccExpenseObject: NetSuiteCorporateCreditCardExpensesObject | null;
+
+  AppName = AppName;
+
+  FyleField = FyleField;
+
+  displayName: string | undefined = undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private mappingService: MappingService,
+    private toastService: IntegrationsToastService,
+    private workspaceService: WorkspaceService
+  ) { }
+
+  triggerAutoMapEmployees(): void {
+    this.isLoading = true;
+    this.mappingService.triggerAutoMapEmployees().subscribe(() => {
+      this.isLoading = false;
+      this.toastService.displayToastMessage(ToastSeverity.INFO, 'Auto mapping of employees may take few minutes');
+    }, () => {
+      this.isLoading = false;
+      this.toastService.displayToastMessage(ToastSeverity.ERROR, 'Something went wrong, please try again');
+    });
+  }
+
+  private getDestinationField(workspaceGeneralSetting: NetsuiteExportSettingWorkspaceGeneralSettingPost, mappingSettings: MappingSetting[]): string {
+    if (this.sourceField === FyleField.EMPLOYEE) {
+      return workspaceGeneralSetting.employee_field_mapping;
+    } else if (this.sourceField === FyleField.CATEGORY) {
+      return AccountingField.ACCOUNT;
+    }
+
+    return mappingSettings.find((setting) => setting.source_field === this.sourceField)?.destination_field || '';
+  }
+
+  private setupPage(): void {
+    this.sourceField = decodeURIComponent(this.route.snapshot.params.source_field.toUpperCase());
+    forkJoin([
+      this.workspaceService.getConfiguration(),
+      this.mappingService.getMappingSettings()
+    ]).subscribe((responses) => {
+      this.reimbursableExpenseObject = responses[0].reimbursable_expenses_object;
+      this.cccExpenseObject = responses[0].corporate_credit_card_expenses_object;
+      this.employeeFieldMapping = (responses[0].employee_field_mapping as unknown as FyleField);
+      this.showAutoMapEmployee = responses[0].auto_map_employees ? true : false;
+
+      this.destinationField = this.getDestinationField(responses[0], responses[1].results);
+
+      if (this.destinationField === AccountingField.ACCOUNT) {
+        this.displayName = responses[0].import_items ? `${AccountingDisplayName.ITEM},${AccountingDisplayName.ACCOUNT}` : AccountingDisplayName.ACCOUNT;
+      } else {
+        this.displayName = undefined;
+      }
+
+      this.mappingService.getPaginatedDestinationAttributes(this.destinationField, undefined, this.displayName).subscribe((responses) => {
+        this.destinationOptions = responses.results;
+        this.isLoading = false;
+      });
+    });
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe(() => {
+      this.isLoading = true;
+      this.setupPage();
+    });
   }
+
 
 }
