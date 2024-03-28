2fc7b548628a23bf2b92a82fda1a27fa7b788410
Fyle <fyle@Fyles-MacBook-Air.local>
2024-03-28 12:43:33 +0530

    Basic Mapping set up

--- src/app/integrations/netsuite/netsuite-main/netsuite-mapping/netsuite-mapping.component.ts
+++ src/app/integrations/netsuite/netsuite-main/netsuite-mapping/netsuite-mapping.component.ts
@@ -1,4 +1,12 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { brandingFeatureConfig, brandingConfig } from 'src/app/branding/branding-config';
+import { FyleField } from 'src/app/core/models/enum/enum.model';
+import { MappingService } from 'src/app/core/services/common/mapping.service';
+import { SentenceCasePipe } from 'src/app/shared/pipes/sentence-case.pipe';
+import { SnakeCaseToSpaceCasePipe } from 'src/app/shared/pipes/snake-case-to-space-case.pipe';
 
 @Component({
   selector: 'app-netsuite-mapping',
@@ -7,9 +15,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetsuiteMappingComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean = true;
+
+  mappingPages: MenuItem[] = [
+    {label: 'Employee', routerLink: '/integrations/netsuite/main/mapping/employee'},
+    {label: 'Category', routerLink: '/integrations/netsuite/main/mapping/category'}
+  ];
+
+  activeModule: MenuItem;
+
+  readonly isGradientAllowed: boolean = brandingFeatureConfig.isGradientAllowed;
+
+  readonly brandingConfig = brandingConfig;
+
+  constructor(
+    private mappingService: MappingService,
+    private router: Router
+  ) { }
+
+  private setupPage(): void {
+    this.mappingService.getMappingSettings().subscribe((response) => {
+      if (response.results && Array.isArray(response.results)) {
+        response.results.forEach((item) => {
+          if (item.source_field !== FyleField.EMPLOYEE && item.source_field !== FyleField.CATEGORY) {
+            const mappingPage = new SnakeCaseToSpaceCasePipe().transform(item.source_field);
+            this.mappingPages.push({
+              label: brandingConfig.brandId === 'co' ? new SentenceCasePipe().transform(mappingPage) : new TitleCasePipe().transform(mappingPage),
+              routerLink: `/integrations/netsuite/main/mapping/${encodeURIComponent(item.source_field.toLowerCase())}`
+            });
+          }
+        });
+      }
+      if (!brandingFeatureConfig.featureFlags.mapEmployees) {
+        this.mappingPages.splice(0, 1);
+      }
+      this.router.navigateByUrl(this.mappingPages[0].routerLink);
+      this.isLoading = false;
+    });
+  }
 
   ngOnInit(): void {
+    this.setupPage();
   }
 
 }
