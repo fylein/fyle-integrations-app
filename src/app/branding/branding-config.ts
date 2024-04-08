cccab0669db1c8df0de1fd1d56903957b51d23e1
Fyle <fyle@Fyles-MacBook-Air.local>
2024-04-08 14:14:09 +0530

    data flow svg change for qbo

--- src/app/branding/branding-config.ts
+++ src/app/branding/branding-config.ts
@@ -887,7 +887,7 @@ const content: ContentConfiguration = {
             filterPlaceholder: 'Select status'
         },
         landing: {
-            contentText: 'Import data from QuickBooks Online to ' + brandingConfig.brandName + ' and export expenses from ' + brandingConfig.brandName + ' to QuickBooks Online. ',
+            contentText: 'Import GL accounts and projects from QuickBooks Online and export expenses from your expense management account.',
             guideHeaderText: 'How to setup your integration'
         },
         common: {
