a392b35ef31a6bc60b6d4517290ccbe137e37c2a
Fyle <fyle@Fyles-MacBook-Air.local>
2024-04-23 12:49:28 +0530

    c1 contents correction accross the apps

--- src/app/core/models/branding/content-configuration.model.ts
+++ src/app/core/models/branding/content-configuration.model.ts
@@ -173,6 +173,7 @@ export type ContentConfiguration = {
                     }
                 },
                 advancedSettings: {
+                    frequencySubLabel: string;
                     stepName: string;
                     contentText: string;
                     scheduleAutoExport: string;
