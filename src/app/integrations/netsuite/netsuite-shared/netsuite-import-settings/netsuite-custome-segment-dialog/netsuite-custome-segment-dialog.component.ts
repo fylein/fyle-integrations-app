e7e28d8bc086e28bba525355008b907c0d3b5448
Fyle <fyle@Fyles-MacBook-Air.local>
2024-05-03 14:10:37 +0530

    netsuite custome segment dialog ts changes

--- src/app/integrations/netsuite/netsuite-shared/netsuite-import-settings/netsuite-custome-segment-dialog/netsuite-custome-segment-dialog.component.ts
+++ src/app/integrations/netsuite/netsuite-shared/netsuite-import-settings/netsuite-custome-segment-dialog/netsuite-custome-segment-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { SelectFormOption } from 'src/app/core/models/common/select-form-option.model';
 
 @Component({
   selector: 'app-netsuite-custome-segment-dialog',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetsuiteCustomeSegmentDialogComponent implements OnInit {
 
+  @Input() form: FormGroup;
+
+  @Input() isLoading: boolean;
+
+  @Input() options: SelectFormOption[];
+
+  @Output() saveClick = new EventEmitter();
+
+  isSave: boolean = false;
+
   constructor() { }
+
+  save() {
+    this.saveClick.emit();
+  }
 
   ngOnInit(): void {
   }
