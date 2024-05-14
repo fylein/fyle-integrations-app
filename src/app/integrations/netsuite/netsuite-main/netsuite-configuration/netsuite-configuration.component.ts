5afd6294c0d4ac518fab55412e5d3465136d674b
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-05-14 14:27:46 +0530

    bringing netsuite connector into configuration page

--- src/app/integrations/netsuite/netsuite-main/netsuite-configuration/netsuite-configuration.component.ts
+++ src/app/integrations/netsuite/netsuite-main/netsuite-configuration/netsuite-configuration.component.ts
@@ -9,12 +9,13 @@ import { brandingContent, brandingFeatureConfig, brandingConfig } from 'src/app/
 })
 export class NetsuiteConfigurationComponent implements OnInit {
 
-  readonly brandingContent = brandingContent.configuration;
+  readonly brandingContent = brandingContent.netsuite.configuration;
 
   modules: MenuItem[] = [
     {label: this.brandingContent.exportSetting.stepName, routerLink: '/integrations/netsuite/main/configuration/export_settings'},
     {label: this.brandingContent.importSetting.stepName, routerLink: '/integrations/netsuite/main/configuration/import_settings'},
-    {label: this.brandingContent.advancedSettings.stepName, routerLink: '/integrations/netsuite/main/configuration/advanced_settings'}
+    {label: this.brandingContent.advancedSettings.stepName, routerLink: '/integrations/netsuite/main/configuration/advanced_settings'},
+    {label: 'Connection', routerLink: '/integrations/netsuite/main/configuration/connector'}
   ];
 
   activeModule: MenuItem = this.modules[0];
