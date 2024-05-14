61dc2b97d35bbc8d2c2f3407f39167af37a7a38c
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2024-05-14 13:00:34 +0530

    Fix skip export category (#789)
    * Fix for Skip Export Category
    
    * Lint Fix
    
    * Fixes for Skip Export
    
    * Skip Export if
    
    * Fix Category
    
    * fix for category

--- src/app/shared/components/configuration/configuration-skip-export/configuration-skip-export.component.ts
+++ src/app/shared/components/configuration/configuration-skip-export/configuration-skip-export.component.ts
@@ -165,10 +165,10 @@ export class ConfigurationSkipExportComponent implements OnInit {
     } else if (rank === 2) {
       this.skipExportForm.controls.value2.reset();
     }
-    if (conditionSelected !== null) {
-      if (conditionSelected.is_custom === true) {
+    if (conditionSelected) {
+      if (conditionSelected.is_custom) {
         this.setCustomOperatorOptions(rank, conditionSelected.type);
-      } else if (conditionSelected.is_custom === false) {
+      } else if (!conditionSelected.is_custom) {
         if (rank === 1) {
           this.operatorFieldOptions1 = this.setDefaultOperatorOptions(
             conditionSelected.field_name
@@ -248,7 +248,8 @@ export class ConfigurationSkipExportComponent implements OnInit {
     if (
       conditionField === 'claim_number' ||
       conditionField === 'employee_email' ||
-      conditionField === 'report_title'
+      conditionField === 'report_title' ||
+      conditionField === 'category'
     ) {
       operatorList.push({
         value: 'iexact',
--- src/app/shared/components/si/helper/skip-export/skip-export.component.ts
+++ src/app/shared/components/si/helper/skip-export/skip-export.component.ts
@@ -196,6 +196,7 @@ export class SkipExportComponent implements OnInit {
   }
 
   resetFields(operator: AbstractControl, value: AbstractControl, conditionSelected: ConditionField, rank: number) {
+
     operator.reset();
     value.reset();
     if (rank === 1) {
@@ -203,10 +204,10 @@ export class SkipExportComponent implements OnInit {
     } else if (rank === 2) {
       this.valueOption2 = [];
     }
-    if (conditionSelected !== null) {
-      if (conditionSelected.is_custom === true) {
+    if (conditionSelected) {
+      if (conditionSelected.is_custom) {
         this.setCustomOperatorOptions(rank, conditionSelected.type);
-      } else if (conditionSelected.is_custom === false) {
+      } else if (!conditionSelected.is_custom) {
         if (rank === 1) {
           this.operatorFieldOptions1 = this.setDefaultOperatorOptions(
             conditionSelected.field_name
@@ -385,7 +386,8 @@ export class SkipExportComponent implements OnInit {
     if (
       conditionField === 'claim_number' ||
       conditionField === 'employee_email' ||
-      conditionField === 'report_title'
+      conditionField === 'report_title' ||
+      conditionField === 'category'
     ) {
       operatorList.push({
         value: 'iexact',
