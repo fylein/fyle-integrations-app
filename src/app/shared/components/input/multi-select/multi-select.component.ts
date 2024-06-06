1f7e69b19566998edf1f2a1bb2e62d12ee053087
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-06-06 18:53:25 +0530

    multi select css changes

--- src/app/shared/components/input/multi-select/multi-select.component.ts
+++ src/app/shared/components/input/multi-select/multi-select.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { brandingConfig } from 'src/app/branding/branding-config';
 
 @Component({
   selector: 'app-multi-select',
   templateUrl: './multi-select.component.html',
   styleUrls: ['./multi-select.component.scss']
 })
-export class MultiSelectComponent {
+export class MultiSelectComponent implements OnInit{
 
   @Input() form: FormGroup;
 
@@ -18,6 +19,38 @@ export class MultiSelectComponent {
 
   @Input() options: any[];
 
+  @Input() optionLabel: string;
+
+  @Input() displayAs: string;
+
+  @Input() isFilter: boolean = false;
+
+  @Input() filterBy: string;
+
+  @Input() isFieldMandatory: boolean = false;
+
+  @Input() selectedValue: string | null;
+
+  @Output() multiSelectChange = new EventEmitter();
+
+  @Output() removeItem = new EventEmitter();
+
+  readonly brandingConfig = brandingConfig;
+
   constructor() { }
+
+  onMultiSelectChange(value: any) {
+    this.multiSelectChange.emit();
+  }
+
+  removeIconClick() {
+    this.removeItem.emit();
+  }
+
+  isOverflowing(element: any): boolean {
+    return element.offsetWidth < element.scrollWidth;
+  }
+
+  ngOnInit(): void {}
 
 }
