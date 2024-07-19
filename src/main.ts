a5dcb5cb937ff2ec669d761cfb71055c5ad726ef
Viswas <viswas.h@fyle.in>
2024-07-19 14:31:09 +0530

    fix: init sentry only if DSN is present in env variables

--- src/main.ts
+++ src/main.ts
@@ -11,33 +11,37 @@ if (environment.production) {
 }
 
 
-Sentry.init({
-  dsn: environment.sentry_dsn,
+if (environment.sentry_dsn) {
+  Sentry.init({
+    dsn: environment.sentry_dsn,
 
-  integrations: [
-    // Registers and configures the Tracing integration,
-    // Which automatically instruments your application to monitor its
-    // Performance, including custom Angular routing instrumentation
-    Sentry.browserTracingIntegration(),
-    // Registers the Replay integration,
-    // Which automatically captures Session Replays
-    Sentry.replayIntegration()
-  ],
+    integrations: [
+      // Registers and configures the Tracing integration,
+      // Which automatically instruments your application to monitor its
+      // Performance, including custom Angular routing instrumentation
+      Sentry.browserTracingIntegration(),
+      // Registers the Replay integration,
+      // Which automatically captures Session Replays
+      Sentry.replayIntegration()
+    ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // Of transactions for tracing.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // Of transactions for tracing.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 
-  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-  // eslint-disable-next-line capitalized-comments
-  // tracePropagationTargets: ["localhost", 'staging.fyle.tech', 'app.fylehq.com'],
+    // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+    // eslint-disable-next-line capitalized-comments
+    // tracePropagationTargets: ["localhost", 'staging.fyle.tech', 'app.fylehq.com'],
 
-  // Capture Replay for 10% of all sessions,
-  // Plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0
-});
+    // Capture Replay for 10% of all sessions,
+    // Plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0
+  });
+} else {
+  console.error("[SENTRY] Not initialized: Missing environment variable SENTRY_DSN");
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
