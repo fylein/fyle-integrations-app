e9b2801cc485a9ed74aef9e8bde0efa1bc93d585
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-07-29 19:11:35 +0530

    import name with code from sage 300 cre mapping

--- src/app/integrations/sage300/sage300-main/sage300-mapping/sage300-base-mapping/sage300-base-mapping.component.ts
+++ src/app/integrations/sage300/sage300-main/sage300-mapping/sage300-base-mapping/sage300-base-mapping.component.ts
@@ -72,7 +72,8 @@ export class Sage300BaseMappingComponent implements OnInit {
       this.cccExpenseObject = exportSettingsResponse.corporate_credit_card_expenses_object;
       this.showAutoMapEmployee = exportSettingsResponse.auto_map_employees ? true : false;
       this.destinationField = this.getSourceType(mappingSettingsResponse.results);
-      this.isMultiLineOption = importSettingsResponse.import_code_fields?.length > 0 ? true : false;
+      this.isMultiLineOption = importSettingsResponse.import_code_fields?.length > 0;
+      console.log(this.isMultiLineOption)
       this.mappingService.getPaginatedDestinationAttributes(this.destinationField).subscribe((response: any) => {
         this.destinationOptions = response.results;
         this.isLoading = false;
