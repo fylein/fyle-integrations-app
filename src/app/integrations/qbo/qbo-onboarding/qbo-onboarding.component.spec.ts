a09f9f4ff5ac74860d70553369394fc89e575b4d
anishfyle <anishkumar.s@fyle.in>
2024-09-03 19:14:00 +0530

    qbo onboarding

--- src/app/integrations/qbo/qbo-onboarding/qbo-onboarding.component.spec.ts
+++ src/app/integrations/qbo/qbo-onboarding/qbo-onboarding.component.spec.ts
@@ -1,16 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { QboOnboardingComponent } from './qbo-onboarding.component';
+import { brandingFeatureConfig } from 'src/app/branding/branding-config';
 
-xdescribe('QboOnboardingComponent', () => {
+describe('QboOnboardingComponent', () => {
   let component: QboOnboardingComponent;
   let fixture: ComponentFixture<QboOnboardingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ QboOnboardingComponent ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(QboOnboardingComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,15 @@ xdescribe('QboOnboardingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize brandingFeatureConfig', () => {
+    expect(component.brandingFeatureConfig).toBe(brandingFeatureConfig);
+  });
+
+  it('should call ngOnInit', () => {
+    const ngOnInitSpy = spyOn(component, 'ngOnInit');
+    component.ngOnInit();
+    expect(ngOnInitSpy).toHaveBeenCalled();
   });
 });
