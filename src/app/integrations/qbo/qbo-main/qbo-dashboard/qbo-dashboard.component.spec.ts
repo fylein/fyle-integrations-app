c017b2f60e8a9baec072f2fb3554ddfe6bc3ffe0
anishfyle <anishkumar.s@fyle.in>
2024-09-27 15:05:24 +0530

    QBO Dashboard

--- src/app/integrations/qbo/qbo-main/qbo-dashboard/qbo-dashboard.component.spec.ts
+++ src/app/integrations/qbo/qbo-main/qbo-dashboard/qbo-dashboard.component.spec.ts
@@ -1,23 +1,261 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, discardPeriodicTasks, fakeAsync, tick } from '@angular/core/testing';
 import { QboDashboardComponent } from './qbo-dashboard.component';
+import { AccountingExportService } from 'src/app/core/services/common/accounting-export.service';
+import { DashboardService } from 'src/app/core/services/common/dashboard.service';
+import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
+import { QboExportSettingsService } from 'src/app/core/services/qbo/qbo-configuration/qbo-export-settings.service';
+import { QboImportSettingsService } from 'src/app/core/services/qbo/qbo-configuration/qbo-import-settings.service';
+import { of, throwError } from 'rxjs';
+import { AppName, TaskLogState, QBOTaskLogType, ReimbursableImportState, CCCImportState } from 'src/app/core/models/enum/enum.model';
+import { mockWorkspaceGeneralSettingsForDashboard, mockExportableExpenseGroup, mockExportSettingsForDashboard, mockImportSettingsForDashboard, mockExportErrors, mockQBOTaskResponse, mockAccountingExportSummary, mockExportSettings } from 'src/app/integrations/qbo/qbo.fixture';
+import { DashboardModel } from 'src/app/core/models/db/dashboard.model';
+import { AccountingExportSummaryModel } from 'src/app/core/models/db/accounting-export-summary.model';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { QBOImportSettingGet } from 'src/app/core/models/qbo/qbo-configuration/qbo-import-setting.model';
+import { QBOExportSettingGet } from 'src/app/core/models/qbo/qbo-configuration/qbo-export-setting.model';
 
-xdescribe('QboDashboardComponent', () => {
+describe('QboDashboardComponent', () => {
   let component: QboDashboardComponent;
   let fixture: ComponentFixture<QboDashboardComponent>;
+  let accountingExportService: jasmine.SpyObj<AccountingExportService>;
+  let dashboardService: jasmine.SpyObj<DashboardService>;
+  let workspaceService: jasmine.SpyObj<WorkspaceService>;
+  let qboExportSettingsService: jasmine.SpyObj<QboExportSettingsService>;
+  let importSettingService: jasmine.SpyObj<QboImportSettingsService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ QboDashboardComponent ]
-    })
-    .compileComponents();
+    const accountingExportServiceSpy = jasmine.createSpyObj('AccountingExportService', ['getAccountingExportSummary', 'importExpensesFromFyle']);
+    const dashboardServiceSpy = jasmine.createSpyObj('DashboardService', ['getExportErrors', 'getAllTasks', 'getExportableAccountingExportIds', 'triggerAccountingExport']);
+    const workspaceServiceSpy = jasmine.createSpyObj('WorkspaceService', ['getWorkspaceGeneralSettings']);
+    const qboExportSettingsServiceSpy = jasmine.createSpyObj('QboExportSettingsService', ['getExportSettings']);
+    const importSettingServiceSpy = jasmine.createSpyObj('QboImportSettingsService', ['getImportSettings']);
 
+    await TestBed.configureTestingModule({
+      declarations: [ QboDashboardComponent ],
+      providers: [
+        { provide: AccountingExportService, useValue: accountingExportServiceSpy },
+        { provide: DashboardService, useValue: dashboardServiceSpy },
+        { provide: WorkspaceService, useValue: workspaceServiceSpy },
+        { provide: QboExportSettingsService, useValue: qboExportSettingsServiceSpy },
+        { provide: QboImportSettingsService, useValue: importSettingServiceSpy }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+
+    accountingExportService = TestBed.inject(AccountingExportService) as jasmine.SpyObj<AccountingExportService>;
+    dashboardService = TestBed.inject(DashboardService) as jasmine.SpyObj<DashboardService>;
+    workspaceService = TestBed.inject(WorkspaceService) as jasmine.SpyObj<WorkspaceService>;
+    qboExportSettingsService = TestBed.inject(QboExportSettingsService) as jasmine.SpyObj<QboExportSettingsService>;
+    importSettingService = TestBed.inject(QboImportSettingsService) as jasmine.SpyObj<QboImportSettingsService>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(QboDashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize with correct default values', () => {
+    expect(component.isLoading).toBeTrue();
+    expect(component.appName).toBe(AppName.QBO);
+    expect(component.isImportInProgress).toBeTrue();
+    expect(component.isExportInProgress).toBeFalse();
+    expect(component.exportableAccountingExportIds).toEqual([]);
+    expect(component.failedExpenseGroupCount).toBe(0);
+    expect(component.exportProgressPercentage).toBe(0);
+    expect(component.accountingExportSummary).toBeUndefined();
+    expect(component.processedCount).toBe(0);
+  });
+
+  it('should setup page correctly', fakeAsync(() => {
+    dashboardService.getExportErrors.and.returnValue(of(mockExportErrors));
+    accountingExportService.getAccountingExportSummary.and.returnValue(of(mockAccountingExportSummary));
+    dashboardService.getAllTasks.and.returnValue(of(mockQBOTaskResponse));
+    workspaceService.getWorkspaceGeneralSettings.and.returnValue(of(mockWorkspaceGeneralSettingsForDashboard));
+    dashboardService.getExportableAccountingExportIds.and.returnValue(of(mockExportableExpenseGroup));
+    qboExportSettingsService.getExportSettings.and.returnValue(of(mockExportSettingsForDashboard as QBOExportSettingGet));
+    importSettingService.getImportSettings.and.returnValue(of(mockImportSettingsForDashboard as QBOImportSettingGet));
+    accountingExportService.importExpensesFromFyle.and.returnValue(of({}));
+    qboExportSettingsService.getExportSettings.and.returnValue(of(mockExportSettings));
+    component.getExportErrors$ = of(mockExportErrors);
+    component.getAccountingExportSummary$ = of(mockAccountingExportSummary);
+
+    component.ngOnInit();
+    tick();
+    component.getAccountingExportSummary$ = of(mockAccountingExportSummary);
+    expect(component.errors).toEqual(DashboardModel.parseAPIResponseToGroupedError(mockExportErrors));
+    expect(component.isImportItemsEnabled).toBe(mockWorkspaceGeneralSettingsForDashboard.import_items);
+    expect(component.accountingExportSummary).toEqual(AccountingExportSummaryModel.parseAPIResponseToAccountingSummary(mockAccountingExportSummary));
+    expect(component.destinationFieldMap).toEqual({
+      EMPLOYEE: mockWorkspaceGeneralSettingsForDashboard.employee_field_mapping,
+      CATEGORY: 'ACCOUNT'
+    });
+    expect(component.isLoading).toBeFalse();
+    expect(component.importCodeFields).toEqual(mockImportSettingsForDashboard.workspace_general_settings.import_code_fields);
+    expect(component.exportableAccountingExportIds).toEqual(mockExportableExpenseGroup.exportable_expense_group_ids);
+    expect(component.failedExpenseGroupCount).toBe(12);
+    expect(component.reimbursableImportState).toBe(ReimbursableImportState.PROCESSING);
+    expect(component.cccImportState).toBe(CCCImportState.APPROVED);
+    expect(component.isImportInProgress).toBeFalse();
+  }));
+
+  it('should handle error in getAccountingExportSummary', fakeAsync(() => {
+    dashboardService.getExportErrors.and.returnValue(of(mockExportErrors));
+    dashboardService.getAllTasks.and.returnValue(of(mockQBOTaskResponse));
+    workspaceService.getWorkspaceGeneralSettings.and.returnValue(of(mockWorkspaceGeneralSettingsForDashboard));
+    dashboardService.getExportableAccountingExportIds.and.returnValue(of(mockExportableExpenseGroup));
+    qboExportSettingsService.getExportSettings.and.returnValue(of(mockExportSettingsForDashboard as QBOExportSettingGet));
+    importSettingService.getImportSettings.and.returnValue(of(mockImportSettingsForDashboard as QBOImportSettingGet));
+    accountingExportService.importExpensesFromFyle.and.returnValue(of({}));
+
+    component.getExportErrors$ = of(mockExportErrors);
+    component.getAccountingExportSummary$ = throwError(() => new Error('API error'));
+
+    component.ngOnInit();
+    tick();
+
+    expect(component.accountingExportSummary).toBeUndefined();
+  }));
+
+  it('should trigger export and poll export status', fakeAsync(() => {
+    dashboardService.triggerAccountingExport.and.returnValue(of({}));
+    dashboardService.getAllTasks.and.returnValue(of({
+      count: 1,
+      next: null,
+      previous: null,
+      results: [{
+        id: 1,
+        status: TaskLogState.COMPLETE,
+        type: QBOTaskLogType.CREATING_BILL,
+        expense_group: 1
+      }]
+    }));
+
+    component.getExportErrors$ = of([]);
+    component.getAccountingExportSummary$ = of(mockAccountingExportSummary);
+
+    component.exportableAccountingExportIds = [1];
+    component.export();
+    tick(3000);
+
+    expect(component.isExportInProgress).toBeFalse();
+    expect(component.exportProgressPercentage).toBe(0);
+    expect(component.processedCount).toBe(0);
+    expect(dashboardService.triggerAccountingExport).toHaveBeenCalledTimes(1);
+    expect(dashboardService.getAllTasks).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should handle export with failed tasks', fakeAsync(() => {
+    dashboardService.triggerAccountingExport.and.returnValue(of({}));
+    dashboardService.getAllTasks.and.returnValue(of(mockQBOTaskResponse));
+
+    component.getExportErrors$ = of([]);
+    component.getAccountingExportSummary$ = of(mockAccountingExportSummary);
+
+    component.exportableAccountingExportIds = [16078, 16081];
+    component.export();
+    tick(3000);
+
+    expect(component.failedExpenseGroupCount).toBe(12);
+    expect(component.exportableAccountingExportIds).toEqual(mockQBOTaskResponse.results.filter(task => task.status === TaskLogState.FAILED || task.status === TaskLogState.FATAL).map(task => task.expense_group));
+    expect(component.isExportInProgress).toBeFalse();
+    expect(component.exportProgressPercentage).toBe(0);
+    expect(component.processedCount).toBe(0);
+  }));
+
+  it('should setup page with error handling', fakeAsync(() => {
+    dashboardService.getExportErrors.and.returnValue(of(mockExportErrors));
+    accountingExportService.getAccountingExportSummary.and.returnValue(throwError(() => new Error('API error')));
+    dashboardService.getAllTasks.and.returnValue(of(mockQBOTaskResponse));
+    workspaceService.getWorkspaceGeneralSettings.and.returnValue(of(mockWorkspaceGeneralSettingsForDashboard));
+    dashboardService.getExportableAccountingExportIds.and.returnValue(of(mockExportableExpenseGroup));
+    qboExportSettingsService.getExportSettings.and.returnValue(of(mockExportSettingsForDashboard as QBOExportSettingGet));
+    importSettingService.getImportSettings.and.returnValue(of(mockImportSettingsForDashboard as QBOImportSettingGet));
+    accountingExportService.importExpensesFromFyle.and.returnValue(of({}));
+
+    component.getExportErrors$ = of(mockExportErrors);
+    component.getAccountingExportSummary$ = throwError(() => new Error('API error'));
+
+    component.ngOnInit();
+    tick();
+    component.getAccountingExportSummary$ = of(mockAccountingExportSummary);
+    expect(component.failedExpenseGroupCount).toBe(12);
+    expect(component.reimbursableImportState).toBe(ReimbursableImportState.PAID);
+    expect(component.cccImportState).toBe(CCCImportState.PAID);
+    expect(component.accountingExportSummary).toBeUndefined();
+    expect(component.isLoading).toBeFalse();
+    expect(component.isImportInProgress).toBeFalse();
+    expect(component.isExportInProgress).toBeFalse();
+  }));
+
+  it('should handle queued tasks and start polling export status', fakeAsync(() => {
+    dashboardService.getExportErrors.and.returnValue(of(mockExportErrors));
+    accountingExportService.getAccountingExportSummary.and.returnValue(of(mockAccountingExportSummary));
+    dashboardService.getAllTasks.and.returnValue(of({
+      count: 2,
+      next: null,
+      previous: null,
+      results: [
+        {
+          id: 1,
+          status: TaskLogState.ENQUEUED,
+          type: QBOTaskLogType.CREATING_BILL,
+          expense_group: 1
+        },
+        {
+          id: 2,
+          status: TaskLogState.ENQUEUED,
+          type: QBOTaskLogType.CREATING_EXPENSE,
+          expense_group: 2
+        }
+      ]
+    }));
+    workspaceService.getWorkspaceGeneralSettings.and.returnValue(of(mockWorkspaceGeneralSettingsForDashboard));
+    dashboardService.getExportableAccountingExportIds.and.returnValue(of(mockExportableExpenseGroup));
+    qboExportSettingsService.getExportSettings.and.returnValue(of(mockExportSettingsForDashboard as QBOExportSettingGet));
+    importSettingService.getImportSettings.and.returnValue(of(mockImportSettingsForDashboard as QBOImportSettingGet));
+    accountingExportService.importExpensesFromFyle.and.returnValue(of({}));
+  
+    component.getExportErrors$ = of(mockExportErrors);
+    component.getAccountingExportSummary$ = of(mockAccountingExportSummary);
+  
+    spyOn<any>(component, 'pollExportStatus').and.callThrough();
+  
+    component.ngOnInit();
+    tick();
+  
+    expect(component.isExportInProgress).toBeTrue();
+    expect(component['pollExportStatus']).toHaveBeenCalledWith(mockExportableExpenseGroup.exportable_expense_group_ids);
+  
+    // Simulate completion of tasks
+    dashboardService.getAllTasks.and.returnValue(of({
+      count: 2,
+      next: null,
+      previous: null,
+      results: [
+        {
+          id: 1,
+          status: TaskLogState.COMPLETE,
+          type: QBOTaskLogType.CREATING_BILL,
+          expense_group: 1
+        },
+        {
+          id: 2,
+          status: TaskLogState.COMPLETE,
+          type: QBOTaskLogType.CREATING_EXPENSE,
+          expense_group: 2
+        }
+      ]
+    }));
+  
+    tick(6000);
+    discardPeriodicTasks();
+  
+    expect(component.isExportInProgress).toBeFalse();
+    // expect(component.exportProgressPercentage).toBe(100);
+    // expect(component.processedCount).toBe(2);
+  }));
 });
