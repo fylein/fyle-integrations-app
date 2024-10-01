549c8310d902ce700a33d01efca7cbda0c23c89a
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-10-01 15:55:23 +0530

    qbd direct post onboarding mapping page implementation

--- src/app/integrations/qbd-direct/qbd-direct-main/qbd-direct-mapping/qbd-direct-base-mapping/qbd-direct-base-mapping.component.ts
+++ src/app/integrations/qbd-direct/qbd-direct-main/qbd-direct-mapping/qbd-direct-base-mapping/qbd-direct-base-mapping.component.ts
@@ -1,12 +1,112 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { brandingConfig } from 'src/app/branding/c1-contents-config';
+import { DestinationAttribute } from 'src/app/core/models/db/destination-attribute.model';
+import { MappingSetting } from 'src/app/core/models/db/mapping-setting.model';
+import { FyleField, AppName, ToastSeverity, AccountingField, AccountingDisplayName, QBDReimbursableExpensesObject, QBDCorporateCreditCardExpensesObject } from 'src/app/core/models/enum/enum.model';
+import { QBOWorkspaceGeneralSetting } from 'src/app/core/models/qbo/db/workspace-general-setting.model';
+import { IntegrationsToastService } from 'src/app/core/services/common/integrations-toast.service';
+import { MappingService } from 'src/app/core/services/common/mapping.service';
+import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
+import { QbdDirectImportSettingsService } from 'src/app/core/services/qbd-direct/qbd-direct-configuration/qbd-direct-import-settings.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-qbd-direct-base-mapping',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './qbd-direct-base-mapping.component.html',
   styleUrl: './qbd-direct-base-mapping.component.scss'
 })
-export class QbdDirectBaseMappingComponent {
+export class QbdDirectBaseMappingComponent implements OnInit {
+
+  isLoading: boolean = true;
+
+  destinationOptions: DestinationAttribute[];
+
+  employeeFieldMapping: FyleField;
+
+  sourceField: string;
+
+  destinationField: string;
+
+  showAutoMapEmployee: boolean;
+
+  reimbursableExpenseObject: QBDReimbursableExpensesObject | null;
+
+  cccExpenseObject: QBDCorporateCreditCardExpensesObject | null;
+
+  AppName = AppName;
+
+  FyleField = FyleField;
+
+  displayName: string | undefined = undefined;
+
+  isMultiLineOption: boolean;
+
+  brandingConfig = brandingConfig;
+
+  constructor(
+    private route: ActivatedRoute,
+    private mappingService: MappingService,
+    private toastService: IntegrationsToastService,
+    private workspaceService: WorkspaceService
+  ) { }
+
+  triggerAutoMapEmployees(): void {
+    this.isLoading = true;
+    this.mappingService.triggerAutoMapEmployees().subscribe(() => {
+      this.isLoading = false;
+      this.toastService.displayToastMessage(ToastSeverity.INFO, 'Auto mapping of employees may take few minutes');
+    }, () => {
+      this.isLoading = false;
+      this.toastService.displayToastMessage(ToastSeverity.ERROR, 'Something went wrong, please try again');
+    });
+  }
+
+  private getDestinationField(workspaceGeneralSetting: QBOWorkspaceGeneralSetting, mappingSettings: MappingSetting[]): string {
+    if (this.sourceField === FyleField.EMPLOYEE) {
+      return workspaceGeneralSetting.employee_field_mapping;
+    } else if (this.sourceField === FyleField.CATEGORY) {
+      return AccountingField.ACCOUNT;
+    }
+
+    return mappingSettings.find((setting) => setting.source_field === this.sourceField)?.destination_field || '';
+  }
+
+  private setupPage(): void {
+    this.sourceField = decodeURIComponent(this.route.snapshot.params.source_field.toUpperCase());
+    forkJoin([
+      this.workspaceService.getWorkspaceGeneralSettings(),
+      this.mappingService.getMappingSettings()
+    ]).subscribe((responses) => {
+      this.reimbursableExpenseObject = responses[0].reimbursable_expenses_object;
+      this.cccExpenseObject = responses[0].corporate_credit_card_expenses_object;
+      this.employeeFieldMapping = (responses[0].employee_field_mapping as unknown as FyleField);
+      this.showAutoMapEmployee = responses[0].auto_map_employees ? true : false;
+
+      this.destinationField = this.getDestinationField(responses[0], responses[1].results);
+
+      if (this.destinationField === AccountingField.ACCOUNT) {
+        this.displayName = responses[0].import_items ? `${AccountingDisplayName.ITEM},${AccountingDisplayName.ACCOUNT}` : AccountingDisplayName.ACCOUNT;
+      } else {
+        this.displayName = undefined;
+      }
+
+      this.mappingService.getPaginatedDestinationAttributes(this.destinationField, undefined, this.displayName).subscribe((responses) => {
+        this.destinationOptions = responses.results;
+        this.isLoading = false;
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(() => {
+      this.isLoading = true;
+      this.setupPage();
+    });
+  }
 
 }
