80c9b4808eaed9180880dd12d9d26cdb1d7cda89
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-11-05 10:36:11 +0530

    PR comments fix

---
+++ src/app/shared/components/input/checkbox-button/checkbox-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkbox-button.component.html',
+  styleUrl: './checkbox-button.component.scss'
+})
+export class CheckboxButtonComponent {
+
+}
