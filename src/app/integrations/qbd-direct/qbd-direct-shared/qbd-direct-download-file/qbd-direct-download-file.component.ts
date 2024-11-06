24271834471de54edf10437dfb8a21bc6e862f6a
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-11-06 12:47:51 +0530

    download file Ui updation

--- src/app/integrations/qbd-direct/qbd-direct-shared/qbd-direct-download-file/qbd-direct-download-file.component.ts
+++ src/app/integrations/qbd-direct/qbd-direct-shared/qbd-direct-download-file/qbd-direct-download-file.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SharedModule } from "../../../../shared/shared.module";
 import { QbdDirectSharedComponent } from '../qbd-direct-shared.component';
 import { CardModule } from 'primeng/card';
+import { ConfigurationCta } from 'src/app/core/models/enum/enum.model';
+import { CommonModule } from '@angular/common';
+import { required } from '@rxweb/reactive-form-validators';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-qbd-direct-download-file',
   standalone: true,
-  imports: [SharedModule, QbdDirectSharedComponent, CardModule],
+  imports: [SharedModule, QbdDirectSharedComponent, CardModule, CommonModule, ProgressSpinnerModule],
   templateUrl: './qbd-direct-download-file.component.html',
   styleUrl: './qbd-direct-download-file.component.scss'
 })
 export class QbdDirectDownloadFileComponent {
-downloadFilePath: any;
+
+   isLoading: boolean = true;
+
+   showDownloadLink: boolean = true;
+
+   isStepCompleted: boolean = false;
+
+   isCompanyPathInvalid: boolean;
+
+  downloadFilePath: string;
+
+  ConfigurationCtaText = ConfigurationCta;
+
+  continueToNextStep() {
+  }
+
+  onDownloadClick() {
+    // Emit output
+  }
+
+  onManualDownload() {
+    // Emit output
+  }
+
+  onRetryClick() {
+    // Emit output
+  }
 
 }
