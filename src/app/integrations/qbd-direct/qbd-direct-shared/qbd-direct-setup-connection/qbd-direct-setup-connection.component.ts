5c0c1a22f29364d848f530fc9421b9bd6ee9a88b
DhaaraniCIT <dhaaranieaswari@gmail.com>
2024-11-06 15:10:49 +0530

    feat: qbd connector setup UI changes

--- src/app/integrations/qbd-direct/qbd-direct-shared/qbd-direct-setup-connection/qbd-direct-setup-connection.component.ts
+++ src/app/integrations/qbd-direct/qbd-direct-shared/qbd-direct-setup-connection/qbd-direct-setup-connection.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CardModule } from 'primeng/card';
+import { ConfigurationCta, QBDConnectionStatus } from 'src/app/core/models/enum/enum.model';
+import { brandingConfig } from 'src/app/branding/branding-config';
 
 @Component({
   selector: 'app-qbd-direct-setup-connection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule, CardModule],
   templateUrl: './qbd-direct-setup-connection.component.html',
   styleUrl: './qbd-direct-setup-connection.component.scss'
 })
 export class QbdDirectSetupConnectionComponent {
+
+  @Input({required: true}) password: string;
+
+  @Input({required: true}) isLoading: boolean;
+
+  @Input({required: true}) connectionStatus: QBDConnectionStatus;
+
+  @Input({required: true}) isStepCompleted: boolean;
+
+  @Input({required: true}) isCTAEnabled: boolean;
+
+  qbdConnectionStatus = QBDConnectionStatus;
+
+  ConfigurationCtaText = ConfigurationCta;
+
+  isPasswordShown: boolean = false;
+
+  readonly brandingConfig = brandingConfig;
+
+  onDoneClick(event: any) {
+    // Emit output
+  }
+
+  onNextClick() {
+    // Emit output
+  }
+
+  onClipboardCopy() {
+    // Copy password to clipboard
+  }
+
+  showPassword(isPasswordVisible: boolean) {
+  }
 
 }
--- src/app/integrations/qbd-direct/qbd-direct-shared/qbd-direct-shared.module.ts
+++ src/app/integrations/qbd-direct/qbd-direct-shared/qbd-direct-shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { QbdDirectDownloadFileComponent } from './qbd-direct-download-file/qbd-direct-download-file.component';
+import { QbdDirectSetupConnectionComponent } from './qbd-direct-setup-connection/qbd-direct-setup-connection.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     SharedModule,
-    QbdDirectDownloadFileComponent
+    QbdDirectDownloadFileComponent,
+    QbdDirectSetupConnectionComponent
   ],
   exports: [
-    QbdDirectDownloadFileComponent
+    QbdDirectDownloadFileComponent,
+    QbdDirectSetupConnectionComponent
   ]
 })
 export class QbdDirectSharedModule { }
