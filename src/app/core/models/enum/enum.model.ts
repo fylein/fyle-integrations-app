69d2ec053982e0a4f102b0363704b0fd2dbc8a88
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2024-12-20 16:33:41 +0530

    feat: qbd direct tracking for export and sync on click (#1134)
    * feat: qbd direct tracking for export and sync on click
    
    * wording change for connector page

--- src/app/core/models/enum/enum.model.ts
+++ src/app/core/models/enum/enum.model.ts
@@ -84,9 +84,9 @@ export enum TravelperkUpdateEvent {
 
 export enum QbdDirectUpdateEvent {
   CONFIRM_PRE_REQUISITES_QBD_DIRECT = 'Confirm Pre Requisites QuickBooks Desktop Connector',
-  CONNECT_QBD_DIRECT = 'Connect QuickBooks Desktop Connector',
-  PENDING_QWC_UPLOAD_QBD_DIRECT = 'Pending QWC Upload QuickBooks Desktop Connector',
-  DESTINATION_SYNC_COMPLETE_QBD_DIRECT = 'Destination Sync Complete QuickBooks Desktop Connector',
+  CONNECT_QBD_DIRECT = 'Step 2.1 - Connect QuickBooks Desktop Connector',
+  PENDING_QWC_UPLOAD_QBD_DIRECT = 'Step 2.2 - Upload QWC downloaded file to QuickBooks Desktop Connector',
+  DESTINATION_SYNC_COMPLETE_QBD_DIRECT = 'Step 2.3 - Sync destination entities with QuickBooks Desktop Connector',
   EXPORT_SETTING_QBD_DIRECT = 'Export Settings QuickBooks Desktop Connector',
   IMPORT_SETTINGS_QBD_DIRECT = 'Import Settings QuickBooks Desktop Connector',
   ADVANCED_SETTINGS_QBD_DIRECT = 'Advanced Settings QuickBooks Desktop Connector',
@@ -165,7 +165,9 @@ export enum ClickEvent {
   PREVIEW_INTACCT_EXPORT = 'Preview Sage Intacct Export',
   INTACCT_EXPORT = 'Export Sage Intacct',
   CONNECT_SAGE300 = 'Connect Sage 300 CRE',
-  CONNECT_BUSINESS_CENTRAL = 'Connect Dynamic 365 Business Central'
+  CONNECT_BUSINESS_CENTRAL = 'Connect Dynamic 365 Business Central',
+  QBD_DIRECT_EXPORT = 'Direct Export Quickbooks Desktop',
+  QBD_DIRECT_SYNC = 'Sync Quickbooks Desktop Direct'
 }
 
 export enum ProgressPhase {
