c1596168158457afbe98aa0a289254ba3a130eb4
Viswas Haridas <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2025-01-07 12:55:40 +0530

    fix: update incorrect memo structure key in sage300 advanced settings payload (#1138)

--- src/app/core/models/sage300/sage300-configuration/sage300-advanced-settings.model.ts
+++ src/app/core/models/sage300/sage300-configuration/sage300-advanced-settings.model.ts
@@ -3,7 +3,7 @@ import { ExpenseFilterPost, ExpenseFilterPayload, ConditionField } from "../../c
 import { JoinOption, Operator } from "../../enum/enum.model";
 
 export type Sage300AdvancedSetting = {
-  memo_structure: string[],
+  expense_memo_structure: string[],
   schedule_is_enabled: boolean,
   auto_create_vendor: boolean,
   interval_hours: number
@@ -23,7 +23,7 @@ export class Sage300AdvancedSettingModel {
   static mapAPIResponseToFormGroup(advancedSettings: Sage300AdvancedSettingGet | null, isSkipExportEnabled: boolean): FormGroup {
     const defaultMemoOptions: string[] = ['employee_email', 'purpose', 'category', 'spent_on', 'report_number', 'expense_link'];
     return new FormGroup({
-      memoStructure: new FormControl(advancedSettings?.memo_structure ? advancedSettings?.memo_structure : defaultMemoOptions),
+      memoStructure: new FormControl(advancedSettings?.expense_memo_structure ? advancedSettings?.expense_memo_structure : defaultMemoOptions),
       scheduleEnabled: new FormControl(advancedSettings?.schedule_is_enabled ? true : false),
       autoCreateVendor: new FormControl(advancedSettings?.auto_create_vendor ? true : false),
       scheduleAutoExportFrequency: new FormControl(advancedSettings?.interval_hours ? advancedSettings.interval_hours : 1),
@@ -33,7 +33,7 @@ export class Sage300AdvancedSettingModel {
 
   static createAdvancedSettingPayload(advancedSettingsForm: FormGroup): Sage300AdvancedSettingPost {
     return {
-      memo_structure: advancedSettingsForm.get('memoStructure')?.value ? advancedSettingsForm.get('memoStructure')?.value : null,
+      expense_memo_structure: advancedSettingsForm.get('memoStructure')?.value ? advancedSettingsForm.get('memoStructure')?.value : null,
       schedule_is_enabled: advancedSettingsForm.get('scheduleEnabled')?.value ? advancedSettingsForm.get('scheduleEnabled')?.value : false,
       interval_hours: advancedSettingsForm.get('scheduleEnabled')?.value ? advancedSettingsForm.get('scheduleAutoExportFrequency')?.value : null,
       auto_create_vendor: advancedSettingsForm.get('autoCreateVendor')?.value ? advancedSettingsForm.get('autoCreateVendor')?.value : false
