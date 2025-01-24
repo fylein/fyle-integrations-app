237cad2ef85ef3d0b4f98dbcee31a3147385673c
Viswas <viswas.h@fyle.in>
2025-01-24 13:33:06 +0530

    fix: remove unused variable and fix employeeFieldMapping init

--- src/app/integrations/qbo/qbo-shared/qbo-export-settings/qbo-export-settings.component.ts
+++ src/app/integrations/qbo/qbo-shared/qbo-export-settings/qbo-export-settings.component.ts
@@ -54,8 +54,6 @@ export class QboExportSettingsComponent implements OnInit {
 
   isImportItemsEnabled: boolean;
 
-  reimbursableExportTypes: SelectFormOption[];
-
   creditCardExportTypes = QBOExportSettingModel.getCreditCardExportTypes();
 
   cccExpenseStateOptions = QBOExportSettingModel.getCCCExpenseStateOptions();
@@ -498,7 +496,7 @@ return of(null);
     ]).subscribe(([exportSetting, workspaceGeneralSettings, destinationAttributes, bankAccounts, cccAccounts, accountsPayables, vendors]) => {
 
       this.exportSettings = exportSetting;
-      this.employeeFieldMapping = workspaceGeneralSettings?.employee_field_mapping || EmployeeFieldMapping.EMPLOYEE;
+      this.employeeFieldMapping = workspaceGeneralSettings?.employee_field_mapping;
       this.setLiveEntityExample(destinationAttributes);
       this.bankAccounts = bankAccounts.results.map((option) => QBOExportSettingModel.formatGeneralMappingPayload(option));
       this.cccAccounts = cccAccounts.results.map((option) => QBOExportSettingModel.formatGeneralMappingPayload(option));
@@ -508,7 +506,6 @@ return of(null);
 
       this.isImportItemsEnabled = workspaceGeneralSettings?.import_items || false;
 
-      this.reimbursableExportTypes = QBOExportSettingModel.getReimbursableExportTypeOptions(this.employeeFieldMapping);
       this.showNameInJournalOption = this.exportSettings.workspace_general_settings?.corporate_credit_card_expenses_object === QBOCorporateCreditCardExpensesObject.JOURNAL_ENTRY ? true : false;
 
       this.addMissingOptions();
