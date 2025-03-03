51431f2197ca26a87f32caf5bef5ed43be159c70
ashwin1111 <winash1998@gmail.com>
2025-03-03 14:06:22 +0530

    asd

--- src/app/integrations/intacct/intacct-main/intacct-mapping/intacct-mapping.component.spec.ts
+++ src/app/integrations/intacct/intacct-main/intacct-mapping/intacct-mapping.component.spec.ts
@@ -57,7 +57,7 @@ describe('IntacctMappingComponent', () => {
     fixture.detectChanges();
 
     expect(component.mappingPages.length).toBe(4);
-    expect(component.mappingPages[3].label).toBe('Sample custom field');
+    expect(component.mappingPages[3].label).toBe('Sample Custom Field');
     expect(component.mappingPages[3].routerLink).toBe('/integrations/intacct/main/mapping/sample_custom_field');
   });
 
