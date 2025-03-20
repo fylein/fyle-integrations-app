0bd4cc3ee20215ff6f4e592c7e1fb85f4248ff33
Viswas <viswas.h@fyle.in>
2025-03-20 21:38:26 +0530

    feat: show display names in intacct > mapping > tab switcher
    feat: add a 'common-resources' service and model to fetch display names

--- src/app/integrations/intacct/intacct-main/intacct-mapping/intacct-mapping.component.ts
+++ src/app/integrations/intacct/intacct-main/intacct-mapping/intacct-mapping.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { brandingConfig, brandingFeatureConfig, brandingStyle } from 'src/app/branding/branding-config';
 import { FyleField } from 'src/app/core/models/enum/enum.model';
+import { CommonResourcesService } from 'src/app/core/services/common/common-resources.service';
 import { SiMappingsService } from 'src/app/core/services/si/si-core/si-mappings.service';
 import { SentenceCasePipe } from 'src/app/shared/pipes/sentence-case.pipe';
 import { SnakeCaseToSpaceCasePipe } from 'src/app/shared/pipes/snake-case-to-space-case.pipe';
@@ -34,27 +35,59 @@ export class IntacctMappingComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private mappingService: SiMappingsService
+    private mappingService: SiMappingsService,
+    private commonResourcesService: CommonResourcesService
   ) { }
 
   private setupPages(): void {
+
+    if (!brandingFeatureConfig.featureFlags.mapEmployees) {
+      this.mappingPages.splice(0, 1);
+    }
+    this.router.navigateByUrl(this.mappingPages[0].routerLink);
+
     this.mappingService.getMappingSettings().subscribe((response) => {
       if (response.results && Array.isArray(response.results)) {
-        response.results.forEach((item) => {
-          if (item.source_field !== FyleField.EMPLOYEE && item.source_field !== FyleField.CATEGORY) {
-            const mappingPage = new SnakeCaseToSpaceCasePipe().transform(item.source_field);
-            this.mappingPages.push({
-              label: brandingFeatureConfig.featureFlags.exportSettings.transformContentToSentenceCase ? new SentenceCasePipe().transform(mappingPage) : new TitleCasePipe().transform(mappingPage),
-              routerLink: `/integrations/intacct/main/mapping/${encodeURIComponent(item.source_field.toLowerCase())}`
-            });
-          }
+
+        // Show display names only for projects and cost centers
+        const attributeTypes = response.results
+          .map((item) => item.source_field)
+          .filter(fieldType => fieldType === FyleField.PROJECT || fieldType === FyleField.COST_CENTER);
+
+        this.commonResourcesService.getDimensionDetails({sourceType: 'FYLE', attributeTypes}).subscribe((dimensionDetails) => {
+          /**
+           * For every mapping
+           * 1. Get the source_field
+           * 2. Check if the source_field has a display name in dimension details
+           *   - If it does, use the display name
+           *   - If it doesn't, use the source_field (space cased and sentence cased)
+           */
+          response.results.forEach((item) => {
+            if (item.source_field !== FyleField.EMPLOYEE && item.source_field !== FyleField.CATEGORY) {
+              const mappingPage = new SnakeCaseToSpaceCasePipe().transform(item.source_field);
+
+              const displayName = dimensionDetails.results.find((detail) => detail.attribute_type === item.source_field)?.display_name;
+
+              let label;
+              if (displayName) {
+                label = displayName;
+              } else if (brandingFeatureConfig.featureFlags.exportSettings.transformContentToSentenceCase) {
+                label = new SentenceCasePipe().transform(mappingPage);
+              } else {
+                label = new TitleCasePipe().transform(mappingPage);
+              }
+
+              this.mappingPages.push({
+                label,
+                routerLink: `/integrations/intacct/main/mapping/${encodeURIComponent(item.source_field.toLowerCase())}`
+              });
+            }
+          });
+          this.isLoading = false;
         });
+      } else {
+        this.isLoading = false;
       }
-      if (!brandingFeatureConfig.featureFlags.mapEmployees) {
-        this.mappingPages.splice(0, 1);
-      }
-      this.router.navigateByUrl(this.mappingPages[0].routerLink);
-      this.isLoading = false;
     });
   }
 
