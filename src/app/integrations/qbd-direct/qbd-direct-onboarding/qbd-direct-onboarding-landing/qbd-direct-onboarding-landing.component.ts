90e83218edec6b46c2b569ab8da4fb7ccfe863a0
Viswas Haridas <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2025-03-28 12:41:20 +0530

    fix: qbd landing page redirection and article links fix (#1236)
    * fix: users being redirected to qbd direct landing after onboarding
    
    If the admin ends up on the onboarding landing page, redirect them based on the onboarding state
    
    * fix: delete unintended testing code
    
    * fix: article links in qbo direct (#1237)

--- src/app/integrations/qbd-direct/qbd-direct-onboarding/qbd-direct-onboarding-landing/qbd-direct-onboarding-landing.component.ts
+++ src/app/integrations/qbd-direct/qbd-direct-onboarding/qbd-direct-onboarding-landing/qbd-direct-onboarding-landing.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { brandingConfig, brandingContent, brandingDemoVideoLinks, brandingKbArticles } from 'src/app/branding/branding-config';
 import { AppName, QbdDirectOnboardingState } from 'src/app/core/models/enum/enum.model';
 import { QbdDirectWorkspace } from 'src/app/core/models/qbd-direct/db/qbd-direct-workspaces.model';
 import { WorkspaceService } from 'src/app/core/services/common/workspace.service';
+import { UserService } from 'src/app/core/services/misc/user.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
@@ -14,7 +15,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   templateUrl: './qbd-direct-onboarding-landing.component.html',
   styleUrl: './qbd-direct-onboarding-landing.component.scss'
 })
-export class QbdDirectOnboardingLandingComponent {
+export class QbdDirectOnboardingLandingComponent implements OnInit {
 
   appName: AppName = AppName.QBD_DIRECT;
 
@@ -30,7 +31,8 @@ export class QbdDirectOnboardingLandingComponent {
 
   constructor(
     private router: Router,
-    private workspaceService: WorkspaceService
+    private workspaceService: WorkspaceService,
+    private userService: UserService
   ) { }
 
   connectQbdDirect() {
@@ -39,6 +41,15 @@ export class QbdDirectOnboardingLandingComponent {
       this.workspaceService.setOnboardingState(workspaceResponse.onboarding_state);
       this.isQbdConnectionInProgress = false;
       this.router.navigate([`/integrations/qbd_direct/onboarding/pre_requisite/`]);
+    });
+  }
+
+  ngOnInit(): void {
+    const user = this.userService.getUserProfile();
+    this.workspaceService.getWorkspace(user.org_id).subscribe((workspaces: QbdDirectWorkspace[]) => {
+      if (workspaces.length && workspaces[0]?.onboarding_state !== QbdDirectOnboardingState.YET_TO_START) {
+        this.router.navigate([`/integrations/qbd_direct`]);
+      }
     });
   }
 
