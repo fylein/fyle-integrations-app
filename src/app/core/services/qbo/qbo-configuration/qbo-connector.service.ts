137cc1ed137a4c637a46c54cae1df5a1a1d51037
Vignesh Vicky <vignesh.s@fylehq.com>
2025-04-15 12:20:23 +0530

    feat: q2_update_token_expired_screen_qbo_xero (#1239)
    * q2_update_token_expired_screen_qbo_xero
    
    * feat: oauth-implementation
    
    * Revert guard token expiry detection created as seprate PR
    
    * minor changes to manage isLoading while connecting to integration
    
    * ngIf to @if
    
    * commit fixes
    
    * feat: disconnect handling
    
    * added disconnect subject
    
    * main menu disconnect added.
    
    * feat: token-expiry-detection (#1252)
    
    * token-expiry-detection
    
    * added disconnect detection for qbo
    
    * remove unwanted files
    
    * commit fixes
    
    * lint fix
    
    * minor changes
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>
    
    * memory leak issue fix
    
    * sentence case fix
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>

--- src/app/core/services/qbo/qbo-configuration/qbo-connector.service.ts
+++ src/app/core/services/qbo/qbo-configuration/qbo-connector.service.ts
@@ -38,6 +38,11 @@ export class QboConnectorService {
     return this.apiService.get(`/workspaces/${this.workspaceService.getWorkspaceId()}/credentials/qbo/`, {});
   }
 
+  @Cacheable()
+  checkQBOTokenHealth(): Observable<{}> {
+    return this.apiService.get(`/workspaces/${this.workspaceService.getWorkspaceId()}/token_health/`, {});
+  }
+
   @Cacheable({
     cacheBusterObserver: qboCredentialsCache$
   })
@@ -46,8 +51,4 @@ export class QboConnectorService {
     return this.apiService.patch(`/workspaces/${this.workspaceService.getWorkspaceId()}/credentials/qbo/`, {});
   }
 
-  @Cacheable()
-  getPreferences(): Observable<{}> {
-    return this.apiService.get(`/workspaces/${this.workspaceService.getWorkspaceId()}/qbo/preferences/`, {});
-  }
 }
