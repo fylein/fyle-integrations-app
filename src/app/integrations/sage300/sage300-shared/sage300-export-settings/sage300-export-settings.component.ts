012bee25e10ab06ac1409d68a19e3943d4b46fb8
Viswas <viswas.h@fyle.in>
2025-04-24 12:30:31 +0530

    fix: revert standardization for sage 300
    keep the old behaviour since we dont support `spent_at` and `current_date`

--- src/app/integrations/sage300/sage300-shared/sage300-export-settings/sage300-export-settings.component.ts
+++ src/app/integrations/sage300/sage300-shared/sage300-export-settings/sage300-export-settings.component.ts
@@ -121,11 +121,13 @@ export class Sage300ExportSettingsComponent implements OnInit {
 
   private setupCustomWatchers(): void {
     this.exportSettingForm.controls.reimbursableExportGroup?.valueChanges.subscribe((reimbursableExportGroup) => {
-      this.reimbursableExpenseGroupingDateOptions = CommonExportSettingModel.constructExportDateOptions(false, reimbursableExportGroup, this.exportSettingForm.controls.reimbursableExportDate.value);
+      this.reimbursableExpenseGroupingDateOptions = this.exportSettingService.getReimbursableExpenseGroupingDateOptions();
+      this.reimbursableExpenseGroupingDateOptions = CommonExportSettingModel.constructGroupingDateOptions(reimbursableExportGroup, this.reimbursableExpenseGroupingDateOptions);
     });
 
     this.exportSettingForm.controls.cccExportGroup?.valueChanges.subscribe((cccExportGroup) => {
-      this.cccExpenseGroupingDateOptions = CommonExportSettingModel.constructExportDateOptions(true, cccExportGroup, this.exportSettingForm.controls.cccExportDate.value);
+      this.cccExpenseGroupingDateOptions = this.exportSettingService.getCCCExpenseGroupingDateOptions();
+      this.cccExpenseGroupingDateOptions = CommonExportSettingModel.constructGroupingDateOptions(cccExportGroup, this.cccExpenseGroupingDateOptions);
     });
   }
 
