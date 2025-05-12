37bf7ac741421b16ba5610e25f1a7c3d5b22f7e4
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2025-05-12 23:28:07 +0530

    feat: introduce real-time export option in advanced setting (#1294)
    * feat: introduce real-time export option in advanced setting
    
    * add width
    
    * update clone setting option as well

--- src/app/integrations/xero/xero-onboarding/xero-clone-settings/xero-clone-settings.component.ts
+++ src/app/integrations/xero/xero-onboarding/xero-clone-settings/xero-clone-settings.component.ts
@@ -96,12 +96,13 @@ export class XeroCloneSettingsComponent implements OnInit {
 
   org: Org = this.orgService.getCachedOrg();
 
-  scheduleIntervalHours: SelectFormOption[] = [...Array(24).keys()].map(day => {
-    return {
-      label: (day + 1).toString(),
-      value: day + 1
-    };
-  });
+  scheduleIntervalHours: SelectFormOption[] = [
+    { label: 'Real-time', value: 0 },
+    ...[...Array(24).keys()].map(hour => ({
+      label: `${hour + 1} hour${hour + 1 > 1 ? 's' : ''}`,
+      value: hour + 1
+    }))
+  ];
 
   advancedSettingForm: FormGroup<any>;
 
