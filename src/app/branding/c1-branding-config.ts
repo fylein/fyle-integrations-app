001d56559b56f45bf0f9c35e0728a8aa960adb5c
Vignesh Vicky <vignesh.s@fylehq.com>
2025-06-09 11:44:32 +0530

    feat: Assisted setup for QuickBooks desktop direct integration (#1309)
    * initial commit
    
    * feat: need help with setup
    
    * fix merge conflits
    
    * debug staging window.alert
    
    * isSlotbooked check fix
    
    * lint fix
    
    * fix isSlotBook Subject
    
    * bug fix isSlotBooking check
    
    * feat need help with setup success dialog
    
    * comment fixes
    
    * fix dead code
    
    * added success toast for book_slot
    
    * comment fix
    
    * unit test fix & minor change
    
    * changes to fix pr-2
    
    * feat: assisted setup for QBD direct onboarding page changes (#1314)
    
    * initial commit
    
    * ng test fix
    
    * comment changes
    
    * sprite update
    
    * comment changes
    
    * minor change
    
    * bug fix
    
    * comment minor change
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>
    
    * Updated sprite.svg
    
    * fix: qbd direct assisted-setup bug fix & missing features (#1324)
    
    * assisted setup bug fix
    
    * assisted setup bug fix
    
    * fix tooltip & tailwind
    
    * icom fix
    
    * gcal link added
    
    * favicon update
    
    * button size fix
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>

--- src/app/branding/c1-branding-config.ts
+++ src/app/branding/c1-branding-config.ts
@@ -140,8 +140,9 @@ export const c1KbArticles: KbArticle[string] = {
             IMPORT_SETTING: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration#h_78e1747002`,
             EXPORT_SETTING: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration#h_eebe5df4b7`,
             ADVANCED_SETTING: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration#h_498f2acc61`,
-            CONNECTOR: `${brandingConfig.helpArticleDomain}/en/articles/9081356-generate-credentials-to-connect-with-sage-intacct`,
-            HELPER_ARTICLE: `${brandingConfig.helpArticleDomain}/en/articles/7882821-how-to-skip-exporting-specific-expenses-from-fyle-to-sage-intacct`
+            CONNECTOR: `${brandingConfig.helpArticleDomain}/en/articles/10259583-quickbooks-desktop-integration-beta#h_d3cc42849a`,
+            HELPER_ARTICLE: `${brandingConfig.helpArticleDomain}/en/articles/7882821-how-to-skip-exporting-specific-expenses-from-fyle-to-sage-intacct`,
+            GCAL_LINK: `https://calendar.app.google/xRwaKsiwEYukqigx9`
         }
     }
 };
