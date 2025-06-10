3051572865b60636c3adb5a73746cee84f1c9090
ashwin1111 <winash1998@gmail.com>
2025-06-10 19:32:09 +0530

    fix

--- src/app/core/services/common/accounting-export.service.ts
+++ src/app/core/services/common/accounting-export.service.ts
@@ -24,13 +24,13 @@ export class AccountingExportService {
 
   getAccountingExportSummary(version?: string | 'v1', useRepurposedExportSummary?: boolean, appName?: AppName): Observable<AccountingExportSummary> {
     const apiParams: { start_date?: string } = {};
+    if (useRepurposedExportSummary && appName && [AppName.XERO, AppName.QBO, AppName.NETSUITE, AppName.INTACCT, AppName.QBD_DIRECT].includes(appName)) {
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      apiParams.start_date = today.toISOString();
+    }
     if (version === 'v1') {
       // Temporary hack to enable repurposed export summary only for allowed apps - #q2_real_time_exports_integrations
-      if (useRepurposedExportSummary && appName && [AppName.XERO, AppName.QBO, AppName.NETSUITE, AppName.INTACCT, AppName.QBD_DIRECT].includes(appName)) {
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-        apiParams.start_date = today.toISOString();
-      }
       return this.apiService.get(`/workspaces/${this.workspaceService.getWorkspaceId()}/export_detail/`, apiParams);
     } else if (version === AppName.QBD_DIRECT) {
       return this.apiService.get(`/workspaces/${this.workspaceService.getWorkspaceId()}/export_logs/summary/`, apiParams);
