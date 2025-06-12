7efef5909331769ba65dfce5466e33f5de6ea091
Vignesh Vicky <vignesh.s@fylehq.com>
2025-06-12 13:24:25 +0530

    fix: qbd direct assisted setup article link changes (#1335)
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>

--- src/app/branding/c1-branding-config.ts
+++ src/app/branding/c1-branding-config.ts
@@ -141,6 +141,7 @@ export const c1KbArticles: KbArticle[string] = {
             EXPORT_SETTING: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration#h_eebe5df4b7`,
             ADVANCED_SETTING: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration#h_498f2acc61`,
             CONNECTOR: `${brandingConfig.helpArticleDomain}/en/articles/10259583-quickbooks-desktop-integration-beta#h_d3cc42849a`,
+            ASSISTED_SETUP_ARTICLE_LINK: `${brandingConfig.helpArticleDomain}/en/articles/10259583-quickbooks-desktop-integration`,
             HELPER_ARTICLE: `${brandingConfig.helpArticleDomain}/en/articles/7882821-how-to-skip-exporting-specific-expenses-from-fyle-to-sage-intacct`,
             GCAL_LINK: `https://calendar.app.google/xRwaKsiwEYukqigx9`
         }
