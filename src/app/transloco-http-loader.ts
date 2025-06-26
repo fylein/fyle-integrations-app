54cea9826f31bf7b63a0eca2ef624a4bd34ba7f9
aditya <aditya.b@fyle.in>
2025-06-26 15:38:57 +0530

    wip

--- src/app/transloco-http-loader.ts
+++ src/app/transloco-http-loader.ts
@@ -1,12 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
 import { HttpBackend, HttpClient } from '@angular/common/http';
+import { forkJoin, map, Observable } from 'rxjs';
+import { brandingConfig } from './branding/branding-config';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = new HttpClient(inject(HttpBackend));
 
-  getTranslation(lang: string) {
-    return this.http.get<Translation>(`./assets/i18n/${lang}.json`);
+  getTranslation(lang: string): Observable<Translation> {
+    const brandId = brandingConfig.brandId;
+    return forkJoin({
+      brandSpecificContent: this.http.get<Translation>(`./assets/i18n/${brandId}/${lang}.json`),
+      app: this.http.get<Translation>(`./assets/i18n/common/${lang}.json`)
+    }).pipe(map(({brandSpecificContent, app}) => ({
+      ...brandSpecificContent,
+      ...app
+    })));
   }
 }
