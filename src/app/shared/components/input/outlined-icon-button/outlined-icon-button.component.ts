7407b931088d615cdab4b3801d89f04a858a4303
Vignesh <vignesh@Vigneshs-MacBook-Air.local>
2025-07-18 00:09:55 +0530

    lint fix

--- src/app/shared/components/input/outlined-icon-button/outlined-icon-button.component.ts
+++ src/app/shared/components/input/outlined-icon-button/outlined-icon-button.component.ts
@@ -18,7 +18,7 @@ export class OutlinedIconButtonComponent {
   readonly brandingFeatureConfig = brandingFeatureConfig;
 
   readonly brandingConfig = brandingConfig;
-  
+
   onClick(): void {
     this.buttonClick.emit();
   }
