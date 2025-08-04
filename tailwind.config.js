07cd5726d15f36ac63b589c0f744d22e2dfac6db
Vignesh Vicky <vignesh.s@fylehq.com>
2025-08-04 15:30:35 +0530

    feat: theme sage primary/secondary buttons (#1408)
    * initial push
    
    * feat: theme sage primary buttons support (#1410)
    
    * primary support
    
    * final push all buttons
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 
 const customColors = {
   white: 'var(--white)',
+  green: 'var(--bg-green)',
   placeholder: 'var(--placeholder)',
   separator: 'var(--separator)',
   'normal-text-color': 'var(--normal-text-color)',
   'slightly-normal-text-color': 'var(--slightly-normal-text-color)',
+  'green-text-color': 'var(--mandatory-field-color)',
   'menu-inactive-text-color': 'var(--menu-inactive-text-color)',
   'sub-text-color': 'var(--sub-text-color)',
   'faded-text-color': 'var(--faded-text-color)',
@@ -30,6 +32,7 @@ const customColors = {
   'bg-warning-lighter': 'var(--bg-warning-lighter)',
   'icon-warning': 'var(--icon-warning)',
   'bg-tertiary-lighter': 'var(--bg-tertiary-lighter)',
+  'bg-grey-primary': 'var(--bg-grey-primary)',
 };
 
 const spacings = {
@@ -119,6 +122,7 @@ const borderColors = {
   'border-disable': 'var(--border-disable)',
   'border-neutral': 'var(--border-neutral)',
   'border-date-picker-border': 'var(--border-date-picker-border)',
+  'border-green': 'var(--bg-green)'
 };
 
 const colors = {
@@ -134,6 +138,7 @@ const colors = {
   'bg-disable': 'var(--bg-disable)',
   'bg-tertiary': 'var(--bg-tertiary)',
   'bg-white': 'var(--bg-white)',
+  'bg-green': 'var(--bg-green)',
   'bg-primary-opacity': 'var(--bg-primary-opacity)',
   //regular bg'
   'bg-success': 'var(--bg-success)',
@@ -1304,6 +1309,7 @@ module.exports = {
         '12-px': '12px',
         '16-px': '16px',
         '20-px': '20px',
+        '32-px': '32px',
         '48-px': '48px',
         '99-px': '99px',
         '50': '50%',
