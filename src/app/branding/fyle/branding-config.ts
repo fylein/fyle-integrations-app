45d00521271c8eed760f0d9e54ffda7fc33b6b6d
Vignesh Vicky <vignesh.s@fylehq.com>
2025-08-08 21:54:07 +0530

    feat: theme sage dialog modifications (#1431)
    * dialog final version
    
    * comment fixes
    
    ---------
    
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>

--- src/app/branding/fyle/branding-config.ts
+++ src/app/branding/fyle/branding-config.ts
@@ -18,6 +18,7 @@ export const fyleFeatureConfig: FeatureConfiguration[string] = {
     loginRedirectUri: false,
     loginToAllConnectedApps: true,
     isDashboardButtonOutlined: true,
+    cancelButtonPosition: 'right',
     isPrimengLoaderEnabled: false,
     featureFlags: {
         cloneSettings: true,
