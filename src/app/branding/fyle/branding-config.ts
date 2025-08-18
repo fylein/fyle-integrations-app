5ea656f0506f9f5135381912edda30fba5c49008
Vignesh Vicky <vignesh.s@fylehq.com>
2025-08-18 12:01:41 +0530

    revert: theme sage button left position changes (#1456)
    Co-authored-by: Vignesh <vignesh@Vigneshs-MacBook-Air.local>

--- src/app/branding/fyle/branding-config.ts
+++ src/app/branding/fyle/branding-config.ts
@@ -18,7 +18,6 @@ export const fyleFeatureConfig: FeatureConfiguration[string] = {
     loginRedirectUri: false,
     loginToAllConnectedApps: true,
     isDashboardButtonOutlined: true,
-    cancelButtonPosition: 'right',
     isPrimengLoaderEnabled: false,
     featureFlags: {
         cloneSettings: true,
