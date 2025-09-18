511c6cadbeb82d86c42a19e0b50639e2200ad487
Viswas <viswas.h@fyle.in>
2025-09-18 20:28:59 +0530

    feat: CSV upload button UI

--- src/app/shared/components/configuration/configuration-csv-upload-field/configuration-csv-upload-field.component.ts
+++ src/app/shared/components/configuration/configuration-csv-upload-field/configuration-csv-upload-field.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { Sage300SharedModule } from "src/app/integrations/sage300/sage300-shared/sage300-shared.module";
 import { ButtonSize, ButtonType } from 'src/app/core/models/enum/enum.model';
+import { CsvUploadButtonComponent } from "../../input/csv-upload-button/csv-upload-button.component";
 
 @Component({
   selector: 'app-configuration-csv-upload-field',
   standalone: true,
-  imports: [TranslocoModule, Sage300SharedModule],
+  imports: [TranslocoModule, Sage300SharedModule, CsvUploadButtonComponent],
   templateUrl: './configuration-csv-upload-field.component.html',
   styleUrl: './configuration-csv-upload-field.component.scss'
 })
