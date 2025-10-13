7f7763f0aee840d4c82a19e8a43fa2912492188f
Vignesh Vicky <vignesh.s@fylehq.com>
2025-10-13 12:36:46 +0530

    feat: Sage branding using isOrgRebranded for BrandName and assets (#1539)
    * rebrand
    
    * Proxy only brandingConfig
    
    ---------
    
    Co-authored-by: vignesh05904 <vignesh.s@fyle.in>

--- src/app/core/services/common/branding.service.ts
+++ src/app/core/services/common/branding.service.ts
@@ -1,7 +1,9 @@
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { brandingConfig } from 'src/app/branding/branding-config';
+import { BrandingConfiguration } from '../../models/branding/branding-configuration.model';
+import { updateBrandingConfigRegistry, defaultBrandingConfig } from 'src/app/branding/branding-config';
+import { FeatureConfiguration } from '../../models/branding/feature-configuration.model';
 
 const faviconMap = {
   'fyle': 'favicon.ico',
@@ -13,15 +15,41 @@ const faviconMap = {
 })
 export class BrandingService {
 
+  isOrgRebranded: boolean = false;
+
+  private defaultBrandingConfig: BrandingConfiguration = defaultBrandingConfig;
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private title: Title
   ) { }
 
+  get brandingConfig(): BrandingConfiguration {
+    return this.defaultBrandingConfig;
+  }
+
+  getAssetsBrandId(): string {
+    if (this.defaultBrandingConfig.brandId === 'fyle' && this.isOrgRebranded) {
+      return 'sage';
+    }
+
+    return this.defaultBrandingConfig.brandId;
+  }
+
+  setOrgRebranded(isOrgRebranded: boolean): void {
+    this.isOrgRebranded = isOrgRebranded;
+  }
+
+  updateBrandingConfig(config: Partial<BrandingConfiguration>): void {
+    const brandingConfig = { ...this.defaultBrandingConfig, ...config };
+    this.defaultBrandingConfig = brandingConfig;
+    updateBrandingConfigRegistry(brandingConfig);
+  }
+
   private setupBrandingConfig(): void {
-    this.title.setTitle(brandingConfig.webpageTitle);
-    this.document.getElementById('appFavicon')?.setAttribute('href', `assets/${brandingConfig.brandId}/${faviconMap[brandingConfig.brandId]}`);
-    this.document.documentElement.setAttribute('data-theme', brandingConfig.brandId);
+    this.title.setTitle(this.brandingConfig.webpageTitle);
+    this.document.getElementById('appFavicon')?.setAttribute('href', `assets/${this.brandingConfig.brandId}/${faviconMap[this.brandingConfig.brandId]}`);
+    this.document.documentElement.setAttribute('data-theme', this.brandingConfig.brandId);
   }
 
   init(): void {
