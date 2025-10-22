d9ad33c680c409f8f700d40ba2f628cf8efde691
Vignesh Vicky <vignesh.s@fylehq.com>
2025-10-22 16:58:42 +0530

    feat: sage50 dashboard page changes (#1569)
    * sage50-dashboard-1
    
    * mappings
    
    * comment fix
    
    * comment fix mapping
    
    * comment fix destination field
    
    * handle mapping page dynamically
    
    * update
    
    * fix: add advanced settings to configurations page
    
    * feat: sage50 dashboard export-summary changes (#1591)
    
    * base fix
    
    * forkJoin fix
    
    * dashboard final export-summary
    
    * dialog header spacing 4px
    
    * sage50 final
    
    * comment fixes
    
    * fix: flowchart and /workspaces/?org_id caching (#1594)
    
    Co-authored-by: vignesh05904 <vignesh.s@fyle.in>
    
    ---------
    
    Co-authored-by: vignesh05904 <vignesh.s@fyle.in>
    
    ---------
    
    Co-authored-by: vignesh05904 <vignesh.s@fyle.in>
    Co-authored-by: Viswas <viswas.h@fyle.in>

--- src/app/integrations/sage50/sage50-routing.ts
+++ src/app/integrations/sage50/sage50-routing.ts
@@ -39,6 +39,59 @@ export const SAGE50_ROUTES: Routes = [
             loadComponent: () => import('./sage50-onboarding/sage50-onboarding-done/sage50-onboarding-done.component').then(m => m.Sage50OnboardingDoneComponent)
           }
         ]
+      },
+      {
+        path: 'main',
+        loadComponent: () => import('./sage50-main/sage50-main.component').then(m => m.Sage50MainComponent),
+        children: [
+          {
+            path: '',
+            redirectTo: 'dashboard',
+            pathMatch: 'full'
+          },
+          {
+            path: 'dashboard',
+            loadComponent: () => import('./sage50-main/sage50-dashboard/sage50-dashboard.component').then(m => m.Sage50DashboardComponent)
+          },
+          {
+            path: 'mapping',
+            loadComponent: () => import('./sage50-main/sage50-mapping/sage50-mapping.component').then(m => m.Sage50MappingComponent),
+            children: [
+              {
+                path: '',
+                redirectTo: 'employee',
+                pathMatch: 'full'
+              },
+              {
+                path: ':source_field',
+                loadComponent: () => import('./sage50-main/sage50-mapping/sage50-base-mapping/sage50-base-mapping.component').then(m => m.Sage50BaseMappingComponent)
+              }
+            ]
+          },
+          {
+            path: 'configuration',
+            loadComponent: () => import('./sage50-main/sage50-configuration/sage50-configuration.component').then(m => m.Sage50ConfigurationComponent),
+            children: [
+              {
+                path: '',
+                redirectTo: 'export_settings',
+                pathMatch: 'full'
+              },
+              {
+                path: 'export_settings',
+                loadComponent: () => import('./sage50-shared/sage50-export-settings/sage50-export-settings.component').then(m => m.Sage50ExportSettingsComponent)
+              },
+              {
+                path: 'import_settings',
+                loadComponent: () => import('./sage50-shared/sage50-import-settings/sage50-import-settings.component').then(m => m.Sage50ImportSettingsComponent)
+              },
+              {
+                path: 'advanced_settings',
+                loadComponent: () => import('./sage50-shared/sage50-advanced-settings/sage50-advanced-settings.component').then(m => m.Sage50AdvancedSettingsComponent)
+              }
+            ]
+          }
+        ]
       }
     ]
   }
