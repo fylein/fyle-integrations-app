045fd052a8e73839673b4b77468396e132194197
Viswas <viswas.h@fyle.in>
2025-12-15 19:47:04 +0530

    feat(intacct): hide password field for migrated orgs

--- src/app/shared/components/si/core/intacct-connector/intacct-connector.component.ts
+++ src/app/shared/components/si/core/intacct-connector/intacct-connector.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { brandingConfig, brandingFeatureConfig, brandingKbArticles, brandingStyle } from 'src/app/branding/branding-config';
 import { IntacctConnectorService } from 'src/app/core/services/si/si-core/si-connector.service';
 import { ConfigurationCta } from 'src/app/core/models/enum/enum.model';
+import { SiWorkspaceService } from 'src/app/core/services/si/si-core/si-workspace.service';
 
 @Component({
     selector: 'app-intacct-connector',
@@ -16,6 +17,8 @@ export class IntacctConnectorComponent implements OnInit {
   isLoading: boolean = true;
 
   connectSageIntacctForm: FormGroup;
+
+  isMigratedToRestApi: boolean = false;
 
   ConfigurationCtaText = ConfigurationCta;
 
@@ -38,7 +41,8 @@ export class IntacctConnectorComponent implements OnInit {
   constructor(
     private router: Router,
     @Inject(FormBuilder) private formBuilder: FormBuilder,
-    private intacctConnectorService: IntacctConnectorService
+    private intacctConnectorService: IntacctConnectorService,
+    private workspaceService: SiWorkspaceService
   ) { }
 
   save() {
@@ -59,9 +63,12 @@ export class IntacctConnectorComponent implements OnInit {
   private setupPage(): void {
     this.isLoading = true;
     this.isOnboarding = this.router.url.includes('onboarding');
-    this.intacctConnectorService.getIntacctFormGroup().subscribe((response) => {
-      this.connectSageIntacctForm = response.intacctSetupForm;
-      this.isLoading = false;
+    this.workspaceService.getFeatureConfigs().subscribe((featureConfigs) => {
+      this.isMigratedToRestApi = featureConfigs.migrated_to_rest_api;
+      this.intacctConnectorService.getIntacctFormGroup(this.isMigratedToRestApi).subscribe((intacctFormGroup) => {
+        this.connectSageIntacctForm = intacctFormGroup.intacctSetupForm;
+        this.isLoading = false;
+      });
     });
   }
 
