f61cd68616d6bee98b22272239386655b89e011b
DhaaraniCIT <dhaaranieaswari@gmail.com>
2026-01-21 16:20:21 +0530

    fix condition to show default_billable_field_based_on_project

--- src/app/integrations/intacct/intacct-shared/intacct-export-settings/intacct-export-settings.component.ts
+++ src/app/integrations/intacct/intacct-shared/intacct-export-settings/intacct-export-settings.component.ts
@@ -685,7 +685,7 @@ export class IntacctExportSettingsComponent implements OnInit {
       this.destinationOptions.CCC_EXPENSE_PAYMENT_TYPE = response[1].results.filter((attr: IntacctDestinationAttribute) => !attr.detail.is_reimbursable);
       this.destinationOptions.VENDOR = response[2].results;
       this.destinationOptions.CHARGE_CARD = response[3].results;
-      
+
       this.isMultiLineOption = brandingConfig.brandId !== 'co' ? true : false;
       this.getSettingsAndSetupForm();
 
