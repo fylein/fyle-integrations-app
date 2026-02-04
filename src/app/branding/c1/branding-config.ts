d213eea85af7e715e4a3e906fdebaf52e5500420
Dhaarani <dhaarani.s@sage.com>
2026-02-04 12:43:01 +0530

    feat: add info for default_billable_field_based_on_project (#1723)
    * feat: add info for default_billable_field_based_on_project
    
    * fix condition to show default_billable_field_based_on_project
    
    * fix condition to show default_billable_field_based_on_project
    
    * PR comment fix
    
    * lint fix
    
    * edgecase fix
    
    * small fix
    
    * small fix

--- src/app/branding/c1/branding-config.ts
+++ src/app/branding/c1/branding-config.ts
@@ -117,7 +117,8 @@ export const c1KbArticles: KbArticle[string] = {
             LANDING: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration`,
             CONNECTOR: `${brandingConfig.helpArticleDomain}/en/articles/9081356-generate-credentials-to-connect-with-sage-intacct`,
             SKIP_EXPORT: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration`,
-            DFV_READ_MORE: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration`
+            DFV_READ_MORE: `${brandingConfig.helpArticleDomain}/en/articles/9082146-configure-the-capital-one-sage-intacct-integration`,
+            DEFAULT_BILLABLE_FIELD_BASED_ON_PROJECT_READ_MORE: `${brandingConfig.helpArticleDomain}/en/articles/7882821-how-to-skip-exporting-specific-expenses-from-fyle-to-sage-intacct`
         },
         NETSUITE: {
             CONNECTOR: `${brandingConfig.helpArticleDomain}/en/articles/9471329-generate-credentials-to-connect-with-netsuite`,
